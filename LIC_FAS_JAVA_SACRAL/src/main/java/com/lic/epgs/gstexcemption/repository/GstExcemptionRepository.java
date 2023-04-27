666aba9236fa24cdd795260be037fad0ee669459
anandvechalapu <anand.vechalapu@gmail.com>
2023-04-27 16:48:39 +0530

    Updated

--- LIC_FAS_JAVA_SACRAL/src/main/java/com/lic/epgs/gstexcemption/repository/GstExcemptionRepository.java
+++ LIC_FAS_JAVA_SACRAL/src/main/java/com/lic/epgs/gstexcemption/repository/GstExcemptionRepository.java
@@ -1,14 +1,19 @@
-package com.lic.epgs.gstexcemption.repository;
-
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
 
-import com.lic.epgs.gstexcemption.dto.GstExcemptionDto;
-import com.lic.epgs.gstexcemption.dto.GstResponseDto;
+import com.lic.epgs.gstexcemption.model.GstExcemption;
 
-@Repository
-public interface GstExcemptionRepository extends JpaRepository<GstExcemptionDto, Integer> {
+public interface GstExcemptionRepository extends JpaRepository<GstExcemption, Long> {
 
-    GstResponseDto<List<GstExcemptionDto>> searchforExisting(String gstNo, String unitCode, String proposalNo);
+    GstExcemption findByGstExcemptionId(Long gstExcemptionId);
+
+    GstExcemption save(GstExcemption gstExcemption);
+
+    GstExcemption findByModifiedBy(String modifiedBy);
+
+    GstExcemption findByGstExcemptionStatus(String gstExcemptionStatus);
+
+    GstExcemption findByWorkflowStatus(String workflowStatus);
+
+    void deleteByGstExcemptionId(Long gstExcemptionId);
 
 }
