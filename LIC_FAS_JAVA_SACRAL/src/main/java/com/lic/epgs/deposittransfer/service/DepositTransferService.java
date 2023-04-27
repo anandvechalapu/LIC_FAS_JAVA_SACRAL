91a14f106dc8af0e24f50112f668e55383733470
anandvechalapu <anand.vechalapu@gmail.com>
2023-04-27 12:46:10 +0530

    Updated

--- LIC_FAS_JAVA_SACRAL/src/main/java/com/lic/epgs/deposittransfer/service/DepositTransferService.java
+++ LIC_FAS_JAVA_SACRAL/src/main/java/com/lic/epgs/deposittransfer/service/DepositTransferService.java
@@ -1,44 +1,49 @@
 package com.lic.epgs.deposittransfer.service;
 
-import java.util.Optional;
+import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.data.jpa.repository.Modifying;
-import org.springframework.data.jpa.repository.Query;
-import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Service;
-import org.springframework.transaction.annotation.Transactional;
 
-import com.lic.epgs.deposittransfer.dto.ApiResponseDto;
-import com.lic.epgs.deposittransfer.dto.DepositTransferDto;
-import com.lic.epgs.deposittransfer.entity.DepositTransferNotesTempEntity;
-import com.lic.epgs.deposittransfer.entity.DepositTransferTempEntity;
+import com.lic.epgs.deposittransfer.model.DepositTransfer;
 import com.lic.epgs.deposittransfer.repository.DepositTransferRepository;
 
 @Service
 public class DepositTransferService {
+	
+	@Autowired
+	private DepositTransferRepository depositTransferRepository;
+	
+	public DepositTransfer saveDepositTransfer(DepositTransfer depositTransfer) {
+		return depositTransferRepository.save(depositTransfer);
+	}
+	
+	public List<DepositTransfer> getAllDepositTransfers(){
+		return depositTransferRepository.findAll();
+	}
+	
+	public DepositTransfer getDepositTransferById(Long transferId) {
+		return depositTransferRepository.findByTransferId(transferId);
+	}
+	
+	public DepositTransfer getDepositTransferByTransferIdAndIsActiveTrue(Long transferId) {
+		return depositTransferRepository.findByTransferIdAndIsActiveTrue(transferId);
+	}
+	
+	public int updateTransferStatus(Long transferId, String transferStatus, String workFlowStatus) {
+		return depositTransferRepository.updateTransferStatus(transferId, transferStatus, workFlowStatus);
+	}
+	
+	public int setOldTransferInactive(Long transferId) {
+		return depositTransferRepository.setOldTransferInactive(transferId);
+	}
+	
+	public int setOldTransferNotesInactive(Long transferId) {
+		return depositTransferRepository.setOldTransferNotesInactive(transferId);
+	}
+	
+	public int updateTransferNotes(Long transferId, String transferNotes) {
+		return depositTransferRepository.updateTransferNotes(transferId, transferNotes);
+	}
 
-    @Autowired
-    private DepositTransferRepository depositTransferRepository;
-
-    public ApiResponseDto saveDepositTransfer(DepositTransferDto depositTransferDto) {
-        return depositTransferRepository.saveDepositTransfer(depositTransferDto);
-    }
-
-    public void updateDepositTransferTempEntity(String referenceNumber, String transferId) {
-        depositTransferRepository.updateDepositTransferTempEntity(referenceNumber, transferId);
-    }
-
-    public Optional<String> getDepositSeq() {
-        return depositTransferRepository.getDepositSeq();
-    }
-
-    public void updateDepositTransferStatus(int transferStatus, int workflowStatus, String transferId) {
-        depositTransferRepository.updateDepositTransferStatus(transferStatus, workflowStatus, transferId);
-    }
-
-    @Transactional
-    public void saveDepositTransferNotes(DepositTransferNotesTempEntity depositTransferNotesTempEntity) {
-        depositTransferRepository.saveDepositTransferNotes(depositTransferNotesTempEntity);
-    }
 }
