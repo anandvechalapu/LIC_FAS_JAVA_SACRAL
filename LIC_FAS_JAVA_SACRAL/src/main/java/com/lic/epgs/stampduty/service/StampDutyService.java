4b3e359afb26bb49208e729fa387235761edb5ee
anandvechalapu <anand.vechalapu@gmail.com>
2023-04-27 13:26:49 +0530

    Updated

--- LIC_FAS_JAVA_SACRAL/src/main/java/com/lic/epgs/stampduty/service/StampDutyService.java
+++ LIC_FAS_JAVA_SACRAL/src/main/java/com/lic/epgs/stampduty/service/StampDutyService.java
@@ -3,6 +3,10 @@ package com.lic.epgs.stampduty.service;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Service;
 
 import com.lic.epgs.stampduty.entity.StampDuty;
@@ -11,11 +15,41 @@ import com.lic.epgs.stampduty.repository.StampDutyRepository;
 @Service
 public class StampDutyService {
 
-    @Autowired
-    StampDutyRepository stampDutyRepository;
+	@Autowired
+	private StampDutyRepository stampDutyRepository;
 
-    public List<StampDuty> findByFilters(String roleType, String unitOfficeCode, String challanGRNNumber, String challanGRNDate, String stampDutyStatus, String startDate, String endDate) {
-        return stampDutyRepository.findByFilters(roleType, unitOfficeCode, challanGRNNumber, challanGRNDate, stampDutyStatus, startDate, endDate);
-    }
+	public StampDuty findByChallanGrnNo(String challanGrnNo) {
+		return stampDutyRepository.findByChallanGrnNo(challanGrnNo);
+	}
 
+	public StampDuty findByChallanGrnDate(String challanGrnDate) {
+		return stampDutyRepository.findByChallanGrnDate(challanGrnDate);
+	}
+
+	public StampDuty findByModifiedDateBetween(String fromDate, String toDate) {
+		return stampDutyRepository.findByModifiedDateBetween(fromDate, toDate);
+	}
+
+	public StampDuty findByRoleTypeMaker(String roleType) {
+		return stampDutyRepository.findByRoleTypeMaker(roleType);
+	}
+
+	public StampDuty findByRoleTypeChecker(String roleType) {
+		return stampDutyRepository.findByRoleTypeChecker(roleType);
+	}
+
+	public StampDuty findByStampDutyStatus(String stampDutyStatus) {
+		return stampDutyRepository.findByStampDutyStatus(stampDutyStatus);
+	}
+
+	public StampDuty findByUnitOfficeCode(String unitOfficeCode) {
+		return stampDutyRepository.findByUnitOfficeCode(unitOfficeCode);
+	}
+
+	public List<StampDuty> findByModifiedDateBetweenWithPagination(String fromDate, String toDate, Integer pageCount,
+			Integer limit) {
+		Pageable pageable = PageRequest.of(pageCount, limit, Sort.by("modifiedDate").descending());
+		Page<StampDuty> page = stampDutyRepository.findByModifiedDateBetweenWithPagination(fromDate, toDate, pageable);
+		return page.getContent();
+	}
 }
