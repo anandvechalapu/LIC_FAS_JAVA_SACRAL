7593f07f398e5a978b83f93c35e9240e4e4d7412
anandvechalapu <anand.vechalapu@gmail.com>
2023-04-27 16:49:14 +0530

    Updated

--- LIC_FAS_JAVA_SACRAL/src/main/java/com/lic/epgs/gstexcemption/service/GstExcemptionService.java
+++ LIC_FAS_JAVA_SACRAL/src/main/java/com/lic/epgs/gstexcemption/service/GstExcemptionService.java
@@ -5,18 +5,44 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-import com.lic.epgs.gstexcemption.dto.GstExcemptionDto;
-import com.lic.epgs.gstexcemption.dto.GstResponseDto;
+import com.lic.epgs.gstexcemption.model.GstExcemption;
 import com.lic.epgs.gstexcemption.repository.GstExcemptionRepository;
 
 @Service
 public class GstExcemptionService {
 
-    @Autowired
-    private GstExcemptionRepository gstExcemptionRepository;
-
-    public GstResponseDto<List<GstExcemptionDto>> searchforExisting(String gstNo, String unitCode, String proposalNo) {
-        return gstExcemptionRepository.searchforExisting(gstNo, unitCode, proposalNo);
-    }
-
+	@Autowired
+	private GstExcemptionRepository gstExcemptionRepository;
+	
+	public List<GstExcemption> getAllGstExcemptions() {
+		return gstExcemptionRepository.findAll();
+	}
+	
+	public GstExcemption getGstExcemptionById(Long gstExcemptionId) {
+		return gstExcemptionRepository.findByGstExcemptionId(gstExcemptionId);
+	}
+	
+	public GstExcemption createGstExcemption(GstExcemption gstExcemption) {
+		return gstExcemptionRepository.save(gstExcemption);
+	}
+	
+	public GstExcemption updateGstExcemption(GstExcemption gstExcemption) {
+		return gstExcemptionRepository.save(gstExcemption);
+	}
+	
+	public void deleteGstExcemptionById(Long gstExcemptionId) {
+		gstExcemptionRepository.deleteByGstExcemptionId(gstExcemptionId);
+	}
+	
+	public GstExcemption getGstExcemptionByModifiedBy(String modifiedBy) {
+		return gstExcemptionRepository.findByModifiedBy(modifiedBy);
+	}
+	
+	public GstExcemption getGstExcemptionByGstExcemptionStatus(String gstExcemptionStatus) {
+		return gstExcemptionRepository.findByGstExcemptionStatus(gstExcemptionStatus);
+	}
+	
+	public GstExcemption getGstExcemptionByWorkflowStatus(String workflowStatus) {
+		return gstExcemptionRepository.findByWorkflowStatus(workflowStatus);
+	}
 }
