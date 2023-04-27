64a9ac82205df65a6563cdde7977a99d19b91a6a
anandvechalapu <anand.vechalapu@gmail.com>
2023-04-27 12:30:20 +0530

    Updated

--- LIC_FAS_JAVA_SACRAL/src/main/java/com/lic/epgs/deposittransfer/repository/.java
+++ LIC_FAS_JAVA_SACRAL/src/main/java/com/lic/epgs/deposittransfer/repository/.java
@@ -1,34 +1,23 @@
 @Repository
-public class DepositTransferNotesRepository {
-    @PersistenceContext
-    private EntityManager entityManager;
+@Transactional
+public class ExistingDepositNotesRepository {
 
-    /**
-     * This method is used to save the deposit transfer note in the database.
-     * 
-     * @param depositTransferNotesDto
-     * @return ApiResponseDto
-     * @throws ConstraintViolationException
-     * @throws PersistenceException
-     */
-    public ApiResponseDto saveDepositnote(DepositTransferNotesDto depositTransferNotesDto) throws ConstraintViolationException, PersistenceException {
-        ApiResponseDto apiResponseDto = new ApiResponseDto();
-        try {
-            depositTransferNotesDto.setCreatedOn(LocalDateTime.now());
-            depositTransferNotesDto.setIsActive(true);
-            entityManager.persist(depositTransferNotesDto);
-            apiResponseDto.setTransactionStatus("SUCCESS");
-            apiResponseDto.setTransactionMessage("Successfully saved deposit transfer note");
-            apiResponseDto.setTransferId(depositTransferNotesDto.getTransferId());
-        } catch (ConstraintViolationException e) {
-            Logger.getLogger(DepositTransferNotesRepository.class.getName()).log(Level.SEVERE, e.getMessage(), e);
-            apiResponseDto.setTransactionStatus("ERROR");
-            apiResponseDto.setTransactionMessage(e.getMessage());
-        } catch (PersistenceException e) {
-            Logger.getLogger(DepositTransferNotesRepository.class.getName()).log(Level.SEVERE, e.getMessage(), e);
-            apiResponseDto.setTransactionStatus("ERROR");
-            apiResponseDto.setTransactionMessage(e.getMessage());
+    @PersistenceContext
+    private EntityManager em;
+    
+    public List<ExistingDepositNotes> getExistingDepositNotes_DepositTransfer(String transferId) {
+        if(transferId == null || transferId.isEmpty()) {
+            throw new IllegalArgumentException("Transfer ID cannot be null or empty.");
         }
-        return apiResponseDto;
+        try {
+            String queryString = "SELECT dn FROM ExistingDepositNotes dn WHERE dn.transferId = :transferId AND dn.isActive = true ORDER BY dn.transferId DESC";
+            TypedQuery<ExistingDepositNotes> query = em.createQuery(queryString, ExistingDepositNotes.class);
+            
+            query.setParameter("transferId", transferId);
+            
+            return query.getResultList();
+        } catch(Exception e) {
+            throw new RuntimeException("An error occurred while retrieving existing deposit notes: " + e.getMessage());
+        }
     }
 }
