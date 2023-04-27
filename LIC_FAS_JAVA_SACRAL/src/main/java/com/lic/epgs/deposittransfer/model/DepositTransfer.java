934345e47154557c6e2d86065f54abc85198d176
anandvechalapu <anand.vechalapu@gmail.com>
2023-04-27 12:42:10 +0530

    Updated

--- LIC_FAS_JAVA_SACRAL/src/main/java/com/lic/epgs/deposittransfer/model/DepositTransfer.java
+++ LIC_FAS_JAVA_SACRAL/src/main/java/com/lic/epgs/deposittransfer/model/DepositTransfer.java
@@ -1,57 +1,65 @@
 package com.lic.epgs.deposittransfer.model;
 
 import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
 import javax.persistence.Id;
-import javax.persistence.Table;
 
 @Entity
-@Table(name = "deposit_transfer")
 public class DepositTransfer {
 	
 	@Id
-	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long transferId;
-	
-	private Long modifiedBy;
-	
-	private String transferStatus;
-	
-	private String workflowStatus;
+	private String sourceAccount;
+	private String destinationAccount;
+	private Double amount;
+	private String status;
+	private String notes;
 	
 	public Long getTransferId() {
 		return transferId;
 	}
-
+	
 	public void setTransferId(Long transferId) {
 		this.transferId = transferId;
 	}
-
-	public Long getModifiedBy() {
-		return modifiedBy;
-	}
-
-	public void setModifiedBy(Long modifiedBy) {
-		this.modifiedBy = modifiedBy;
-	}
-
-	public String getTransferStatus() {
-		return transferStatus;
-	}
-
-	public void setTransferStatus(String transferStatus) {
-		this.transferStatus = transferStatus;
-	}
-
-	public String getWorkflowStatus() {
-		return workflowStatus;
-	}
-
-	public void setWorkflowStatus(String workflowStatus) {
-		this.workflowStatus = workflowStatus;
+	
+	public String getSourceAccount() {
+		return sourceAccount;
 	}
 	
+	public void setSourceAccount(String sourceAccount) {
+		this.sourceAccount = sourceAccount;
+	}
 	
-
+	public String getDestinationAccount() {
+		return destinationAccount;
+	}
+	
+	public void setDestinationAccount(String destinationAccount) {
+		this.destinationAccount = destinationAccount;
+	}
+	
+	public Double getAmount() {
+		return amount;
+	}
+	
+	public void setAmount(Double amount) {
+		this.amount = amount;
+	}
+	
+	public String getStatus() {
+		return status;
+	}
+	
+	public void setStatus(String status) {
+		this.status = status;
+	}
+	
+	public String getNotes() {
+		return notes;
+	}
+	
+	public void setNotes(String notes) {
+		this.notes = notes;
+	}
+	
 }
