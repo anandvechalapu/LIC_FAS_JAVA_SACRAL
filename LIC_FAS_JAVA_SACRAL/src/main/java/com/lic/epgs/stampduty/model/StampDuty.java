8fcc0c49b4b9c9b73b42b96b3d62750b60e29156
anandvechalapu <anand.vechalapu@gmail.com>
2023-04-27 13:25:36 +0530

    Updated

--- LIC_FAS_JAVA_SACRAL/src/main/java/com/lic/epgs/stampduty/model/StampDuty.java
+++ LIC_FAS_JAVA_SACRAL/src/main/java/com/lic/epgs/stampduty/model/StampDuty.java
@@ -1,40 +1,104 @@
 package com.lic.epgs.stampduty.model;
 
+import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.Table;
 
 @Entity
+@Table(name = "stamp_duty")
 public class StampDuty {
 
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Long id;
-    private String role;
-    private String unitOfficeCode;
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@Column(name = "id")
+	private Long id;
 
-    public Long getId() {
-        return id;
-    }
+	@Column(name = "challan_grn_no")
+	private String challanGrnNo;
 
-    public void setId(Long id) {
-        this.id = id;
-    }
+	@Column(name = "challan_grn_date")
+	private String challanGrnDate;
 
-    public String getRole() {
-        return role;
-    }
+	@Column(name = "role_type")
+	private String roleType;
 
-    public void setRole(String role) {
-        this.role = role;
-    }
+	@Column(name = "stamp_duty_status")
+	private String stampDutyStatus;
 
-    public String getUnitOfficeCode() {
-        return unitOfficeCode;
-    }
+	@Column(name = "unit_office_code")
+	private String unitOfficeCode;
 
-    public void setUnitOfficeCode(String unitOfficeCode) {
-        this.unitOfficeCode = unitOfficeCode;
-    }
+	@Column(name = "active")
+	private Boolean active;
+
+	@Column(name = "modified_date")
+	private String modifiedDate;
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getChallanGrnNo() {
+		return challanGrnNo;
+	}
+
+	public void setChallanGrnNo(String challanGrnNo) {
+		this.challanGrnNo = challanGrnNo;
+	}
+
+	public String getChallanGrnDate() {
+		return challanGrnDate;
+	}
+
+	public void setChallanGrnDate(String challanGrnDate) {
+		this.challanGrnDate = challanGrnDate;
+	}
+
+	public String getRoleType() {
+		return roleType;
+	}
+
+	public void setRoleType(String roleType) {
+		this.roleType = roleType;
+	}
+
+	public String getStampDutyStatus() {
+		return stampDutyStatus;
+	}
+
+	public void setStampDutyStatus(String stampDutyStatus) {
+		this.stampDutyStatus = stampDutyStatus;
+	}
+
+	public String getUnitOfficeCode() {
+		return unitOfficeCode;
+	}
+
+	public void setUnitOfficeCode(String unitOfficeCode) {
+		this.unitOfficeCode = unitOfficeCode;
+	}
+
+	public Boolean getActive() {
+		return active;
+	}
+
+	public void setActive(Boolean active) {
+		this.active = active;
+	}
+
+	public String getModifiedDate() {
+		return modifiedDate;
+	}
+
+	public void setModifiedDate(String modifiedDate) {
+		this.modifiedDate = modifiedDate;
+	}
+
 }
