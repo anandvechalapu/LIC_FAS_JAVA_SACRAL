a420f1030afc9c425914077b77fc9dbaf0d9058b
anandvechalapu <anand.vechalapu@gmail.com>
2023-04-27 15:33:07 +0530

    Updated

--- LIC_FAS_JAVA_SACRAL/src/main/java/com/lic/epgs/gstexcemption/controller/GstExcemptionController.java
+++ LIC_FAS_JAVA_SACRAL/src/main/java/com/lic/epgs/gstexcemption/controller/GstExcemptionController.java
@@ -1,30 +1,36 @@
-package com.lic.epgs.gstexcemption.repository;
-
-import com.lic.epgs.gstexcemption.dto.GstExcemptionDto;
-import com.lic.epgs.gstexcemption.dto.GSTResponseDto;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface GstAllDetailsByGstId9GstExcemptionRepository extends JpaRepository<GstExcemptionDto, Long> {
-    GSTResponseDto<GstExcemptionDto> getAllDetailsByGstId(Long gstExcemptionId);
-}
-
 package com.lic.epgs.gstexcemption.controller;
 
-import com.lic.epgs.gstexcemption.dto.GstExcemptionDto;
-import com.lic.epgs.gstexcemption.dto.GSTResponseDto;
-import com.lic.epgs.gstexcemption.service.GstAllDetailsByGstId9GstExcemptionService;
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.lic.epgs.gstexcemption.model.GstExcemption;
+import com.lic.epgs.gstexcemption.service.GstExcemptionService;
 
 @RestController
-@RequestMapping("/gstexcemption")
+@RequestMapping("/gstExcemption")
 public class GstExcemptionController {
+	
+	@Autowired
+	GstExcemptionService gstExcemptionService;
+	
+	@GetMapping("/{loginUser}/{unitCode}")
+	public List<GstExcemption> getGstExcemptionStatusForLoginUserAndUnitCode(@PathVariable String loginUser, @PathVariable String unitCode) {
+		return gstExcemptionService.findByLoginUserAndUnitCode(loginUser, unitCode);
+	}
+	
+	@GetMapping("/{gstExcemptionStatus}/{unitCode}")
+	public List<GstExcemption> getGstExcemptionStatusForGstExcemptionStatusAndUnitCode(@PathVariable String gstExcemptionStatus, @PathVariable String unitCode) {
+		return gstExcemptionService.findByGstExcemptionStatusAndUnitCode(gstExcemptionStatus, unitCode);
+	}
+	
+	@GetMapping("/{unitCode}")
+	public List<GstExcemption> getGstExcemptionStatusForUnitCode(@PathVariable String unitCode) {
+		return gstExcemptionService.findByUnitCode(unitCode);
+	}
 
-    @Autowired
-    private GstAllDetailsByGstId9GstExcemptionService gstAllDetailsByGstId9GstExcemptionService;
-
-    @GetMapping("/details/{gstExcemptionId}")
-    public GSTResponseDto<GstExcemptionDto> getAllDetailsByGstId(@PathVariable("gstExcemptionId") Long gstExcemptionId) {
-        return gstAllDetailsByGstId9GstExcemptionService.getAllDetailsByGstId(gstExcemptionId);
-    }
 }
