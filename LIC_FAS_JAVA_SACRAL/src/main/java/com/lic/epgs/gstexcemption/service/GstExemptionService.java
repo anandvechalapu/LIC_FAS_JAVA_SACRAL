16a4dcb7d203dafb4a3e48b37da8e51766adeaaf
anandvechalapu <anand.vechalapu@gmail.com>
2023-04-27 15:05:59 +0530

    Updated

--- LIC_FAS_JAVA_SACRAL/src/main/java/com/lic/epgs/gstexcemption/service/GstExemptionService.java
+++ LIC_FAS_JAVA_SACRAL/src/main/java/com/lic/epgs/gstexcemption/service/GstExemptionService.java
@@ -1,36 +1,45 @@
-package com.lic.epgs.gstexcemption.service;
-
-import com.lic.epgs.gstexcemption.dto.GstResponseDto;
-import com.lic.epgs.gstexcemption.dto.GstNotesDto;
-import com.lic.epgs.gstexcemption.repository.GstExemptionRepository;
-
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Service;
-
-import java.util.List;
-
 @Service
 public class GstExemptionService {
-
-    private static final Logger LOGGER = LoggerFactory.getLogger(GstExemptionService.class);
-
-    @Autowired
+    
     private GstExemptionRepository gstExemptionRepository;
-
-    public GstResponseDto<List<GstNotesDto>> getNotesList(String gstExemptionId) {
-        try {
-            GstResponseDto<List<GstNotesDto>> response = gstExemptionRepository.getNotesList(gstExemptionId);
-            if (response != null) {
-                return response;
-            } else {
-                return new GstResponseDto<>(false, "Deny");
-            }
-        } catch (Exception e) {
-            LOGGER.error("Error while retrieving GST notes of GST exemption ID: {}", gstExemptionId, e);
-            return new GstResponseDto<>(false, "Fail");
-        }
+ 
+    @Autowired
+    public GstExemptionService(GstExemptionRepository gstExemptionRepository) {
+        this.gstExemptionRepository = gstExemptionRepository;
     }
-
+    
+    // Get all documents for a given GST exemption
+    public List<DocumentDetailsDto> getDocumentsByGstExemptionId(Long gstExemptionId) {
+        return gstExemptionRepository.findByGstExemptionId(gstExemptionId);
+    }
+    
+    // Get a document for a given GST exemption and document ID
+    public Optional<DocumentDetailsDto> getDocumentByGstExemptionIdAndDocumentId(Long gstExemptionId, Long documentId) {
+        return gstExemptionRepository.findByGstExemptionIdAndDocumentId(gstExemptionId, documentId);
+    }
+    
+    // Check if a GST exemption exists
+    public boolean isGstExemptionExists(Long gstExemptionId) {
+        return gstExemptionRepository.existsByGstExemptionId(gstExemptionId);
+    }
+    
+    // Check if a document exists for a given GST exemption and document ID
+    public boolean isDocumentExistsForGstExemptionIdAndDocumentId(Long gstExemptionId, Long documentId) {
+        return gstExemptionRepository.existsByGstExemptionIdAndDocumentId(gstExemptionId, documentId);
+    }
+    
+    // Check if a document exists with the given document ID
+    public boolean isDocumentExistsByDocumentId(Long documentId) {
+        return gstExemptionRepository.existsByDocumentId(documentId);
+    }
+    
+    // Validate existingCondition1
+    public List<DocumentDetailsDto> validateExistingCondition1(Long gstExemptionId, boolean existingCondition1) {
+        return gstExemptionRepository.findByGstExemptionIdAndExistingCondition1(gstExemptionId, existingCondition1);
+    }
+    
+    // Validate existingCondition2
+    public List<DocumentDetailsDto> validateExistingCondition2(Long gstExemptionId, boolean existingCondition2) {
+        return gstExemptionRepository.findByGstExemptionIdAndExistingCondition2(gstExemptionId, existingCondition2);
+    }
 }
