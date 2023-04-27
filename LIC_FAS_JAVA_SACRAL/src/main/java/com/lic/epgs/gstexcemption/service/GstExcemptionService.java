07fee375cba185a03c4d97015eec31597c000c79
anandvechalapu <anand.vechalapu@gmail.com>
2023-04-27 16:00:39 +0530

    Updated

--- LIC_FAS_JAVA_SACRAL/src/main/java/com/lic/epgs/gstexcemption/service/GstExcemptionService.java
+++ LIC_FAS_JAVA_SACRAL/src/main/java/com/lic/epgs/gstexcemption/service/GstExcemptionService.java
@@ -5,25 +5,18 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-import com.lic.epgs.gstexcemption.model.GstExcemption;
+import com.lic.epgs.gstexcemption.dto.GstExcemptionDto;
+import com.lic.epgs.gstexcemption.dto.GstResponseDto;
 import com.lic.epgs.gstexcemption.repository.GstExcemptionRepository;
 
 @Service
 public class GstExcemptionService {
-	
-	@Autowired
-	GstExcemptionRepository gstExcemptionRepository;
-	
-	public List<GstExcemption> findByLoginUserAndUnitCode(String loginUser, String unitCode) {
-		return gstExcemptionRepository.findByLoginUserAndUnitCode(loginUser, unitCode);
-	}
-	
-	public List<GstExcemption> findByGstExcemptionStatusAndUnitCode(String gstExcemptionStatus, String unitCode) {
-		return gstExcemptionRepository.findByGstExcemptionStatusAndUnitCode(gstExcemptionStatus, unitCode);
-	}
-	
-	public List<GstExcemption> findByUnitCode(String unitCode) {
-		return gstExcemptionRepository.findByUnitCode(unitCode);
-	}
+
+    @Autowired
+    private GstExcemptionRepository gstExcemptionRepository;
+
+    public GstResponseDto<List<GstExcemptionDto>> searchforExisting(String gstNo, String unitCode, String proposalNo) {
+        return gstExcemptionRepository.searchforExisting(gstNo, unitCode, proposalNo);
+    }
 
 }
