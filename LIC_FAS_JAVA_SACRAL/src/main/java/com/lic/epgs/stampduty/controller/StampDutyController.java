40e45f5be55fc855acce9a18924a121b0fe54912
anandvechalapu <anand.vechalapu@gmail.com>
2023-04-27 13:37:58 +0530

    Updated

--- LIC_FAS_JAVA_SACRAL/src/main/java/com/lic/epgs/stampduty/controller/StampDutyController.java
+++ LIC_FAS_JAVA_SACRAL/src/main/java/com/lic/epgs/stampduty/controller/StampDutyController.java
@@ -1,30 +1,67 @@
 package com.lic.epgs.stampduty.controller;
 
-import java.util.List;
-
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.lic.epgs.stampduty.entity.StampDuty;
-import com.lic.epgs.stampduty.service.StampDutyService;
+import com.lic.epgs.stampduty.model.StampDutyTempModel;
+import com.lic.epgs.stampduty.service.SaveStampDutyBasicDetailsService;
 
 @RestController
+@RequestMapping("/stampduty/")
 public class StampDutyController {
 
-    @Autowired
-    StampDutyService stampDutyService;
+	@Autowired
+	private SaveStampDutyBasicDetailsService saveStampDutyBasicDetailsService;
 
-    @GetMapping(value = "/stampduty")
-    public List<StampDuty> getStampDutyRecords(@RequestParam(value = "roleType") String roleType,
-            @RequestParam(value = "unitOfficeCode") String unitOfficeCode,
-            @RequestParam(value = "challanGRNNumber") String challanGRNNumber,
-            @RequestParam(value = "challanGRNDate") String challanGRNDate,
-            @RequestParam(value = "stampDutyStatus") String stampDutyStatus,
-            @RequestParam(value = "startDate") String startDate,
-            @RequestParam(value = "endDate") String endDate) {
-        return stampDutyService.findByFilters(roleType, unitOfficeCode, challanGRNNumber, challanGRNDate, stampDutyStatus, startDate, endDate);
-    }
+	@GetMapping("/stampId/{stampId}")
+	public ResponseEntity<StampDutyTempModel> getStampDutyByStampId(@PathVariable Long stampId) {
+		StampDutyTempModel stampDutyTempModel = saveStampDutyBasicDetailsService.findByStampId(stampId);
+		if (stampDutyTempModel != null) {
+			return new ResponseEntity<>(stampDutyTempModel, HttpStatus.OK);
+		} else {
+			return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+		}
+	}
 
-}
+	@GetMapping("/policyStampTypeId/{policyStampTypeId}")
+	public ResponseEntity<StampDutyTempModel> getStampDutyByPolicyStampTypeId(@PathVariable Long policyStampTypeId) {
+		StampDutyTempModel stampDutyTempModel = saveStampDutyBasicDetailsService.findByPolicyStampTypeId(policyStampTypeId);
+		if (stampDutyTempModel != null) {
+			return new ResponseEntity<>(stampDutyTempModel, HttpStatus.OK);
+		} else {
+			return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+		}
+	}
+
+	@GetMapping("/mstStampId/{mstStampId}")
+	public ResponseEntity<StampDutyTempModel> getStampDutyByMstStampId(@PathVariable Long mstStampId) {
+		StampDutyTempModel stampDutyTempModel = saveStampDutyBasicDetailsService.findByMstStampId(mstStampId);
+		if (stampDutyTempModel != null) {
+			return new ResponseEntity<>(stampDutyTempModel, HttpStatus.OK);
+		} else {
+			return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+		}
+	}
+
+	@GetMapping("/challanGrnNo/{challanGrnNo}")
+	public ResponseEntity<StampDutyTempModel> getStampDutyByChallanGrnNo(@PathVariable String challanGrnNo) {
+		StampDutyTempModel stampDutyTempModel = saveStampDutyBasicDetailsService.findByChallanGrnNo(challanGrnNo);
+		if (stampDutyTempModel != null) {
+			return new ResponseEntity<>(stampDutyTempModel, HttpStatus.OK);
+		} else {
+			return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+		}
+	}
+
+	@GetMapping("/certificateQueryIdNo/{certificateQueryIdNo}")
+	public ResponseEntity<StampDutyTempModel> getStampDutyByCertificateQueryIdNo(@PathVariable String certificateQueryIdNo) {
+		StampDutyTempModel stampDutyTempModel = saveStampDutyBasicDetailsService.findByCertificateQueryIdNo(certificateQueryIdNo);
+		if (stampDutyTempModel != null) {
+			return new ResponseEntity<>(stampDutyTempModel, HttpStatus.OK);
+		} else {
+			return new ResponseEntity<>(
