099b6ad539e75c1160cbd0a2cbdc3bacfc26dafc
anandvechalapu <anand.vechalapu@gmail.com>
2023-04-27 15:06:38 +0530

    Updated

--- LIC_FAS_JAVA_SACRAL/src/main/java/com/lic/epgs/gstexcemption/controller/GstExemptionController.java
+++ LIC_FAS_JAVA_SACRAL/src/main/java/com/lic/epgs/gstexcemption/controller/GstExemptionController.java
@@ -1,26 +1,56 @@
 package com.lic.epgs.gstexcemption.controller;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
-
-import com.lic.epgs.gstexcemption.dto.GstResponseDto;
-import com.lic.epgs.gstexcemption.dto.GstNotesDto;
+import com.lic.epgs.gstexcemption.dto.DocumentDetailsDto;
 import com.lic.epgs.gstexcemption.service.GstExemptionService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.Optional;
 
 @RestController
-@RequestMapping("/gst/exemption")
+@RequestMapping("/gst-exemption")
 public class GstExemptionController {
 
+    @Autowired
     private GstExemptionService gstExemptionService;
 
-    public GstExemptionController(GstExemptionService gstExemptionService) {
-        this.gstExemptionService = gstExemptionService;
+    @GetMapping("/documents/{gstExemptionId}")
+    public List<DocumentDetailsDto> getDocumentsByGstExemptionId(@PathVariable Long gstExemptionId) {
+        return gstExemptionService.getDocumentsByGstExemptionId(gstExemptionId);
     }
 
-    @GetMapping("/{gstExemptionId}/notes")
-    public GstResponseDto<List<GstNotesDto>> getNotesList(@PathVariable("gstExemptionId")String gstExemptionId) {
-        return gstExemptionService.getNotesList(gstExemptionId);
+    @GetMapping("/document/{gstExemptionId}/{documentId}")
+    public Optional<DocumentDetailsDto> getDocumentByGstExemptionIdAndDocumentId(@PathVariable Long gstExemptionId,
+                                                                                @PathVariable Long documentId) {
+        return gstExemptionService.getDocumentByGstExemptionIdAndDocumentId(gstExemptionId, documentId);
+    }
+
+    @GetMapping("/exists/{gstExemptionId}")
+    public boolean isGstExemptionExists(@PathVariable Long gstExemptionId) {
+        return gstExemptionService.isGstExemptionExists(gstExemptionId);
+    }
+
+    @GetMapping("/exists/{gstExemptionId}/{documentId}")
+    public boolean isDocumentExistsForGstExemptionIdAndDocumentId(@PathVariable Long gstExemptionId,
+                                                                 @PathVariable Long documentId) {
+        return gstExemptionService.isDocumentExistsForGstExemptionIdAndDocumentId(gstExemptionId, documentId);
+    }
+
+    @GetMapping("/exists/document/{documentId}")
+    public boolean isDocumentExistsByDocumentId(@PathVariable Long documentId) {
+        return gstExemptionService.isDocumentExistsByDocumentId(documentId);
+    }
+
+    @GetMapping("/validate/existingCondition1/{gstExemptionId}/{existingCondition1}")
+    public List<DocumentDetailsDto> validateExistingCondition1(@PathVariable Long gstExemptionId,
+                                                              @PathVariable boolean existingCondition1) {
+        return gstExemptionService.validateExistingCondition1(gstExemptionId, existingCondition1);
+    }
+
+    @GetMapping("/validate/existingCondition2/{gstExemptionId}/{existingCondition2}")
+    public List<DocumentDetailsDto> validateExistingCondition2(@PathVariable Long gstExemptionId,
+                                                              @PathVariable boolean existingCondition2) {
+        return gstExemptionService.validateExistingCondition2(gstExemptionId, existingCondition2);
     }
 }
