34ff8b9532cb1a1898d2d31cb0ce2c6db970d4e0
anandvechalapu <anand.vechalapu@gmail.com>
2023-04-27 12:37:45 +0530

    initial commit

---
+++ LIC_FAS_JAVA_SACRAL/src/main/java/com/lic/epgs/deposittransfer/model/DepositTransfer.java
@@ -0,0 +1,57 @@
+package com.lic.epgs.deposittransfer.model;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "deposit_transfer")
+public class DepositTransfer {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long transferId;
+	
+	private Long modifiedBy;
+	
+	private String transferStatus;
+	
+	private String workflowStatus;
+	
+	public Long getTransferId() {
+		return transferId;
+	}
+
+	public void setTransferId(Long transferId) {
+		this.transferId = transferId;
+	}
+
+	public Long getModifiedBy() {
+		return modifiedBy;
+	}
+
+	public void setModifiedBy(Long modifiedBy) {
+		this.modifiedBy = modifiedBy;
+	}
+
+	public String getTransferStatus() {
+		return transferStatus;
+	}
+
+	public void setTransferStatus(String transferStatus) {
+		this.transferStatus = transferStatus;
+	}
+
+	public String getWorkflowStatus() {
+		return workflowStatus;
+	}
+
+	public void setWorkflowStatus(String workflowStatus) {
+		this.workflowStatus = workflowStatus;
+	}
+	
+	
+
+}
