b0fb0f1c12d3af92f020f2cffe64259a6b9e77e9
anandvechalapu <anand.vechalapu@gmail.com>
2023-04-27 16:01:23 +0530

    Updated

--- LIC_FAS_JAVA_SACRAL/src/main/java/com/lic/epgs/gstexcemption/controller/GstExcemptionController.java
+++ LIC_FAS_JAVA_SACRAL/src/main/java/com/lic/epgs/gstexcemption/controller/GstExcemptionController.java
@@ -5,32 +5,21 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.lic.epgs.gstexcemption.model.GstExcemption;
+import com.lic.epgs.gstexcemption.dto.GstExcemptionDto;
+import com.lic.epgs.gstexcemption.dto.GstResponseDto;
 import com.lic.epgs.gstexcemption.service.GstExcemptionService;
 
 @RestController
-@RequestMapping("/gstExcemption")
 public class GstExcemptionController {
-	
-	@Autowired
-	GstExcemptionService gstExcemptionService;
-	
-	@GetMapping("/{loginUser}/{unitCode}")
-	public List<GstExcemption> getGstExcemptionStatusForLoginUserAndUnitCode(@PathVariable String loginUser, @PathVariable String unitCode) {
-		return gstExcemptionService.findByLoginUserAndUnitCode(loginUser, unitCode);
-	}
-	
-	@GetMapping("/{gstExcemptionStatus}/{unitCode}")
-	public List<GstExcemption> getGstExcemptionStatusForGstExcemptionStatusAndUnitCode(@PathVariable String gstExcemptionStatus, @PathVariable String unitCode) {
-		return gstExcemptionService.findByGstExcemptionStatusAndUnitCode(gstExcemptionStatus, unitCode);
-	}
-	
-	@GetMapping("/{unitCode}")
-	public List<GstExcemption> getGstExcemptionStatusForUnitCode(@PathVariable String unitCode) {
-		return gstExcemptionService.findByUnitCode(unitCode);
-	}
+
+    @Autowired
+    private GstExcemptionService gstExcemptionService;
+
+    @GetMapping("/gstExcemption/{gstNo}/{unitCode}/{proposalNo}")
+    public GstResponseDto<List<GstExcemptionDto>> searchforExisting(@PathVariable String gstNo, @PathVariable String unitCode, @PathVariable String proposalNo) {
+        return gstExcemptionService.searchforExisting(gstNo, unitCode, proposalNo);
+    }
 
 }
