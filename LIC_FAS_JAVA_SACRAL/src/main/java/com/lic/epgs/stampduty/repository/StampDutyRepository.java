0090fbeea1cc5059b64392598d43d4efe7c66467
anandvechalapu <anand.vechalapu@gmail.com>
2023-04-27 13:24:34 +0530

    Updated

--- LIC_FAS_JAVA_SACRAL/src/main/java/com/lic/epgs/stampduty/repository/StampDutyRepository.java
+++ LIC_FAS_JAVA_SACRAL/src/main/java/com/lic/epgs/stampduty/repository/StampDutyRepository.java
@@ -6,12 +6,31 @@ import org.springframework.stereotype.Repository;
 
 import com.lic.epgs.stampduty.entity.StampDuty;
 
-import java.util.List;
-
 @Repository
 public interface StampDutyRepository extends JpaRepository<StampDuty, Long> {
 
-    @Query("SELECT s FROM StampDuty s WHERE s.roleType = ?1 AND s.unitOfficeCode = ?2 AND s.challanGRNNumber = ?3 AND s.challanGRNDate = ?4 AND s.stampDutyStatus = ?5 AND s.createdDate BETWEEN ?6 AND ?7")
-    List<StampDuty> findByFilters(String roleType, String unitOfficeCode, String challanGRNNumber, String challanGRNDate, String stampDutyStatus, String startDate, String endDate);
-
+	@Query(value = "SELECT s FROM StampDuty s WHERE s.challanGrnNo = ?1 AND s.active = true ORDER BY s.modifiedDate DESC")
+	public StampDuty findByChallanGrnNo(String challanGrnNo);
+	
+	@Query(value = "SELECT s FROM StampDuty s WHERE s.challanGrnDate = ?1 AND s.active = true ORDER BY s.modifiedDate DESC")
+	public StampDuty findByChallanGrnDate(String challanGrnDate);
+	
+	@Query(value = "SELECT s FROM StampDuty s WHERE s.modifiedDate BETWEEN ?1 AND ?2 AND s.active = true ORDER BY s.modifiedDate DESC")
+	public StampDuty findByModifiedDateBetween(String fromDate, String toDate);
+	
+	@Query(value = "SELECT s FROM StampDuty s WHERE s.roleType = ?1 AND s.stampDutyStatus IS NULL AND s.active = true ORDER BY s.modifiedDate DESC")
+	public StampDuty findByRoleTypeMaker(String roleType);
+	
+	@Query(value = "SELECT s FROM StampDuty s WHERE s.roleType = ?1 AND s.stampDutyStatus IS NULL AND s.active = true ORDER BY s.modifiedDate DESC")
+	public StampDuty findByRoleTypeChecker(String roleType);
+	
+	@Query(value = "SELECT s FROM StampDuty s WHERE s.stampDutyStatus = ?1 AND s.active = true ORDER BY s.modifiedDate DESC")
+	public StampDuty findByStampDutyStatus(String stampDutyStatus);
+	
+	@Query(value = "SELECT s FROM StampDuty s WHERE s.unitOfficeCode = ?1 AND s.active = true ORDER BY s.modifiedDate DESC")
+	public StampDuty findByUnitOfficeCode(String unitOfficeCode);
+	
+	@Query(value = "SELECT s FROM StampDuty s WHERE s.modifiedDate BETWEEN ?1 AND ?2 AND s.active = true ORDER BY s.modifiedDate DESC")
+	public StampDuty findByModifiedDateBetweenWithPagination(String fromDate, String toDate, Integer pageCount, Integer limit);
+	
 }
