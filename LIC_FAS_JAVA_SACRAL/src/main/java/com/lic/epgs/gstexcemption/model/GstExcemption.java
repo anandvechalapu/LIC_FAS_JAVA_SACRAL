5bcfc9c565808dc8f3424daaccdd4a63ff33865e
anandvechalapu <anand.vechalapu@gmail.com>
2023-04-27 16:16:13 +0530

    Updated

--- LIC_FAS_JAVA_SACRAL/src/main/java/com/lic/epgs/gstexcemption/model/GstExcemption.java
+++ LIC_FAS_JAVA_SACRAL/src/main/java/com/lic/epgs/gstexcemption/model/GstExcemption.java
@@ -1,49 +1,57 @@
 package com.lic.epgs.gstexcemption.model;
 
-import java.util.List;
-
 import javax.persistence.Entity;
+import javax.persistence.Id;
 import javax.persistence.Table;
 
 @Entity
-@Table(name = "gst_excemption")
+@Table(name="gst_excemption")
 public class GstExcemption {
-
-    private String gstNo;
-    private String unitCode;
-    private String proposalNo;
-    private List<GstExcemptionDto> gstExcemptionDto;
-
-    public String getGstNo() {
-        return gstNo;
-    }
-
-    public void setGstNo(String gstNo) {
-        this.gstNo = gstNo;
-    }
-
-    public String getUnitCode() {
-        return unitCode;
-    }
-
-    public void setUnitCode(String unitCode) {
-        this.unitCode = unitCode;
-    }
-
-    public String getProposalNo() {
-        return proposalNo;
-    }
-
-    public void setProposalNo(String proposalNo) {
-        this.proposalNo = proposalNo;
-    }
-
-    public List<GstExcemptionDto> getGstExcemptionDto() {
-        return gstExcemptionDto;
-    }
-
-    public void setGstExcemptionDto(List<GstExcemptionDto> gstExcemptionDto) {
-        this.gstExcemptionDto = gstExcemptionDto;
-    }
-
+  
+	@Id
+	private String gstExcemptionId;
+	private String clientId;
+	private String gstNumber;
+	private String gstExcempted;
+	private String gstExcemptedDate;
+	private String gstExcemptedReason;
+	
+	public String getGstExcemptionId() {
+		return gstExcemptionId;
+	}
+	public void setGstExcemptionId(String gstExcemptionId) {
+		this.gstExcemptionId = gstExcemptionId;
+	}
+	public String getClientId() {
+		return clientId;
+	}
+	public void setClientId(String clientId) {
+		this.clientId = clientId;
+	}
+	public String getGstNumber() {
+		return gstNumber;
+	}
+	public void setGstNumber(String gstNumber) {
+		this.gstNumber = gstNumber;
+	}
+	public String getGstExcempted() {
+		return gstExcempted;
+	}
+	public void setGstExcempted(String gstExcempted) {
+		this.gstExcempted = gstExcempted;
+	}
+	public String getGstExcemptedDate() {
+		return gstExcemptedDate;
+	}
+	public void setGstExcemptedDate(String gstExcemptedDate) {
+		this.gstExcemptedDate = gstExcemptedDate;
+	}
+	public String getGstExcemptedReason() {
+		return gstExcemptedReason;
+	}
+	public void setGstExcemptedReason(String gstExcemptedReason) {
+		this.gstExcemptedReason = gstExcemptedReason;
+	}
+	
+	
 }
