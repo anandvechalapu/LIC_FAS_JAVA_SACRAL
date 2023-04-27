1fd2cac9f28be7e0688298f22fcba93605bb5930
anandvechalapu <anand.vechalapu@gmail.com>
2023-04-27 12:46:57 +0530

    Updated

--- LIC_FAS_JAVA_SACRAL/src/main/java/com/lic/epgs/deposittransfer/controller/DepositTransferController.java
+++ LIC_FAS_JAVA_SACRAL/src/main/java/com/lic/epgs/deposittransfer/controller/DepositTransferController.java
@@ -1,45 +1,64 @@
 package com.lic.epgs.deposittransfer.controller;
 
-import java.util.Optional;
+import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.lic.epgs.deposittransfer.dto.ApiResponseDto;
-import com.lic.epgs.deposittransfer.dto.DepositTransferDto;
-import com.lic.epgs.deposittransfer.entity.DepositTransferNotesTempEntity;
+import com.lic.epgs.deposittransfer.model.DepositTransfer;
 import com.lic.epgs.deposittransfer.service.DepositTransferService;
 
 @RestController
+@RequestMapping("/api/deposittransfer")
 public class DepositTransferController {
-
+	
 	@Autowired
 	private DepositTransferService depositTransferService;
 	
-	@PostMapping("/saveDepositTransfer")
-	public ApiResponseDto saveDepositTransfer(@RequestBody DepositTransferDto depositTransferDto) {
-		return depositTransferService.saveDepositTransfer(depositTransferDto);
+	@PostMapping
+	public DepositTransfer saveDepositTransfer(@RequestBody DepositTransfer depositTransfer) {
+		return depositTransferService.saveDepositTransfer(depositTransfer);
 	}
 	
-	@PostMapping("/updateDepositTransferTempEntity")
-	public void updateDepositTransferTempEntity(String referenceNumber, String transferId) {
-		depositTransferService.updateDepositTransferTempEntity(referenceNumber, transferId);
+	@GetMapping
+	public List<DepositTransfer> getAllDepositTransfers(){
+		return depositTransferService.getAllDepositTransfers();
 	}
 	
-	@PostMapping("/getDepositSeq")
-	public Optional<String> getDepositSeq() {
-		return depositTransferService.getDepositSeq();
+	@GetMapping("/{transferId}")
+	public DepositTransfer getDepositTransferById(@PathVariable Long transferId) {
+		return depositTransferService.getDepositTransferById(transferId);
 	}
 	
-	@PostMapping("/updateDepositTransferStatus")
-	public void updateDepositTransferStatus(int transferStatus, int workflowStatus, String transferId) {
-		depositTransferService.updateDepositTransferStatus(transferStatus, workflowStatus, transferId);
+	@GetMapping("/active/{transferId}")
+	public DepositTransfer getDepositTransferByTransferIdAndIsActiveTrue(@PathVariable Long transferId) {
+		return depositTransferService.getDepositTransferByTransferIdAndIsActiveTrue(transferId);
 	}
 	
-	@PostMapping("/saveDepositTransferNotes")
-	public void saveDepositTransferNotes(@RequestBody DepositTransferNotesTempEntity depositTransferNotesTempEntity) {
-		depositTransferService.saveDepositTransferNotes(depositTransferNotesTempEntity);
+	@PutMapping("/{transferId}")
+	public int updateTransferStatus(@PathVariable Long transferId, String transferStatus, String workFlowStatus) {
+		return depositTransferService.updateTransferStatus(transferId, transferStatus, workFlowStatus);
 	}
+	
+	@PutMapping("/inactive/{transferId}")
+	public int setOldTransferInactive(@PathVariable Long transferId) {
+		return depositTransferService.setOldTransferInactive(transferId);
+	}
+	
+	@PutMapping("/notes/inactive/{transferId}")
+	public int setOldTransferNotesInactive(@PathVariable Long transferId) {
+		return depositTransferService.setOldTransferNotesInactive(transferId);
+	}
+	
+	@PutMapping("/notes/{transferId}")
+	public int updateTransferNotes(@PathVariable Long transferId, String transferNotes) {
+		return depositTransferService.updateTransferNotes(transferId, transferNotes);
+	}
+
 }
