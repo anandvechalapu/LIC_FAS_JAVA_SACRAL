ed6dcbe7c476772a05e62535b65bf56ac33e7292
anandvechalapu <anand.vechalapu@gmail.com>
2023-04-27 11:43:44 +0530

    Updated

--- LIC_FAS_JAVA_SACRAL/src/main/java/com/lic/epgs/commission/controller/CommissionController.java
+++ LIC_FAS_JAVA_SACRAL/src/main/java/com/lic/epgs/commission/controller/CommissionController.java
@@ -3,40 +3,54 @@ package com.lic.epgs.commission.controller;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.lic.epgs.commission.model.Commission;
+import com.lic.epgs.commission.model.CommissionDetails;
+import com.lic.epgs.commission.model.CommissionNotesTemp;
+import com.lic.epgs.commission.model.CommissionQuestionDetailsTemp;
 import com.lic.epgs.commission.service.CommissionService;
+
+import java.util.List;
+import java.util.Optional;
 
 @RestController
 @RequestMapping("/commission")
 public class CommissionController {
-    
+
     @Autowired
     private CommissionService commissionService;
 
-    @GetMapping("/{commissionId}/{commissionDetailsId}")
-    public Commission getCommission(@PathVariable("commissionId") String commissionId, @PathVariable("commissionDetailsId") Long commissionDetailsId){
-        return commissionService.findByCommissionIdAndCommissionDetailsId(commissionId, commissionDetailsId);
+    @GetMapping("/{commissionDetailsId}")
+    public Optional<Commission> findByCommissionDetailsId(@PathVariable Long commissionDetailsId) {
+        return commissionService.findByCommissionDetailsId(commissionDetailsId);
     }
 
-    @PostMapping
-    public Commission saveCommission(@RequestBody Commission commission){
-        return commissionService.save(commission);
+    @PutMapping("/{commissionId}")
+    public int updateCommissionStatusAndWorkflowStatusByCommissionId(@PathVariable Long commissionId) {
+        return commissionService.updateCommissionStatusAndWorkflowStatusByCommissionId(commissionId);
     }
 
-    @PutMapping
-    public Commission updateCommission(@RequestBody Commission commission){
-        return commissionService.update(commission);
+    @GetMapping("/deleteByCommissionDetailsIdIsNull")
+    public int deleteByCommissionDetailsIdIsNull() {
+        return commissionService.deleteByCommissionDetailsIdIsNull();
     }
 
-    @PutMapping("/addNotesAndQuestions")
-    public void addCommissionNotesAndQuestions(@RequestBody Commission commission){
-        commissionService.addCommissionNotesAndQuestions(commission);
+    @GetMapping("/findCommissionDetailsByCommissionDetailsId/{commissionDetailsId}")
+    public Optional<CommissionDetails> findCommissionDetailsByCommissionDetailsId(@PathVariable Long commissionDetailsId) {
+        return commissionService.findCommissionDetailsByCommissionDetailsId(commissionDetailsId);
+    }
+
+    @GetMapping("/findCommissionQuestionDetailsTempByCommissionDetailsId/{commissionDetailsId}")
+    public List<CommissionQuestionDetailsTemp> findCommissionQuestionDetailsTempByCommissionDetailsId(@PathVariable Long commissionDetailsId) {
+        return commissionService.findCommissionQuestionDetailsTempByCommissionDetailsId(commissionDetailsId);
+    }
+
+    @GetMapping("/findCommissionNotesTempByCommissionDetailsId/{commissionDetailsId}")
+    public List<CommissionNotesTemp> findCommissionNotesTempByCommissionDetailsId(@PathVariable Long commissionDetailsId) {
+        return commissionService.findCommissionNotesTempByCommissionDetailsId(commissionDetailsId);
     }
 
 }
