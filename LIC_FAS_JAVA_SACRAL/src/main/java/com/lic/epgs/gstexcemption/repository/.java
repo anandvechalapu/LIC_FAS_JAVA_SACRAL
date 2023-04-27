7c545035921d6ad3f656e69ffdb4330fde5de58d
anandvechalapu <anand.vechalapu@gmail.com>
2023-04-27 16:07:05 +0530

    Updated

--- LIC_FAS_JAVA_SACRAL/src/main/java/com/lic/epgs/gstexcemption/repository/.java
+++ LIC_FAS_JAVA_SACRAL/src/main/java/com/lic/epgs/gstexcemption/repository/.java
@@ -1,34 +1,31 @@
-package com.lic.epgs.gstexcemption.repository;
-
-import com.lic.epgs.gstexcemption.dto.GstResponseDto;
-import com.lic.epgs.gstexcemption.dto.GstNotesDto;
-
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Repository;
-
-import java.util.List;
-
 @Repository
-public class GstExemptionRepository {
-
-    private static final Logger LOGGER = LoggerFactory.getLogger(GstExemptionRepository.class);
-
-    @Autowired
-    private GstExemptionDao gstExemptionDao;
-
-    public GstResponseDto<List<GstNotesDto>> getNotesList(String gstExemptionId) {
-        try {
-            List<GstNotesDto> gstNotesList = gstExemptionDao.getNotesList(gstExemptionId);
-            if (gstNotesList != null && !gstNotesList.isEmpty()) {
-                return new GstResponseDto<>(gstNotesList);
-            } else {
-                return new GstResponseDto<>(false, "Deny");
-            }
-        } catch (Exception e) {
-            LOGGER.error("Error while retrieving GST notes of GST exemption ID: {}", gstExemptionId, e);
-            return new GstResponseDto<>(false, "Fail");
-        }
-    }
+public class GSTExcemptionRepository {
+	
+	public List<GstExcemptionDto> searchforInProgress(String loginUser, String unitCode, String gstNo, String proposalNo) {
+		try {
+			if (loginUser.equals("GST Checker")) {
+				return gstCheckerSearchInProgress(unitCode, gstNo, proposalNo);
+			} else if (loginUser.equals("GST Approver")) {
+				return mapEntity(unitCode, gstNo, proposalNo);
+			}
+		} catch (Exception e) {
+			return new ArrayList<GstExcemptionDto>() {{
+				add(new GstExcemptionDto() {{
+					setTransactionMessage("FAIL");
+					setTransactionStatus("ERROR");
+				}});
+			}};
+		}
+		return new ArrayList<>();
+	}
+	
+	private List<GstExcemptionDto> gstCheckerSearchInProgress(String unitCode, String gstNo, String proposalNo) {
+		// TODO: implement method
+		return null;
+	}
+	
+	private List<GstExcemptionDto> mapEntity(String unitCode, String gstNo, String proposalNo) {
+		// TODO: implement method
+		return null;
+	}
 }
