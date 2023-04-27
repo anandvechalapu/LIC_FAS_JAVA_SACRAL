38b91f9abd0a3ecbd085ded672e0f01062dde4b1
anandvechalapu <anand.vechalapu@gmail.com>
2023-04-27 16:17:22 +0530

    Updated

--- LIC_FAS_JAVA_SACRAL/src/main/java/com/lic/epgs/gstexcemption/controller/GstExcemptionController.java
+++ LIC_FAS_JAVA_SACRAL/src/main/java/com/lic/epgs/gstexcemption/controller/GstExcemptionController.java
@@ -5,21 +5,44 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.lic.epgs.gstexcemption.dto.GstExcemptionDto;
-import com.lic.epgs.gstexcemption.dto.GstResponseDto;
-import com.lic.epgs.gstexcemption.service.GstExcemptionService;
+import com.lic.epgs.gstexcemption.model.GstExcemption;
+import com.lic.epgs.gstexcemption.service.SendToApprove5_GstExcemptionService;
 
 @RestController
+@RequestMapping("/gstexcemption")
 public class GstExcemptionController {
-
-    @Autowired
-    private GstExcemptionService gstExcemptionService;
-
-    @GetMapping("/gstExcemption/{gstNo}/{unitCode}/{proposalNo}")
-    public GstResponseDto<List<GstExcemptionDto>> searchforExisting(@PathVariable String gstNo, @PathVariable String unitCode, @PathVariable String proposalNo) {
-        return gstExcemptionService.searchforExisting(gstNo, unitCode, proposalNo);
-    }
+	
+	@Autowired
+	private SendToApprove5_GstExcemptionService sendToApprove5_GstExcemptionService;
+	
+	@GetMapping("/{gstExcemptionId}")
+	public GstExcemption getGstExcemptionById(@PathVariable String gstExcemptionId) {
+		return sendToApprove5_GstExcemptionService.findByGstExcemptionId(gstExcemptionId);
+	}
+	
+	@PostMapping("/save")
+	public void saveGstExcemption(@RequestBody GstExcemption gstExcemption) {
+		sendToApprove5_GstExcemptionService.save(gstExcemption);
+	}
+	
+	@PostMapping("/update")
+	public void updateGstExcemption(@RequestBody GstExcemption gstExcemption) {
+		sendToApprove5_GstExcemptionService.update(gstExcemption);
+	}
+	
+	@PostMapping("/create/gstExcemption")
+	public void createNewGstNotesAndDocDetails(@RequestBody GstExcemption gstExcemption) {
+		sendToApprove5_GstExcemptionService.createNewGstNotesAndDocDetails(gstExcemption);
+	}
+	
+	@GetMapping("/get/gstExcemptionId")
+	public String getNewGstExcemptionId() {
+		return sendToApprove5_GstExcemptionService.getNewGstExcemptionId();
+	}
 
 }
