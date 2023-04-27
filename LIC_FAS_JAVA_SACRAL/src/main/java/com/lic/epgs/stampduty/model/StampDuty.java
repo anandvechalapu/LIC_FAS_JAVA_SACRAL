3c9f91da2692aa0b6e0e0472381cd59686680b1e
anandvechalapu <anand.vechalapu@gmail.com>
2023-04-27 13:01:47 +0530

    Updated

--- LIC_FAS_JAVA_SACRAL/src/main/java/com/lic/epgs/stampduty/model/StampDuty.java
+++ LIC_FAS_JAVA_SACRAL/src/main/java/com/lic/epgs/stampduty/model/StampDuty.java
@@ -1,38 +1,18 @@
 package com.lic.epgs.stampduty.model;
 
 import javax.persistence.Entity;
-import javax.persistence.Table;
-import javax.persistence.Id;
-import javax.persistence.Column;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
-import java.util.Date;
+import javax.persistence.Id;
 
 @Entity
-@Table(name = "stamp_duty")
 public class StampDuty {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
-
-    @Column(name = "role_type")
-    private String roleType;
-
-    @Column(name = "unit_office_code")
+    private String role;
     private String unitOfficeCode;
-
-    @Column(name = "challan_grn_number")
-    private String challanGRNNumber;
-
-    @Column(name = "challan_grn_date")
-    private String challanGRNDate;
-
-    @Column(name = "stamp_duty_status")
-    private String stampDutyStatus;
-
-    @Column(name = "created_date")
-    private Date createdDate;
 
     public Long getId() {
         return id;
@@ -42,12 +22,12 @@ public class StampDuty {
         this.id = id;
     }
 
-    public String getRoleType() {
-        return roleType;
+    public String getRole() {
+        return role;
     }
 
-    public void setRoleType(String roleType) {
-        this.roleType = roleType;
+    public void setRole(String role) {
+        this.role = role;
     }
 
     public String getUnitOfficeCode() {
@@ -56,37 +36,5 @@ public class StampDuty {
 
     public void setUnitOfficeCode(String unitOfficeCode) {
         this.unitOfficeCode = unitOfficeCode;
-    }
-
-    public String getChallanGRNNumber() {
-        return challanGRNNumber;
-    }
-
-    public void setChallanGRNNumber(String challanGRNNumber) {
-        this.challanGRNNumber = challanGRNNumber;
-    }
-
-    public String getChallanGRNDate() {
-        return challanGRNDate;
-    }
-
-    public void setChallanGRNDate(String challanGRNDate) {
-        this.challanGRNDate = challanGRNDate;
-    }
-
-    public String getStampDutyStatus() {
-        return stampDutyStatus;
-    }
-
-    public void setStampDutyStatus(String stampDutyStatus) {
-        this.stampDutyStatus = stampDutyStatus;
-    }
-
-    public Date getCreatedDate() {
-        return createdDate;
-    }
-
-    public void setCreatedDate(Date createdDate) {
-        this.createdDate = createdDate;
     }
 }
