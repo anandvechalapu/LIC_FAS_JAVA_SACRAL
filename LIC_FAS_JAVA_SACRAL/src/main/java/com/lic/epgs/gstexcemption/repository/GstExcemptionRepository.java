bd1dc58bac7290d73eaa815ca9c269c77d8a2181
anandvechalapu <anand.vechalapu@gmail.com>
2023-04-27 15:58:56 +0530

    Updated

--- LIC_FAS_JAVA_SACRAL/src/main/java/com/lic/epgs/gstexcemption/repository/GstExcemptionRepository.java
+++ LIC_FAS_JAVA_SACRAL/src/main/java/com/lic/epgs/gstexcemption/repository/GstExcemptionRepository.java
@@ -3,11 +3,12 @@ package com.lic.epgs.gstexcemption.repository;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-import com.lic.epgs.gstexcemption.model.GstExcemption;
+import com.lic.epgs.gstexcemption.dto.GstExcemptionDto;
+import com.lic.epgs.gstexcemption.dto.GstResponseDto;
 
 @Repository
-public interface GstExcemptionRepository extends JpaRepository<GstExcemption, Long> {
-	List<GstExcemption> findByLoginUserAndUnitCode(String loginUser, String unitCode);
-	List<GstExcemption> findByGstExcemptionStatusAndUnitCode(String gstExcemptionStatus, String unitCode);
-	List<GstExcemption> findByUnitCode(String unitCode);
+public interface GstExcemptionRepository extends JpaRepository<GstExcemptionDto, Integer> {
+
+    GstResponseDto<List<GstExcemptionDto>> searchforExisting(String gstNo, String unitCode, String proposalNo);
+
 }
