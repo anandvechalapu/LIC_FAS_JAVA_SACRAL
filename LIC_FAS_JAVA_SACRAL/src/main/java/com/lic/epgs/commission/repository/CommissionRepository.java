ee884f73592230b8bc471dd4e7b114ed367538e4
anandvechalapu <anand.vechalapu@gmail.com>
2023-04-27 11:42:42 +0530

    Updated

--- LIC_FAS_JAVA_SACRAL/src/main/java/com/lic/epgs/commission/repository/CommissionRepository.java
+++ LIC_FAS_JAVA_SACRAL/src/main/java/com/lic/epgs/commission/repository/CommissionRepository.java
@@ -1,16 +1,24 @@
-package com.lic.epgs.commission.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import com.lic.epgs.commission.model.Commission;
-
+@Repository
 public interface CommissionRepository extends JpaRepository<Commission, Long> {
 
-    Commission findByCommissionIdAndCommissionDetailsId(String commissionId, Long commissionDetailsId);
+    @Query("SELECT c FROM Commission c WHERE c.commissionDetailsId= :commissionDetailsId")
+    Optional<Commission> findByCommissionDetailsId(@Param("commissionDetailsId") Long commissionDetailsId);
 
-    Commission save(Commission commission);
+    @Modifying
+    @Query("UPDATE Commission c SET c.commissionStatus = 'draft', c.workflowStatus = 'draft' WHERE c.commissionId = :commissionId")
+    int updateCommissionStatusAndWorkflowStatusByCommissionId(@Param("commissionId") Long commissionId);
 
-    Commission update(Commission commission);
+    @Transactional
+    @Modifying
+    @Query("DELETE FROM Commission c WHERE c.commissionDetailsId is null")
+    int deleteByCommissionDetailsIdIsNull();
 
-    void addCommissionNotesAndQuestions(Commission commission);
-    
+    @Query("SELECT cd FROM CommissionDetails cd WHERE cd.commissionDetailsId= :commissionDetailsId")
+    Optional<CommissionDetails> findCommissionDetailsByCommissionDetailsId(@Param("commissionDetailsId") Long commissionDetailsId);
+
+    @Query("SELECT cdt FROM CommissionQuestionDetailsTemp cdt WHERE cdt.commissionDetailsId= :commissionDetailsId")
+    List<CommissionQuestionDetailsTemp> findCommissionQuestionDetailsTempByCommissionDetailsId(@Param("commissionDetailsId") Long commissionDetailsId);
+
+    @Query("SELECT cnt FROM CommissionNotesTemp cnt WHERE cnt.commissionDetailsId= :commissionDetailsId")
+    List<CommissionNotesTemp> findCommissionNotesTempByCommissionDetailsId(@Param("commissionDetailsId") Long commissionDetailsId);
 }
