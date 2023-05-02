fa3366fd56cf80a59b060470601e838ec532b127
anandvechalapu <anand.vechalapu@gmail.com>
2023-05-02 12:13:41 +0530

    Updated

--- LIC_FAS_JAVA_SACRAL/src/main/java/com/lic/epgs/customer/customercheckercontroller/model/CustomerCheckerController.java
+++ LIC_FAS_JAVA_SACRAL/src/main/java/com/lic/epgs/customer/customercheckercontroller/model/CustomerCheckerController.java
@@ -1,62 +1,22 @@
 package com.lic.epgs.customer.customercheckercontroller.model;
 
-import java.util.UUID;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
 
+@Entity
 public class CustomerCheckerController {
+ 
+    @Id
+    @GeneratedValue(strategy=GenerationType.IDENTITY)
+    private Long customerId;
 
-  private UUID id;
-  private String address;
-  private String contactDetails;
-  private String migrationData;
-  private boolean approved;
-  private String status;
+    public Long getCustomerId() {
+        return customerId;
+    }
 
-  public UUID getId() {
-    return id;
-  }
-
-  public void setId(UUID id) {
-    this.id = id;
-  }
-
-  public String getAddress() {
-    return address;
-  }
-
-  public void setAddress(String address) {
-    this.address = address;
-  }
-
-  public String getContactDetails() {
-    return contactDetails;
-  }
-
-  public void setContactDetails(String contactDetails) {
-    this.contactDetails = contactDetails;
-  }
-
-  public String getMigrationData() {
-    return migrationData;
-  }
-
-  public void setMigrationData(String migrationData) {
-    this.migrationData = migrationData;
-  }
-
-  public boolean isApproved() {
-    return approved;
-  }
-
-  public void setApproved(boolean approved) {
-    this.approved = approved;
-  }
-
-  public String getStatus() {
-    return status;
-  }
-
-  public void setStatus(String status) {
-    this.status = status;
-  }
-
+    public void setCustomerId(Long customerId) {
+        this.customerId = customerId;
+    }
 }
