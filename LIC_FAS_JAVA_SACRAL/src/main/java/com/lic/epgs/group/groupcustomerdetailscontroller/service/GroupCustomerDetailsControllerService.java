cc18fb8edb01681d49a462cdcff89d55efd90e08
anandvechalapu <anand.vechalapu@gmail.com>
2023-05-02 18:56:15 +0530

    Updated

--- LIC_FAS_JAVA_SACRAL/src/main/java/com/lic/epgs/group/groupcustomerdetailscontroller/service/GroupCustomerDetailsControllerService.java
+++ LIC_FAS_JAVA_SACRAL/src/main/java/com/lic/epgs/group/groupcustomerdetailscontroller/service/GroupCustomerDetailsControllerService.java
@@ -1,38 +1,19 @@
 package com.lic.epgs.group.groupcustomerdetailscontroller.service;
 
-import java.util.List;
-
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-import org.springframework.transaction.annotation.Transactional;
 
+import com.lic.epgs.group.groupcustomerdetailscontroller.model.GroupCustomerDetailsController;
 import com.lic.epgs.group.groupcustomerdetailscontroller.repository.GroupCustomerDetailsControllerRepository;
-import com.lic.epgs.model.Customer;
-import com.lic.epgs.model.Trust;
 
 @Service
-@Transactional
 public class GroupCustomerDetailsControllerService {
-
-	private final GroupCustomerDetailsControllerRepository groupCustomerDetailsControllerRepository;
-
-	public GroupCustomerDetailsControllerService(GroupCustomerDetailsControllerRepository groupCustomerDetailsControllerRepository) {
-		this.groupCustomerDetailsControllerRepository = groupCustomerDetailsControllerRepository;
-	}
-
-	public List<Customer> searchMemberCustomerDetails(String customerCode, String customerName, String trustName) {
-		return groupCustomerDetailsControllerRepository.searchMemberCustomerDetails(customerCode, customerName, trustName);
-	}
-
-	public List<Customer> getActiveMemberCustomers() {
-		return groupCustomerDetailsControllerRepository.getActiveMemberCustomers();
-	}
-
-	public List<Customer> getCustomerBasicDetails(String customerCode, String customerName, String customerStatus) {
-		return groupCustomerDetailsControllerRepository.getCustomerBasicDetails(customerCode, customerName, customerStatus);
-	}
-
-	public List<Trust> getTrustDetails(String trustName, String trustId) {
-		return groupCustomerDetailsControllerRepository.getTrustDetails(trustName, trustId);
+	
+	@Autowired
+	GroupCustomerDetailsControllerRepository groupCustomerDetailsControllerRepository;
+	
+	public GroupCustomerDetailsController updateGroupStatus(Long id, boolean status, String modifiedBy, String modifiedOn) {
+		return groupCustomerDetailsControllerRepository.updateGroupStatus(id, status, modifiedBy, modifiedOn);
 	}
 
 }
