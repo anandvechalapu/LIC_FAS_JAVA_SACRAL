1908c6467fde27b85fe33346c5c53fcab3298a84
anandvechalapu <anand.vechalapu@gmail.com>
2023-05-02 17:22:00 +0530

    Updated

--- LIC_FAS_JAVA_SACRAL/src/main/java/com/lic/epgs/group/groupcustomerdetailscontroller/model/GroupCustomerDetails.java
+++ LIC_FAS_JAVA_SACRAL/src/main/java/com/lic/epgs/group/groupcustomerdetailscontroller/model/GroupCustomerDetails.java
@@ -5,45 +5,103 @@ import javax.persistence.Id;
 import javax.persistence.Table;
 
 @Entity
-@Table(name="group_customer_details")
+@Table(name = "GroupCustomerDetails")
 public class GroupCustomerDetails {
-
-    @Id
-    private Long groupCustomerId;
-    private String unitCode;
-    private String name;
-    private String description;
-
-    public Long getGroupCustomerId() {
-        return groupCustomerId;
-    }
-
-    public void setGroupCustomerId(Long groupCustomerId) {
-        this.groupCustomerId = groupCustomerId;
-    }
-
-    public String getUnitCode() {
-        return unitCode;
-    }
-
-    public void setUnitCode(String unitCode) {
-        this.unitCode = unitCode;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public String getDescription() {
-        return description;
-    }
-
-    public void setDescription(String description) {
-        this.description = description;
-    }
+	
+	@Id
+	private Long groupCustomerId;
+	
+	private String basicDetailsName;
+	private String type;
+	
+	private String contactDetailsPrimaryContactName;
+	private String contactDetailsPrimaryContactNumber;
+	
+	private String addressDetailsAddressLine;
+	private String addressDetailsCity;
+	
+	private Long groupCustomerDetailsCustomerId;
+	private String groupCustomerDetailsCustomerName;
+	
+	private String notes;
+	
+	private String bankAccountDetailsAccountNumber;
+	private String bankAccountDetailsBankName;
+	
+	public Long getGroupCustomerId() {
+		return groupCustomerId;
+	}
+	public void setGroupCustomerId(Long groupCustomerId) {
+		this.groupCustomerId = groupCustomerId;
+	}
+	public String getBasicDetailsName() {
+		return basicDetailsName;
+	}
+	public void setBasicDetailsName(String basicDetailsName) {
+		this.basicDetailsName = basicDetailsName;
+	}
+	public String getType() {
+		return type;
+	}
+	public void setType(String type) {
+		this.type = type;
+	}
+	public String getContactDetailsPrimaryContactName() {
+		return contactDetailsPrimaryContactName;
+	}
+	public void setContactDetailsPrimaryContactName(String contactDetailsPrimaryContactName) {
+		this.contactDetailsPrimaryContactName = contactDetailsPrimaryContactName;
+	}
+	public String getContactDetailsPrimaryContactNumber() {
+		return contactDetailsPrimaryContactNumber;
+	}
+	public void setContactDetailsPrimaryContactNumber(String contactDetailsPrimaryContactNumber) {
+		this.contactDetailsPrimaryContactNumber = contactDetailsPrimaryContactNumber;
+	}
+	public String getAddressDetailsAddressLine() {
+		return addressDetailsAddressLine;
+	}
+	public void setAddressDetailsAddressLine(String addressDetailsAddressLine) {
+		this.addressDetailsAddressLine = addressDetailsAddressLine;
+	}
+	public String getAddressDetailsCity() {
+		return addressDetailsCity;
+	}
+	public void setAddressDetailsCity(String addressDetailsCity) {
+		this.addressDetailsCity = addressDetailsCity;
+	}
+	public Long getGroupCustomerDetailsCustomerId() {
+		return groupCustomerDetailsCustomerId;
+	}
+	public void setGroupCustomerDetailsCustomerId(Long groupCustomerDetailsCustomerId) {
+		this.groupCustomerDetailsCustomerId = groupCustomerDetailsCustomerId;
+	}
+	public String getGroupCustomerDetailsCustomerName() {
+		return groupCustomerDetailsCustomerName;
+	}
+	public void setGroupCustomerDetailsCustomerName(String groupCustomerDetailsCustomerName) {
+		this.groupCustomerDetailsCustomerName = groupCustomerDetailsCustomerName;
+	}
+	public String getNotes() {
+		return notes;
+	}
+	public void setNotes(String notes) {
+		this.notes = notes;
+	}
+	public String getBankAccountDetailsAccountNumber() {
+		return bankAccountDetailsAccountNumber;
+	}
+	public void setBankAccountDetailsAccountNumber(String bankAccountDetailsAccountNumber) {
+		this.bankAccountDetailsAccountNumber = bankAccountDetailsAccountNumber;
+	}
+	public String getBankAccountDetailsBankName() {
+		return bankAccountDetailsBankName;
+	}
+	public void setBankAccountDetailsBankName(String bankAccountDetailsBankName) {
+		this.bankAccountDetailsBankName = bankAccountDetailsBankName;
+	}
+	
+	
+	
 
 }
