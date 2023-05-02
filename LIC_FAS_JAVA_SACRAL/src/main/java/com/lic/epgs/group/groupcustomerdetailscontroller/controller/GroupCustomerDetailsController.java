e4e9fba79dd3e138860f466bb4259f293b418f2a
anandvechalapu <anand.vechalapu@gmail.com>
2023-05-02 16:53:10 +0530

    Updated

--- LIC_FAS_JAVA_SACRAL/src/main/java/com/lic/epgs/group/groupcustomerdetailscontroller/controller/GroupCustomerDetailsController.java
+++ LIC_FAS_JAVA_SACRAL/src/main/java/com/lic/epgs/group/groupcustomerdetailscontroller/controller/GroupCustomerDetailsController.java
@@ -1,24 +1,42 @@
 package com.lic.epgs.group.groupcustomerdetailscontroller.controller;
 
-import com.lic.epgs.group.groupcustomerdetailscontroller.dto.CommonResponseDto;
-import com.lic.epgs.group.groupcustomerdetailscontroller.service.GroupCustomerDetailsControllerService;
+import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-
-import java.util.List;
+import com.lic.epgs.group.groupcustomerdetailscontroller.model.GroupCustomerDetails;
+import com.lic.epgs.group.groupcustomerdetailscontroller.service.GroupCustomerDetailsService;
 
 @RestController
+@RequestMapping("/groupcustomerdetails")
 public class GroupCustomerDetailsController {
-
+    
     @Autowired
-    private GroupCustomerDetailsControllerService groupCustomerDetailsControllerService;
+    private GroupCustomerDetailsService groupCustomerDetailsService;
 
-    @GetMapping("/getGroupBasicDetails/{groupId}")
-    public List<CommonResponseDto> getGroupBasicDetails_GROUP_CUSTOMER_DETAILS_CONTROLLER(@PathVariable("groupId") Long groupId, @RequestParam("pageCount") int pageCount, @RequestParam("limit") int limit){
-        return groupCustomerDetailsControllerService.getGroupBasicDetails_GROUP_CUSTOMER_DETAILS_CONTROLLER(groupId, pageCount, limit);
+    @PostMapping
+    public GroupCustomerDetails save(@RequestBody GroupCustomerDetails groupCustomerDetails) {
+        return groupCustomerDetailsService.save(groupCustomerDetails);
+    }
+
+    @GetMapping("/{groupCustomerId}/{unitCode}")
+    public GroupCustomerDetails getGroupDetailsMakerByGroupCustomerIdAndUnitCode(@PathVariable Long groupCustomerId,
+            @PathVariable String unitCode) {
+        return groupCustomerDetailsService.getGroupDetailsMakerByGroupCustomerIdAndUnitCode(groupCustomerId, unitCode);
+    }
+
+    @GetMapping("/{groupCustomerId}")
+    public GroupCustomerDetails getGroupDetailsMakerByGroupCustomerId(@PathVariable Long groupCustomerId) {
+        return groupCustomerDetailsService.getGroupDetailsMakerByGroupCustomerId(groupCustomerId);
+    }
+
+    @GetMapping
+    public List<GroupCustomerDetails> findAll() {
+        return groupCustomerDetailsService.findAll();
     }
 
 }
