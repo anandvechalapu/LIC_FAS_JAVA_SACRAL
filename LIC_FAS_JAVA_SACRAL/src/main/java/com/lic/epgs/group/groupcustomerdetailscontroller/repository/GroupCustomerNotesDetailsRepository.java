e249c050e8d384bd6594c862bba7952363904f56
anandvechalapu <anand.vechalapu@gmail.com>
2023-05-02 17:52:20 +0530

    Updated

--- LIC_FAS_JAVA_SACRAL/src/main/java/com/lic/epgs/group/groupcustomerdetailscontroller/repository/GroupCustomerNotesDetailsRepository.java
+++ LIC_FAS_JAVA_SACRAL/src/main/java/com/lic/epgs/group/groupcustomerdetailscontroller/repository/GroupCustomerNotesDetailsRepository.java
@@ -1,13 +1,13 @@
 package com.lic.epgs.group.groupcustomerdetailscontroller.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
+import org.springframework.stereotype.Repository;
 
 import com.lic.epgs.group.groupcustomerdetailscontroller.model.GroupCustomerNotesDetails;
 
+@Repository
 public interface GroupCustomerNotesDetailsRepository extends JpaRepository<GroupCustomerNotesDetails, Long> {
 
-    @Query("SELECT gcnd FROM GroupCustomerNotesDetails gcnd WHERE gcnd.groupCustomerId = :groupCustomerId AND gcnd.date BETWEEN :startDate AND :endDate")
-    public GroupCustomerNotesDetails getGroupCustomerNotesDetailsByGroupCustomerIdAndDateRange(Long groupCustomerId, String startDate, String endDate);
+	public GroupCustomerNotesDetails findByGroupCustomerIdAndDateRange(Long groupCustomerId, String startDate, String endDate);
 
 }
