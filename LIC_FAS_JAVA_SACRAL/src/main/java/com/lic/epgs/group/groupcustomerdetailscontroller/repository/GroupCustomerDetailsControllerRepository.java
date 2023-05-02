448915aa28c8ad29a22373ebe9ccfc981f70f50b
anandvechalapu <anand.vechalapu@gmail.com>
2023-05-02 18:55:39 +0530

    Updated

--- LIC_FAS_JAVA_SACRAL/src/main/java/com/lic/epgs/group/groupcustomerdetailscontroller/repository/GroupCustomerDetailsControllerRepository.java
+++ LIC_FAS_JAVA_SACRAL/src/main/java/com/lic/epgs/group/groupcustomerdetailscontroller/repository/GroupCustomerDetailsControllerRepository.java
@@ -1,11 +1,13 @@
+package com.lic.epgs.group.groupcustomerdetailscontroller.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.lic.epgs.group.groupcustomerdetailscontroller.model.GroupCustomerDetailsController;
+
 @Repository
-public interface GroupCustomerDetailsControllerRepository { 
- 
-    public List<Customer> searchMemberCustomerDetails(String customerCode, String customerName, String trustName);
- 
-    public List<Customer> getActiveMemberCustomers();
- 
-    public List<Customer> getCustomerBasicDetails(String customerCode, String customerName, String customerStatus);
- 
-    public List<Trust> getTrustDetails(String trustName, String trustId);
+public interface GroupCustomerDetailsControllerRepository extends JpaRepository<GroupCustomerDetailsController, Long> {
+
+    public GroupCustomerDetailsController updateGroupStatus(Long id, boolean status, String modifiedBy, String modifiedOn);
+
 }
