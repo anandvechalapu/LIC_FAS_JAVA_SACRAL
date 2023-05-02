7f8633bd62fe612ed1d412a7b623bfc21c618471
anandvechalapu <anand.vechalapu@gmail.com>
2023-05-02 15:43:02 +0530

    Updated

--- LIC_FAS_JAVA_SACRAL/src/main/java/com/lic/epgs/group/groupcustomercheckercontroller/repository/GroupCustomerCheckerRepository.java
+++ LIC_FAS_JAVA_SACRAL/src/main/java/com/lic/epgs/group/groupcustomercheckercontroller/repository/GroupCustomerCheckerRepository.java
@@ -1,17 +1,33 @@
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-import com.lic.epgs.group.groupcustomercheckercontroller.dto.GroupCustomerBasicDetailsDto;
-import com.lic.epgs.group.groupcustomercheckercontroller.entity.AddressDetailsEntity;
-import com.lic.epgs.group.groupcustomercheckercontroller.entity.GroupCustomerBasicDetailsEntity;
-import com.lic.epgs.group.groupcustomercheckercontroller.entity.GroupCustomerBasicDetailsTempEntity;
+must be tested manually.
 
 @Repository
-public interface GroupCustomerCheckerRepository extends JpaRepository<GroupCustomerBasicDetailsEntity, Long> {
- 
-    public GroupCustomerBasicDetailsTempEntity findByGroupCustomerIdAndIsActiveTrue(String groupCustomerId);
+public interface GroupCustomerCheckerRepository {
+    
+    // Method to reject the group customer
+    public void rejectGroupCustomer(Integer groupCustomerId, String rejectionRemarks, String modifiedBy);
+    
+    // Method to update the isActive field in GroupCustomerBasicDetailsTempEntity table
+    public void updateIsActiveInGroupCustomerBasicDetailsTempEntity(Integer groupCustomerId, Boolean isActive);
+    
+    // Method to add a new entry in GroupCustomerBasicDetailsTempEntity table
+    public void addNewEntryInGroupCustomerBasicDetailsTempEntity(Integer groupCustomerId, String rejectionRemarks, String modifiedBy);
+    
+    // Method to update the group status, workflow status, rejection remarks, modified by, and modified on fields in GroupCustomerBasicDetailsTempEntity table
+    public void updateGroupCustomerBasicDetailsTempEntity(Integer groupCustomerId, String groupStatus, String workflowStatus, String rejectionRemarks, String modifiedBy);
+    
+    // Method to update the isActive field in the new entry of GroupCustomerBasicDetailsTempEntity table
+    public void updateIsActiveInNewEntryGroupCustomerBasicDetailsTempEntity(Integer groupCustomerId, Boolean isActive);
+    
+    // Method to update the GroupCustomerBasicDetailsEntity table
+    public void updateGroupCustomerBasicDetailsEntity(String groupCode, String groupStatus, String workflowStatus);
+    
+    // Method to add a new entry in AddressDetailsEntity table
+    public void addNewEntryInAddressDetailsEntity(Integer tempAddressId, Integer addressId);
+    
+    // Method to add a new entry in ContactDetailEntity table
+    public void addNewEntryInContactDetailEntity(Integer tempContactPersonId, Integer contactPersonId);
+    
+    // Method to add a new entry in CustomerNotesEntity table
+    public void addNewEntryInCustomerNotesEntity(Integer tempNoteId, Integer noteId);
 
-    public AddressDetailsEntity findByGroupCustomerIdAndIsActiveTrue(String newEntityGroupCustomerId);
-
-    public GroupCustomerBasicDetailsEntity save(GroupCustomerBasicDetailsEntity groupCustomerBasicDetailsEntity);
 }
