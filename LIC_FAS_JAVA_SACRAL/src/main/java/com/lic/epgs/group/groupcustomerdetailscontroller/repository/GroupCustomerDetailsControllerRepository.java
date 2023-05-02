b01d8327f3e70db90f340a081e36aa5340337f37
anandvechalapu <anand.vechalapu@gmail.com>
2023-05-02 17:12:36 +0530

    Updated

--- LIC_FAS_JAVA_SACRAL/src/main/java/com/lic/epgs/group/groupcustomerdetailscontroller/repository/GroupCustomerDetailsControllerRepository.java
+++ LIC_FAS_JAVA_SACRAL/src/main/java/com/lic/epgs/group/groupcustomerdetailscontroller/repository/GroupCustomerDetailsControllerRepository.java
@@ -1,12 +1,6 @@
-package com.lic.epgs.group.groupcustomerdetailscontroller.repository;
-
-import com.lic.epgs.group.groupcustomerdetailscontroller.dto.CommonResponseDto;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
-
-public interface GroupCustomerDetailsControllerRepository extends JpaRepository<CommonResponseDto, Long> {
-
-    @Query("SELECT g.groupId, g.groupName, g.trustId, t.trustName, t.trustType, c.customerId, c.name, c.address FROM Group g LEFT JOIN Trust t ON g.trustId = t.trustId LEFT JOIN Customer c ON g.customerId = c.customerId WHERE g.groupId = ?1")
-    CommonResponseDto getGroupBasicDetails_GROUP_CUSTOMER_DETAILS_CONTROLLER(Long groupId, int pageCount, int limit);
-
+@Repository
+public interface GroupCustomerDetailsControllerRepository extends JpaRepository<GroupCustomerDetailsController, Long> { 
+ 
+    List<GroupCustomerDetailsController> getInProgressCommonSearch_GROUP_CUSTOMER_DETAILS_CONTROLLER(String groupName, String groupCode, String groupStatus, String workflowStatus,
+                                                                                                    Integer pageCount, Integer limit, Date startDate, Date endDate);
 }
