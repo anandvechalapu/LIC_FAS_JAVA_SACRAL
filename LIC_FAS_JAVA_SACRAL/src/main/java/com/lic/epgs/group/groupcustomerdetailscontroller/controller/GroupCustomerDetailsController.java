f90a67d772c0fd36975fe62b78d4b51aad38f1bb
anandvechalapu <anand.vechalapu@gmail.com>
2023-05-02 18:49:07 +0530

    Updated

--- LIC_FAS_JAVA_SACRAL/src/main/java/com/lic/epgs/group/groupcustomerdetailscontroller/controller/GroupCustomerDetailsController.java
+++ LIC_FAS_JAVA_SACRAL/src/main/java/com/lic/epgs/group/groupcustomerdetailscontroller/controller/GroupCustomerDetailsController.java
@@ -1,48 +1,46 @@
 package com.lic.epgs.group.groupcustomerdetailscontroller.controller;
 
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
+import java.util.List;
+
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.lic.epgs.group.groupcustomerdetailscontroller.model.GroupCustomerDetailsController;
+import com.lic.epgs.model.Customer;
+import com.lic.epgs.model.Trust;
 import com.lic.epgs.group.groupcustomerdetailscontroller.service.GroupCustomerDetailsControllerService;
 
 @RestController
-@RequestMapping("/groupcustomerdetails")
 public class GroupCustomerDetailsController {
-	
-	@Autowired
-	GroupCustomerDetailsControllerService groupCustomerDetailsControllerService;
-	
-	@GetMapping("/findbygroupname")
-	public ResponseEntity<Object> findByGroupName(@RequestParam(value="groupName") String groupName) {
-		GroupCustomerDetailsController groupCustomerDetailsController = groupCustomerDetailsControllerService.findByGroupName(groupName).orElse(null);
-		if(groupCustomerDetailsController != null) {
-			return new ResponseEntity<>(groupCustomerDetailsController, HttpStatus.OK);
-		}
-		return new ResponseEntity<>("No such group name", HttpStatus.NOT_FOUND);
+
+	private final GroupCustomerDetailsControllerService groupCustomerDetailsControllerService;
+
+	public GroupCustomerDetailsController(GroupCustomerDetailsControllerService groupCustomerDetailsControllerService) {
+		this.groupCustomerDetailsControllerService = groupCustomerDetailsControllerService;
 	}
-	
-	@PostMapping("/save")
-	public ResponseEntity<String> saveGroupCustomerBasicDetails(@RequestBody GroupCustomerDetailsController groupCustomerDetailsController) {
-		groupCustomerDetailsControllerService.saveGroupCustomerBasicDetails(groupCustomerDetailsController.getGroupName(), 
-				groupCustomerDetailsController.getCustomerName(), groupCustomerDetailsController.getCustomerNumber(), 
-				groupCustomerDetailsController.getId());
-		return new ResponseEntity<>("Group Customer Details Saved Successfully", HttpStatus.CREATED);
+
+	@GetMapping("/customer/search")
+	public List<Customer> searchMemberCustomerDetails(@RequestParam String customerCode,
+			@RequestParam String customerName, @RequestParam String trustName) {
+		return groupCustomerDetailsControllerService.searchMemberCustomerDetails(customerCode, customerName,
+				trustName);
 	}
-	
-	@PostMapping("/update")
-	public ResponseEntity<String> updateGroupCustomerRelatedDetails(@RequestBody GroupCustomerDetailsController groupCustomerDetailsController) {
-		groupCustomerDetailsControllerService.updateGroupCustomerRelatedDetails(groupCustomerDetailsController.getGroupName(), 
-				groupCustomerDetailsController.getCustomerName(), groupCustomerDetailsController.getCustomerNumber(), 
-				groupCustomerDetailsController.getId());
-		return new ResponseEntity<>("Group Customer Details Updated Successfully", HttpStatus.OK);
+
+	@GetMapping("/customer/active")
+	public List<Customer> getActiveMemberCustomers() {
+		return groupCustomerDetailsControllerService.getActiveMemberCustomers();
+	}
+
+	@GetMapping("/customer/basic")
+	public List<Customer> getCustomerBasicDetails(@RequestParam String customerCode, @RequestParam String customerName,
+			@RequestParam String customerStatus) {
+		return groupCustomerDetailsControllerService.getCustomerBasicDetails(customerCode, customerName,
+				customerStatus);
+	}
+
+	@GetMapping("/trust/details")
+	public List<Trust> getTrustDetails(@RequestParam String trustName, @RequestParam String trustId) {
+		return groupCustomerDetailsControllerService.getTrustDetails(trustName, trustId);
 	}
 
 }
