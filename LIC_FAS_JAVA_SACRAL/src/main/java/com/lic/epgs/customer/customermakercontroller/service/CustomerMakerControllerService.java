c960cef0d3c557bcdc98cd476bfc50a117fec816
anandvechalapu <anand.vechalapu@gmail.com>
2023-05-02 15:20:57 +0530

    initial commit

---
+++ LIC_FAS_JAVA_SACRAL/src/main/java/com/lic/epgs/customer/customermakercontroller/service/CustomerMakerControllerService.java
@@ -0,0 +1,13 @@
+package com.lic.epgs.customer.customermakercontroller.service;
+
+import com.lic.epgs.customer.customermakercontroller.dto.CommonResponseDto;
+import org.springframework.stereotype.Service;
+
+@Service
+public class CustomerMakerControllerService {
+
+    public CommonResponseDto searchCustomerdetailsbyName(String customerName) {
+        // Implement your business logic here
+        return new CommonResponseDto();
+    }
+}
