f19114225b646cb30a557910c50e99f4b53e53ec
anandvechalapu <anand.vechalapu@gmail.com>
2023-05-02 14:19:51 +0530

    Updated

--- LIC_FAS_JAVA_SACRAL/src/main/java/com/lic/epgs/customer/customermakercontroller/model/CommonResponseDto.java
+++ LIC_FAS_JAVA_SACRAL/src/main/java/com/lic/epgs/customer/customermakercontroller/model/CommonResponseDto.java
@@ -1,60 +1,31 @@
 package com.lic.epgs.customer.customermakercontroller.model;
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
-
-@Entity
 public class CommonResponseDto {
-
-    @Id
-    @GeneratedValue(strategy = GenerationType.AUTO)
-    private Long id;
-
+    
     private String status;
     private String message;
-    private Object data;
-
+    
     public CommonResponseDto() {
     }
-
-    public CommonResponseDto(String status, String message, Object data) {
+    
+    public CommonResponseDto(String status, String message) {
         this.status = status;
         this.message = message;
-        this.data = data;
     }
-
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
+    
     public String getStatus() {
         return status;
     }
-
+    
     public void setStatus(String status) {
         this.status = status;
     }
-
+    
     public String getMessage() {
         return message;
     }
-
+    
     public void setMessage(String message) {
         this.message = message;
     }
-
-    public Object getData() {
-        return data;
-    }
-
-    public void setData(Object data) {
-        this.data = data;
-    }
-
 }
