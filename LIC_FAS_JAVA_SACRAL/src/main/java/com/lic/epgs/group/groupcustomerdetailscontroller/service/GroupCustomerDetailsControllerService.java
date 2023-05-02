c518d099c6d441a854d1a45fd3bc1e0e1e67044b
anandvechalapu <anand.vechalapu@gmail.com>
2023-05-02 18:48:27 +0530

    Updated

--- LIC_FAS_JAVA_SACRAL/src/main/java/com/lic/epgs/group/groupcustomerdetailscontroller/service/GroupCustomerDetailsControllerService.java
+++ LIC_FAS_JAVA_SACRAL/src/main/java/com/lic/epgs/group/groupcustomerdetailscontroller/service/GroupCustomerDetailsControllerService.java
@@ -1,56 +1,38 @@
-import java.util.Optional;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Modifying;
-import org.springframework.data.jpa.repository.Query;
-import org.springframework.data.repository.query.Param;
-import org.springframework.stereotype.Repository;
-import org.springframework.transaction.annotation.Transactional;
-
-import com.lic.epgs.group.groupcustomerdetailscontroller.model.GroupCustomerDetailsController;
-
-@Repository
-public interface SaveGroupCustomerBasicDetailsRepository extends JpaRepository<GroupCustomerDetailsController, Long> {
-
-	Optional<GroupCustomerDetailsController> findByGroupName(String groupName);
-    
-	@Transactional
-	@Modifying
-	@Query("UPDATE GroupCustomerDetailsController g SET g.groupName = :groupName, g.customerName = :customerName, g.customerNumber = :customerNumber WHERE g.id = :id")
-	void saveGroupCustomerBasicDetails(@Param("groupName") String groupName, @Param("customerName") String customerName, @Param("customerNumber") String customerNumber, @Param("id") Long id);
-	
-	@Transactional
-	@Modifying
-	@Query("UPDATE GroupCustomerDetailsController g SET g.groupName = :groupName, g.customerName = :customerName, g.customerNumber = :customerNumber WHERE g.id = :id")
-	void updateGroupCustomerRelatedDetails(@Param("groupName") String groupName, @Param("customerName") String customerName, @Param("customerNumber") String customerNumber, @Param("id") Long id);
-}
-
 package com.lic.epgs.group.groupcustomerdetailscontroller.service;
 
-import java.util.Optional;
+import java.util.List;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
-import com.lic.epgs.group.groupcustomerdetailscontroller.model.GroupCustomerDetailsController;
-import com.lic.epgs.group.groupcustomerdetailscontroller.repository.SaveGroupCustomerBasicDetailsRepository;
+import com.lic.epgs.group.groupcustomerdetailscontroller.repository.GroupCustomerDetailsControllerRepository;
+import com.lic.epgs.model.Customer;
+import com.lic.epgs.model.Trust;
 
 @Service
+@Transactional
 public class GroupCustomerDetailsControllerService {
-	
-	@Autowired
-	SaveGroupCustomerBasicDetailsRepository saveGroupCustomerBasicDetailsRepository;
-	
-	public Optional<GroupCustomerDetailsController> findByGroupName(String groupName) {
-		return saveGroupCustomerBasicDetailsRepository.findByGroupName(groupName);
+
+	private final GroupCustomerDetailsControllerRepository groupCustomerDetailsControllerRepository;
+
+	public GroupCustomerDetailsControllerService(GroupCustomerDetailsControllerRepository groupCustomerDetailsControllerRepository) {
+		this.groupCustomerDetailsControllerRepository = groupCustomerDetailsControllerRepository;
 	}
-	
-	public void saveGroupCustomerBasicDetails(String groupName, String customerName, String customerNumber, Long id) {
-		saveGroupCustomerBasicDetailsRepository.saveGroupCustomerBasicDetails(groupName, customerName, customerNumber, id);
+
+	public List<Customer> searchMemberCustomerDetails(String customerCode, String customerName, String trustName) {
+		return groupCustomerDetailsControllerRepository.searchMemberCustomerDetails(customerCode, customerName, trustName);
 	}
-	
-	public void updateGroupCustomerRelatedDetails(String groupName, String customerName, String customerNumber, Long id) {
-		saveGroupCustomerBasicDetailsRepository.updateGroupCustomerRelatedDetails(groupName, customerName, customerNumber, id);
+
+	public List<Customer> getActiveMemberCustomers() {
+		return groupCustomerDetailsControllerRepository.getActiveMemberCustomers();
+	}
+
+	public List<Customer> getCustomerBasicDetails(String customerCode, String customerName, String customerStatus) {
+		return groupCustomerDetailsControllerRepository.getCustomerBasicDetails(customerCode, customerName, customerStatus);
+	}
+
+	public List<Trust> getTrustDetails(String trustName, String trustId) {
+		return groupCustomerDetailsControllerRepository.getTrustDetails(trustName, trustId);
 	}
 
 }
