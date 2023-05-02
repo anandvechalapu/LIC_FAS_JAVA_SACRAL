6aabd69d98f31bdb70e5b520687882d71b01af85
anandvechalapu <anand.vechalapu@gmail.com>
2023-05-02 14:38:35 +0530

    initial commit

---
+++ LIC_FAS_JAVA_SACRAL/src/main/java/com/lic/epgs/customer/customermakercontroller/model/CustomerBankAccountDetails.java
@@ -0,0 +1,87 @@
+package com.lic.epgs.customer.customermakercontroller.model;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "customer_bank_account_details")
+public class CustomerBankAccountDetails {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "customer_id")
+    private Long customerId;
+
+    @Column(name = "account_number")
+    private String accountNumber;
+
+    @Column(name = "account_type")
+    private String accountType;
+
+    @Column(name = "bank_name")
+    private String bankName;
+
+    @Column(name = "ifsc_code")
+    private String ifscCode;
+
+    @Column(name = "branch_name")
+    private String branchName;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Long getCustomerId() {
+        return customerId;
+    }
+
+    public void setCustomerId(Long customerId) {
+        this.customerId = customerId;
+    }
+
+    public String getAccountNumber() {
+        return accountNumber;
+    }
+
+    public void setAccountNumber(String accountNumber) {
+        this.accountNumber = accountNumber;
+    }
+
+    public String getAccountType() {
+        return accountType;
+    }
+
+    public void setAccountType(String accountType) {
+        this.accountType = accountType;
+    }
+
+    public String getBankName() {
+        return bankName;
+    }
+
+    public void setBankName(String bankName) {
+        this.bankName = bankName;
+    }
+
+    public String getIfscCode() {
+        return ifscCode;
+    }
+
+    public void setIfscCode(String ifscCode) {
+        this.ifscCode = ifscCode;
+    }
+
+    public String getBranchName() {
+        return branchName;
+    }
+
+    public void setBranchName(String branchName) {
+        this.branchName = branchName;
+    }
+
+}
