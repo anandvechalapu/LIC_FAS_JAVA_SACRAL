c1fdfaaaed937736a7a971d7ad7c7e879771b392
anandvechalapu <anand.vechalapu@gmail.com>
2023-05-02 12:20:33 +0530

    initial commit

---
+++ LIC_FAS_JAVA_SACRAL/src/main/java/com/lic/epgs/customer/customercheckercontroller/repository/SaveCheckerNotes_CustomerCheckerControllerRepository.java
@@ -0,0 +1,9 @@
+package com.lic.epgs.customer.customercheckercontroller.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.lic.epgs.customer.customercheckercontroller.entity.SaveCheckerNotes;
+
+public interface SaveCheckerNotes_CustomerCheckerControllerRepository extends JpaRepository<SaveCheckerNotes, Long>{
+
+}
