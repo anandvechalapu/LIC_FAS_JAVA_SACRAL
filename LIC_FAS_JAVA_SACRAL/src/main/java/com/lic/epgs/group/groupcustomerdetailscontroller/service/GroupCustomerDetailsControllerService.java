c7f75262e6d31de4aaf29cb52050c9ef33751412
anandvechalapu <anand.vechalapu@gmail.com>
2023-05-02 17:13:52 +0530

    Updated

--- LIC_FAS_JAVA_SACRAL/src/main/java/com/lic/epgs/group/groupcustomerdetailscontroller/service/GroupCustomerDetailsControllerService.java
+++ LIC_FAS_JAVA_SACRAL/src/main/java/com/lic/epgs/group/groupcustomerdetailscontroller/service/GroupCustomerDetailsControllerService.java
@@ -1,10 +1,11 @@
 package com.lic.epgs.group.groupcustomerdetailscontroller.service;
 
-import com.lic.epgs.group.groupcustomerdetailscontroller.dto.CommonResponseDto;
+import com.lic.epgs.group.groupcustomerdetailscontroller.model.GroupCustomerDetailsController;
 import com.lic.epgs.group.groupcustomerdetailscontroller.repository.GroupCustomerDetailsControllerRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.Date;
 import java.util.List;
 
 @Service
@@ -13,8 +14,10 @@ public class GroupCustomerDetailsControllerService {
     @Autowired
     private GroupCustomerDetailsControllerRepository groupCustomerDetailsControllerRepository;
 
-    public List<CommonResponseDto> getGroupBasicDetails_GROUP_CUSTOMER_DETAILS_CONTROLLER(Long groupId, int pageCount, int limit){
-        return groupCustomerDetailsControllerRepository.getGroupBasicDetails_GROUP_CUSTOMER_DETAILS_CONTROLLER(groupId, pageCount, limit);
+    public List<GroupCustomerDetailsController> getInProgressCommonSearch_GROUP_CUSTOMER_DETAILS_CONTROLLER(String groupName, String groupCode,
+                                                                                                        String groupStatus, String workflowStatus, Integer pageCount,
+                                                                                                        Integer limit, Date startDate, Date endDate) {
+        return groupCustomerDetailsControllerRepository.getInProgressCommonSearch_GROUP_CUSTOMER_DETAILS_CONTROLLER(groupName, groupCode,
+                groupStatus, workflowStatus, pageCount, limit, startDate, endDate);
     }
-
 }
