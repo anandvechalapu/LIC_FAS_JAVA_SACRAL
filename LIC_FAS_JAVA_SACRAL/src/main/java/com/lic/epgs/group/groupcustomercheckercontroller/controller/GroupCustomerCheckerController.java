771ca349bc270bcccdcc10e07df9ad5da919f85b
anandvechalapu <anand.vechalapu@gmail.com>
2023-05-02 15:51:19 +0530

    Updated

--- LIC_FAS_JAVA_SACRAL/src/main/java/com/lic/epgs/group/groupcustomercheckercontroller/controller/GroupCustomerCheckerController.java
+++ LIC_FAS_JAVA_SACRAL/src/main/java/com/lic/epgs/group/groupcustomercheckercontroller/controller/GroupCustomerCheckerController.java
@@ -1,37 +1,54 @@
 package com.lic.epgs.group.groupcustomercheckercontroller.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.lic.epgs.group.groupcustomercheckercontroller.dto.GroupCustomerBasicDetailsDto;
-import com.lic.epgs.group.groupcustomercheckercontroller.entity.GroupCustomerBasicDetailsEntity;
-import com.lic.epgs.group.groupcustomercheckercontroller.service.GroupCustomerCheckerService;
+import com.lic.epgs.group.groupcustomercheckercontroller.model.GroupCustomerBasicDetailsEntity;
+import com.lic.epgs.group.groupcustomercheckercontroller.model.GroupCustomerBasicDetailsTempEntity;
+import com.lic.epgs.group.groupcustomercheckercontroller.service.RejectGroupService;
 
 @RestController
-@RequestMapping("/groupcustomerchecker")
+@RequestMapping("/groupCustomer")
 public class GroupCustomerCheckerController {
 	
 	@Autowired
-	private GroupCustomerCheckerService groupCustomerCheckerService;
-
-	@GetMapping("/{groupCustomerId}")
-	public GroupCustomerBasicDetailsDto getGroupCustomerDetails(@PathVariable("groupCustomerId") String groupCustomerId){
-		return groupCustomerCheckerService.getGroupCustomerDetails(groupCustomerId);
+	RejectGroupService rejectGroupService;
+	
+	@PostMapping("/rejectGroupCustomerTemp")
+	public void rejectGroupCustomerTemp(@RequestBody Long groupCustomerId) {
+		rejectGroupService.rejectGroupCustomerTemp(groupCustomerId);
 	}
 	
-	@PostMapping
-	public void saveGroupCustomerDetails(@RequestBody GroupCustomerBasicDetailsEntity groupCustomerBasicDetailsEntity){
-		groupCustomerCheckerService.save(groupCustomerBasicDetailsEntity);
+	@PostMapping("/updateRejectionRemarks")
+	public void updateRejectionRemarks(@RequestBody GroupCustomerBasicDetailsTempEntity groupCustomerBasicDetailsTempEntity) {
+		rejectGroupService.updateRejectionRemarks(groupCustomerBasicDetailsTempEntity.getGroupCustomerId(), groupCustomerBasicDetailsTempEntity.getRejectionRemarks(), groupCustomerBasicDetailsTempEntity.getModifiedBy());
 	}
 	
-	@PutMapping
-	public void updateGroupCustomerDetails(@RequestBody GroupCustomerBasicDetailsEntity groupCustomerBasicDetailsEntity){
-		groupCustomerCheckerService.save(groupCustomerBasicDetailsEntity);
+	@PostMapping("/updateGroupStatusForDefunct")
+	public void updateGroupStatusForDefunct(@RequestBody GroupCustomerBasicDetailsEntity groupCustomerBasicDetailsEntity) {
+		rejectGroupService.updateGroupStatusForDefunct(groupCustomerBasicDetailsEntity.getGroupCode(), groupCustomerBasicDetailsEntity.getGroupStatus(), groupCustomerBasicDetailsEntity.getWorkflowStatus());
+	}
+	
+	@PostMapping("/updateGroupStatusForReopen")
+	public void updateGroupStatusForReopen(@RequestBody GroupCustomerBasicDetailsEntity groupCustomerBasicDetailsEntity) {
+		rejectGroupService.updateGroupStatusForReopen(groupCustomerBasicDetailsEntity.getGroupCode(), groupCustomerBasicDetailsEntity.getGroupStatus(), groupCustomerBasicDetailsEntity.getWorkflowStatus());
+	}
+	
+	@PostMapping("/insertGroupCustomerBasicDetails")
+	public void insertGroupCustomerBasicDetails(@RequestBody GroupCustomerBasicDetailsEntity groupCustomerBasicDetailsEntity) {
+		rejectGroupService.insertGroupCustomerBasicDetails(groupCustomerBasicDetailsEntity.getGroupCode(), groupCustomerBasicDetailsEntity.getGroupStatus(), groupCustomerBasicDetailsEntity.getWorkflowStatus(), groupCustomerBasicDetailsEntity.getTempGroupCustomerId());
+	}
+	
+	@PostMapping("/findByGroupCustomerId")
+	public Optional<GroupCustomerBasicDetailsTempEntity> findByGroupCustomerId(@RequestBody Long groupCustomerId) {
+		return rejectGroupService.findByGroupCustomerId(groupCustomerId);
+	}
+	
+	@PostMapping("/findByGroupCode")
+	public Optional<GroupCustomerBasicDetailsEntity> findByGroupCode(@RequestBody String groupCode) {
+		return rejectGroupService.findByGroupCode(groupCode);
 	}
 }
