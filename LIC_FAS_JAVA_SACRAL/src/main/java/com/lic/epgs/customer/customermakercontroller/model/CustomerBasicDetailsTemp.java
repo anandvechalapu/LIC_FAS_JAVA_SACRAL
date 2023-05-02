f7c9c06b2d2b821be9e3cb4a1287cad453c1c9d3
anandvechalapu <anand.vechalapu@gmail.com>
2023-05-02 12:59:18 +0530

    initial commit

---
+++ LIC_FAS_JAVA_SACRAL/src/main/java/com/lic/epgs/customer/customermakercontroller/model/CustomerBasicDetailsTemp.java
@@ -0,0 +1,101 @@
+package com.lic.epgs.customer.customermakercontroller.model;
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
+
+@Entity
+public class CustomerBasicDetailsTemp {
+
+    @Id
+    private Long customerId;
+    private String customerName;
+    private String contactPerson;
+    private String contactNumber;
+    private String emailId;
+    private String address;
+    private String city;
+    private String state;
+    private String zipCode;
+    private String country;
+
+    public Long getCustomerId() {
+        return customerId;
+    }
+
+    public void setCustomerId(Long customerId) {
+        this.customerId = customerId;
+    }
+
+    public String getCustomerName() {
+        return customerName;
+    }
+
+    public void setCustomerName(String customerName) {
+        this.customerName = customerName;
+    }
+
+    public String getContactPerson() {
+        return contactPerson;
+    }
+
+    public void setContactPerson(String contactPerson) {
+        this.contactPerson = contactPerson;
+    }
+
+    public String getContactNumber() {
+        return contactNumber;
+    }
+
+    public void setContactNumber(String contactNumber) {
+        this.contactNumber = contactNumber;
+    }
+
+    public String getEmailId() {
+        return emailId;
+    }
+
+    public void setEmailId(String emailId) {
+        this.emailId = emailId;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    public String getState() {
+        return state;
+    }
+
+    public void setState(String state) {
+        this.state = state;
+    }
+
+    public String getZipCode() {
+        return zipCode;
+    }
+
+    public void setZipCode(String zipCode) {
+        this.zipCode = zipCode;
+    }
+
+    public String getCountry() {
+        return country;
+    }
+
+    public void setCountry(String country) {
+        this.country = country;
+    }
+    
+}
