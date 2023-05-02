384cb594ac339bcc690f148941df0f7605425915
anandvechalapu <anand.vechalapu@gmail.com>
2023-05-02 17:57:48 +0530

    Updated

--- LIC_FAS_JAVA_SACRAL/src/main/java/com/lic/epgs/group/groupcustomerdetailscontroller/service/GroupCustomerDetailsService.java
+++ LIC_FAS_JAVA_SACRAL/src/main/java/com/lic/epgs/group/groupcustomerdetailscontroller/service/GroupCustomerDetailsService.java
@@ -1,66 +1,33 @@
 package com.lic.epgs.group.groupcustomerdetailscontroller.service;
 
-import java.util.List;
-import java.util.Optional;
+import com.lic.epgs.group.groupcustomerdetailscontroller.dto.GroupCustomerBasicDetailsDto;
+import com.lic.epgs.group.groupcustomerdetailscontroller.dto.CommonResponseDto;
+import com.lic.epgs.group.groupcustomerdetailscontroller.entity.GroupCustomerBasicDetailsTempEntity;
+import com.lic.epgs.group.groupcustomerdetailscontroller.repository.GroupCustomerDetailsRepository;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-
-import com.lic.epgs.group.groupcustomerdetailscontroller.model.GroupCustomerDetails;
-import com.lic.epgs.group.groupcustomerdetailscontroller.repository.GroupCustomerDetailsRepository;
+import org.springframework.transaction.annotation.Transactional;
 
 @Service
 public class GroupCustomerDetailsService {
 
-	@Autowired
-	GroupCustomerDetailsRepository groupCustomerDetailsRepository;
+    @Autowired
+    private GroupCustomerDetailsRepository groupCustomerDetailsRepository;
 
-	public GroupCustomerDetails findByGroupCustomerId(Long groupCustomerId) {
-		return groupCustomerDetailsRepository.findByGroupCustomerId(groupCustomerId);
-	}
+    @Transactional
+    public GroupCustomerBasicDetailsTempEntity findByGroupCustomerId(String groupCustomerId) {
+        return groupCustomerDetailsRepository.findByGroupCustomerId(groupCustomerId);
+    }
 
-	public GroupCustomerDetails findByBasicDetailsNameAndType(String name, String type) {
-		return groupCustomerDetailsRepository.findByBasicDetailsNameAndType(name, type);
-	}
+    @Transactional
+    public GroupCustomerBasicDetailsTempEntity save(GroupCustomerBasicDetailsDto groupCustomerBasicDetailsDto) {
+        return groupCustomerDetailsRepository.save(groupCustomerBasicDetailsDto);
+    }
 
-	public GroupCustomerDetails findByContactDetailsPrimaryContactNameAndNumber(String contactName, String contactNumber) {
-		return groupCustomerDetailsRepository.findByContactDetailsPrimaryContactNameAndNumber(contactName, contactNumber);
-	}
+    @Transactional
+    public CommonResponseDto updateGroupStatusAndWorkflowStatus(Long groupCustomerId, String groupStatus, String workflowStatus) {
+        return groupCustomerDetailsRepository.updateGroupStatusAndWorkflowStatus(groupCustomerId, groupStatus, workflowStatus);
+    }
 
-	public GroupCustomerDetails findByAddressDetailsAddressLineAndCity(String addressLine, String city) {
-		return groupCustomerDetailsRepository.findByAddressDetailsAddressLineAndCity(addressLine, city);
-	}
-
-	public GroupCustomerDetails findByGroupCustomerDetailsCustomerIdAndName(Long customerId, String customerName) {
-		return groupCustomerDetailsRepository.findByGroupCustomerDetailsCustomerIdAndName(customerId, customerName);
-	}
-
-	public GroupCustomerDetails findByNotes(String notes) {
-		return groupCustomerDetailsRepository.findByNotes(notes);
-	}
-
-	public GroupCustomerDetails findByBankAccountDetailsAccountNumberAndBankName(String accountNumber,
-			String bankName) {
-		return groupCustomerDetailsRepository.findByBankAccountDetailsAccountNumberAndBankName(accountNumber, bankName);
-	}
-
-	public GroupCustomerDetails findByGroupCustomerIdAndReturnErrorMessage(Long groupCustomerId) {
-		return groupCustomerDetailsRepository.findByGroupCustomerIdAndReturnErrorMessage(groupCustomerId);
-	}
-
-	public List<GroupCustomerDetails> findAll() {
-		return groupCustomerDetailsRepository.findAll();
-	}
-
-	public Optional<GroupCustomerDetails> findById(Long id) {
-		return groupCustomerDetailsRepository.findById(id);
-	}
-
-	public GroupCustomerDetails save(GroupCustomerDetails groupCustomerDetails) {
-		return groupCustomerDetailsRepository.save(groupCustomerDetails);
-	}
-
-	public void deleteById(Long id) {
-		groupCustomerDetailsRepository.deleteById(id);
-	}
 }
