58b67627f0c64a3d2aa58b8b9f0e3c77c4c7ec6a
anandvechalapu <anand.vechalapu@gmail.com>
2023-05-02 17:47:41 +0530

    initial commit

---
+++ LIC_FAS_JAVA_SACRAL/src/main/java/com/lic/epgs/group/groupcustomerdetailscontroller/repository/GroupCustomerNotesDetailsRepository.java
@@ -0,0 +1,11 @@
+package com.lic.epgs.group.groupcustomerdetailscontroller.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.lic.epgs.group.groupcustomerdetailscontroller.model.GroupCustomerNotesDetails;
+
+@Repository
+public interface GroupCustomerNotesDetailsRepository extends JpaRepository<GroupCustomerNotesDetails, Long> {
+    public GroupCustomerNotesDetails findByGroupCustomerIdAndDateRange(Long groupCustomerId, String startDate, String endDate);
+}
