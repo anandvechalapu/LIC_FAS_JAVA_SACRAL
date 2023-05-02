90dcec6ab08a5808cf3fd4ba01df80fa4b78da9a
anandvechalapu <anand.vechalapu@gmail.com>
2023-05-02 18:09:39 +0530

    Updated

--- LIC_FAS_JAVA_SACRAL/src/main/java/com/lic/epgs/group/groupcustomerdetailscontroller/service/GroupCustomerDetailsService.java
+++ LIC_FAS_JAVA_SACRAL/src/main/java/com/lic/epgs/group/groupcustomerdetailscontroller/service/GroupCustomerDetailsService.java
@@ -1,33 +1,25 @@
 package com.lic.epgs.group.groupcustomerdetailscontroller.service;
 
-import com.lic.epgs.group.groupcustomerdetailscontroller.dto.GroupCustomerBasicDetailsDto;
-import com.lic.epgs.group.groupcustomerdetailscontroller.dto.CommonResponseDto;
-import com.lic.epgs.group.groupcustomerdetailscontroller.entity.GroupCustomerBasicDetailsTempEntity;
-import com.lic.epgs.group.groupcustomerdetailscontroller.repository.GroupCustomerDetailsRepository;
+import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-import org.springframework.transaction.annotation.Transactional;
+
+import com.lic.epgs.group.groupcustomerdetailscontroller.model.GroupCustomerDetails;
+import com.lic.epgs.group.groupcustomerdetailscontroller.repository.GroupCustomerDetailsRepository;
 
 @Service
 public class GroupCustomerDetailsService {
 
-    @Autowired
-    private GroupCustomerDetailsRepository groupCustomerDetailsRepository;
+  @Autowired
+  private GroupCustomerDetailsRepository groupCustomerDetailsRepository;
 
-    @Transactional
-    public GroupCustomerBasicDetailsTempEntity findByGroupCustomerId(String groupCustomerId) {
-        return groupCustomerDetailsRepository.findByGroupCustomerId(groupCustomerId);
-    }
+  public List<GroupCustomerDetails> getGroupCustomerDetails() {
+    return groupCustomerDetailsRepository.findAll();
+  }
 
-    @Transactional
-    public GroupCustomerBasicDetailsTempEntity save(GroupCustomerBasicDetailsDto groupCustomerBasicDetailsDto) {
-        return groupCustomerDetailsRepository.save(groupCustomerBasicDetailsDto);
-    }
-
-    @Transactional
-    public CommonResponseDto updateGroupStatusAndWorkflowStatus(Long groupCustomerId, String groupStatus, String workflowStatus) {
-        return groupCustomerDetailsRepository.updateGroupStatusAndWorkflowStatus(groupCustomerId, groupStatus, workflowStatus);
-    }
-
+  public void deleteGroupCustomerDetails(Long groupCustomerId) {
+    groupCustomerDetailsRepository.deleteGroupCustomerDetails(groupCustomerId);
+  }
+  
 }
