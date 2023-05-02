bcb9005801ed3f93dcbfbb592fe85bb2c63c283d
anandvechalapu <anand.vechalapu@gmail.com>
2023-05-02 17:16:57 +0530

    Updated

--- LIC_FAS_JAVA_SACRAL/src/main/java/com/lic/epgs/group/groupcustomerdetailscontroller/model/GroupCustomer.java
+++ LIC_FAS_JAVA_SACRAL/src/main/java/com/lic/epgs/group/groupcustomerdetailscontroller/model/GroupCustomer.java
@@ -1,9 +1,5 @@
 package com.lic.epgs.group.groupcustomerdetailscontroller.model;
 
-import java.io.Serializable;
-import java.time.LocalDateTime;
-
-import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
@@ -11,99 +7,18 @@ import javax.persistence.Id;
 import javax.persistence.Table;
 
 @Entity
-@Table(name="group_customer")
-public class GroupCustomer implements Serializable {
+@Table(name = "group_customer")
+public class GroupCustomer {
 
-    private static final long serialVersionUID = 1L;
-    
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    @Column(name="id")
-    private Long id;
-
-    @Column(name="group_code")
-    private String groupCode;
-
-    @Column(name="group_name")
-    private String groupName;
-
-    @Column(name="group_status")
-    private String groupStatus;
-
-    @Column(name="unit_code")
-    private String unitCode;
-
-    @Column(name="status")
+    private Long groupCustomerId;
+    
+    private String name;
+    
+    private String address;
+    
     private String status;
-
-    @Column(name="created_on")
-    private LocalDateTime createdOn;
-
-    @Column(name="modified_on")
-    private LocalDateTime modifiedOn;
-
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public String getGroupCode() {
-        return groupCode;
-    }
-
-    public void setGroupCode(String groupCode) {
-        this.groupCode = groupCode;
-    }
-
-    public String getGroupName() {
-        return groupName;
-    }
-
-    public void setGroupName(String groupName) {
-        this.groupName = groupName;
-    }
-
-    public String getGroupStatus() {
-        return groupStatus;
-    }
-
-    public void setGroupStatus(String groupStatus) {
-        this.groupStatus = groupStatus;
-    }
-
-    public String getUnitCode() {
-        return unitCode;
-    }
-
-    public void setUnitCode(String unitCode) {
-        this.unitCode = unitCode;
-    }
-
-    public String getStatus() {
-        return status;
-    }
-
-    public void setStatus(String status) {
-        this.status = status;
-    }
-
-    public LocalDateTime getCreatedOn() {
-        return createdOn;
-    }
-
-    public void setCreatedOn(LocalDateTime createdOn) {
-        this.createdOn = createdOn;
-    }
-
-    public LocalDateTime getModifiedOn() {
-        return modifiedOn;
-    }
-
-    public void setModifiedOn(LocalDateTime modifiedOn) {
-        this.modifiedOn = modifiedOn;
-    }
-
+    
+    // getters and setters
 }
