0b322728b16b7e2d3b26157f328c7f9ecead79ac
anandvechalapu <anand.vechalapu@gmail.com>
2023-05-02 12:13:53 +0530

    Updated

--- LIC_FAS_JAVA_SACRAL/src/main/java/com/lic/epgs/customer/customercheckercontroller/service/CustomerCheckerControllerService.java
+++ LIC_FAS_JAVA_SACRAL/src/main/java/com/lic/epgs/customer/customercheckercontroller/service/CustomerCheckerControllerService.java
@@ -1,33 +1,18 @@
 package com.lic.epgs.customer.customercheckercontroller.service;
 
-import java.util.List;
-
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Service;
-
-import com.lic.epgs.customer.customercheckercontroller.model.Customer;
+import org.springframework.http.ResponseEntity;
 import com.lic.epgs.customer.customercheckercontroller.repository.CustomerCheckerControllerRepository;
 
-@Service
 public class CustomerCheckerControllerService {
-
-    @Autowired
+ 
     private CustomerCheckerControllerRepository customerCheckerControllerRepository;
-
-    public Customer getCustomer(Long customerId) {
-        return customerCheckerControllerRepository.findByCustomerId(customerId);
+ 
+    public CustomerCheckerControllerService(CustomerCheckerControllerRepository customerCheckerControllerRepository) {
+        this.customerCheckerControllerRepository = customerCheckerControllerRepository;
     }
-
-    public Customer getCustomer(Long customerId, String trustCode) {
-        return customerCheckerControllerRepository.findByCustomerIdAndTrustCode(customerId, trustCode);
+ 
+    public ResponseEntity<?> getMasterCustomerDetails(Long customerId) throws Exception {
+        return customerCheckerControllerRepository.getMasterCustomerDetails(customerId);
     }
-
-    public Customer getCustomer(Long customerId, String trustCode, String trustName) {
-        return customerCheckerControllerRepository.findByCustomerIdAndTrustCodeAndTrustName(customerId, trustCode, trustName);
-    }
-
-    public List<Customer> getAllCustomers() {
-        return customerCheckerControllerRepository.findAll();
-    }
-
+ 
 }
