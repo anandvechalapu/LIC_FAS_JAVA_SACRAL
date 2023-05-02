8ebdb9775235a37536ce9a05e75e4a1b846216ad
anandvechalapu <anand.vechalapu@gmail.com>
2023-05-02 17:53:30 +0530

    Updated

--- LIC_FAS_JAVA_SACRAL/src/main/java/com/lic/epgs/group/groupcustomerdetailscontroller/model/GroupCustomerNotesDetails.java
+++ LIC_FAS_JAVA_SACRAL/src/main/java/com/lic/epgs/group/groupcustomerdetailscontroller/model/GroupCustomerNotesDetails.java
@@ -1,45 +1,55 @@
 package com.lic.epgs.group.groupcustomerdetailscontroller.model;
 
+import java.util.Date;
+
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
-import javax.persistence.Table;
 
 @Entity
-@Table(name="GroupCustomerNotesDetails")
 public class GroupCustomerNotesDetails {
 
-    @Id
-    @GeneratedValue(strategy=GenerationType.AUTO)
-    private Long id;
-    private Long groupCustomerId;
-    private String notes;
-    private String date;
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
 
-    public Long getId() {
-        return id;
-    }
-    public void setId(Long id) {
-        this.id = id;
-    }
-    public Long getGroupCustomerId() {
-        return groupCustomerId;
-    }
-    public void setGroupCustomerId(Long groupCustomerId) {
-        this.groupCustomerId = groupCustomerId;
-    }
-    public String getNotes() {
-        return notes;
-    }
-    public void setNotes(String notes) {
-        this.notes = notes;
-    }
-    public String getDate() {
-        return date;
-    }
-    public void setDate(String date) {
-        this.date = date;
-    }
+	private Long groupCustomerId;
+
+	private String notes;
+
+	private Date createdDate;
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public Long getGroupCustomerId() {
+		return groupCustomerId;
+	}
+
+	public void setGroupCustomerId(Long groupCustomerId) {
+		this.groupCustomerId = groupCustomerId;
+	}
+
+	public String getNotes() {
+		return notes;
+	}
+
+	public void setNotes(String notes) {
+		this.notes = notes;
+	}
+
+	public Date getCreatedDate() {
+		return createdDate;
+	}
+
+	public void setCreatedDate(Date createdDate) {
+		this.createdDate = createdDate;
+	}
 
 }
