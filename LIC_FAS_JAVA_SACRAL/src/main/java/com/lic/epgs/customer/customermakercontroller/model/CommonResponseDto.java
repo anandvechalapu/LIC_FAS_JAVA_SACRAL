1e3db6c55b99676525f1005180db9ddee0c259f2
anandvechalapu <anand.vechalapu@gmail.com>
2023-05-02 13:10:01 +0530

    initial commit

---
+++ LIC_FAS_JAVA_SACRAL/src/main/java/com/lic/epgs/customer/customermakercontroller/model/CommonResponseDto.java
@@ -0,0 +1,69 @@
+package com.lic.epgs.customer.customermakercontroller.model;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+public class CommonResponseDto {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String roleType;
+    private String unitCode;
+    private String customerName;
+    private String trustCode;
+    private String trustName;
+    
+    // getters and setters
+    
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getRoleType() {
+        return roleType;
+    }
+
+    public void setRoleType(String roleType) {
+        this.roleType = roleType;
+    }
+
+    public String getUnitCode() {
+        return unitCode;
+    }
+
+    public void setUnitCode(String unitCode) {
+        this.unitCode = unitCode;
+    }
+
+    public String getCustomerName() {
+        return customerName;
+    }
+
+    public void setCustomerName(String customerName) {
+        this.customerName = customerName;
+    }
+
+    public String getTrustCode() {
+        return trustCode;
+    }
+
+    public void setTrustCode(String trustCode) {
+        this.trustCode = trustCode;
+    }
+
+    public String getTrustName() {
+        return trustName;
+    }
+
+    public void setTrustName(String trustName) {
+        this.trustName = trustName;
+    }
+    
+}
