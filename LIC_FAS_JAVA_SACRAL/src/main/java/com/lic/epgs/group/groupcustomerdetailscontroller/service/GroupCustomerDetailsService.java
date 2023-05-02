0e95e7597fbf71dec8b5ce3397d6bd821c328a37
anandvechalapu <anand.vechalapu@gmail.com>
2023-05-02 17:22:51 +0530

    Updated

--- LIC_FAS_JAVA_SACRAL/src/main/java/com/lic/epgs/group/groupcustomerdetailscontroller/service/GroupCustomerDetailsService.java
+++ LIC_FAS_JAVA_SACRAL/src/main/java/com/lic/epgs/group/groupcustomerdetailscontroller/service/GroupCustomerDetailsService.java
@@ -1,31 +1,66 @@
 package com.lic.epgs.group.groupcustomerdetailscontroller.service;
 
 import java.util.List;
+import java.util.Optional;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
 import com.lic.epgs.group.groupcustomerdetailscontroller.model.GroupCustomerDetails;
 import com.lic.epgs.group.groupcustomerdetailscontroller.repository.GroupCustomerDetailsRepository;
 
 @Service
 public class GroupCustomerDetailsService {
-    
-    @Autowired
-    private GroupCustomerDetailsRepository groupCustomerDetailsRepository;
 
-    public GroupCustomerDetails getGroupDetailsMakerByGroupCustomerIdAndUnitCode(Long groupCustomerId, String unitCode) {
-        return groupCustomerDetailsRepository.getGroupDetailsMakerByGroupCustomerIdAndUnitCode(groupCustomerId, unitCode);
-    }
+	@Autowired
+	GroupCustomerDetailsRepository groupCustomerDetailsRepository;
 
-    public GroupCustomerDetails getGroupDetailsMakerByGroupCustomerId(Long groupCustomerId) {
-        return groupCustomerDetailsRepository.getGroupDetailsMakerByGroupCustomerId(groupCustomerId);
-    }
+	public GroupCustomerDetails findByGroupCustomerId(Long groupCustomerId) {
+		return groupCustomerDetailsRepository.findByGroupCustomerId(groupCustomerId);
+	}
 
-    public List<GroupCustomerDetails> findAll() {
-        return groupCustomerDetailsRepository.findAll();
-    }
+	public GroupCustomerDetails findByBasicDetailsNameAndType(String name, String type) {
+		return groupCustomerDetailsRepository.findByBasicDetailsNameAndType(name, type);
+	}
 
-    public GroupCustomerDetails save(GroupCustomerDetails groupCustomerDetails) {
-        return groupCustomerDetailsRepository.save(groupCustomerDetails);
-    }
+	public GroupCustomerDetails findByContactDetailsPrimaryContactNameAndNumber(String contactName, String contactNumber) {
+		return groupCustomerDetailsRepository.findByContactDetailsPrimaryContactNameAndNumber(contactName, contactNumber);
+	}
 
+	public GroupCustomerDetails findByAddressDetailsAddressLineAndCity(String addressLine, String city) {
+		return groupCustomerDetailsRepository.findByAddressDetailsAddressLineAndCity(addressLine, city);
+	}
+
+	public GroupCustomerDetails findByGroupCustomerDetailsCustomerIdAndName(Long customerId, String customerName) {
+		return groupCustomerDetailsRepository.findByGroupCustomerDetailsCustomerIdAndName(customerId, customerName);
+	}
+
+	public GroupCustomerDetails findByNotes(String notes) {
+		return groupCustomerDetailsRepository.findByNotes(notes);
+	}
+
+	public GroupCustomerDetails findByBankAccountDetailsAccountNumberAndBankName(String accountNumber,
+			String bankName) {
+		return groupCustomerDetailsRepository.findByBankAccountDetailsAccountNumberAndBankName(accountNumber, bankName);
+	}
+
+	public GroupCustomerDetails findByGroupCustomerIdAndReturnErrorMessage(Long groupCustomerId) {
+		return groupCustomerDetailsRepository.findByGroupCustomerIdAndReturnErrorMessage(groupCustomerId);
+	}
+
+	public List<GroupCustomerDetails> findAll() {
+		return groupCustomerDetailsRepository.findAll();
+	}
+
+	public Optional<GroupCustomerDetails> findById(Long id) {
+		return groupCustomerDetailsRepository.findById(id);
+	}
+
+	public GroupCustomerDetails save(GroupCustomerDetails groupCustomerDetails) {
+		return groupCustomerDetailsRepository.save(groupCustomerDetails);
+	}
+
+	public void deleteById(Long id) {
+		groupCustomerDetailsRepository.deleteById(id);
+	}
 }
