564e3997a9a01c254be7dd19c10018f309542d28
anandvechalapu <anand.vechalapu@gmail.com>
2023-05-02 17:54:31 +0530

    Updated

--- LIC_FAS_JAVA_SACRAL/src/main/java/com/lic/epgs/group/groupcustomerdetailscontroller/controller/GroupCustomerNotesDetailsController.java
+++ LIC_FAS_JAVA_SACRAL/src/main/java/com/lic/epgs/group/groupcustomerdetailscontroller/controller/GroupCustomerNotesDetailsController.java
@@ -1,16 +1,3 @@
-package com.lic.epgs.group.groupcustomerdetailscontroller.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
-
-import com.lic.epgs.group.groupcustomerdetailscontroller.model.GroupCustomerNotesDetails;
-
-public interface GroupCustomerNotesDetailsRepository extends JpaRepository<GroupCustomerNotesDetails, Long> {
-
-    @Query("SELECT g FROM GroupCustomerNotesDetails g WHERE g.groupCustomerId = :groupCustomerId AND g.startDate >= :startDate AND g.endDate <= :endDate")
-    List<GroupCustomerNotesDetails> findByGroupCustomerIdAndDateRange(Long groupCustomerId, String startDate, String endDate);
-}
-
 package com.lic.epgs.group.groupcustomerdetailscontroller.controller;
 
 import java.util.List;
@@ -18,7 +5,6 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -26,16 +12,21 @@ import com.lic.epgs.group.groupcustomerdetailscontroller.model.GroupCustomerNote
 import com.lic.epgs.group.groupcustomerdetailscontroller.service.GroupCustomerNotesDetailsService;
 
 @RestController
-@RequestMapping("/group-customer-notes-details")
 public class GroupCustomerNotesDetailsController {
 
     @Autowired
     private GroupCustomerNotesDetailsService groupCustomerNotesDetailsService;
-    
-    @GetMapping("/{groupCustomerId}")
-    public List<GroupCustomerNotesDetails> getGroupCustomerNotesDetailsByGroupCustomerIdAndDateRange(@PathVariable Long groupCustomerId,
-                                                                                                    @RequestParam(value="startDate") String startDate, 
-                                                                                                    @RequestParam(value="endDate") String endDate) {
-        return groupCustomerNotesDetailsService.findByGroupCustomerIdAndDateRange(groupCustomerId, startDate, endDate);
+
+    @GetMapping("/groupcustomer/{groupCustomerId}/notes")
+    public List<GroupCustomerNotesDetails> getGroupCustomerNotesDetailsByGroupCustomerId(@PathVariable Long groupCustomerId) {
+        return groupCustomerNotesDetailsService.getGroupCustomerNotesDetailsByGroupCustomerId(groupCustomerId);
     }
+
+    @GetMapping("/groupcustomer/{groupCustomerId}/notes/daterange")
+    public GroupCustomerNotesDetails getGroupCustomerNotesDetailsByGroupCustomerIdAndDateRange(@PathVariable Long groupCustomerId,
+                                                                                              @RequestParam(name = "startDate") String startDate,
+                                                                                              @RequestParam(name = "endDate") String endDate) {
+        return groupCustomerNotesDetailsService.getGroupCustomerNotesDetailsByGroupCustomerIdAndDateRange(groupCustomerId, startDate, endDate);
+    }
+
 }
