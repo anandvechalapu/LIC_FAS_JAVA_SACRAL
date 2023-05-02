3a1b5db64a3f39b1da720f3eb648adcfaa22f09c
anandvechalapu <anand.vechalapu@gmail.com>
2023-05-02 17:52:08 +0530

    Updated

--- LIC_FAS_JAVA_SACRAL/src/main/java/com/lic/epgs/group/groupcustomerdetailscontroller/model/GroupCustomerNotesDetails.java
+++ LIC_FAS_JAVA_SACRAL/src/main/java/com/lic/epgs/group/groupcustomerdetailscontroller/model/GroupCustomerNotesDetails.java
@@ -4,54 +4,42 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.Table;
 
 @Entity
+@Table(name="GroupCustomerNotesDetails")
 public class GroupCustomerNotesDetails {
+
     @Id
-    @GeneratedValue(strategy= GenerationType.IDENTITY)
+    @GeneratedValue(strategy=GenerationType.AUTO)
     private Long id;
     private Long groupCustomerId;
     private String notes;
-    private String startDate;
-    private String endDate;
+    private String date;
 
     public Long getId() {
         return id;
     }
-
     public void setId(Long id) {
         this.id = id;
     }
-
     public Long getGroupCustomerId() {
         return groupCustomerId;
     }
-
     public void setGroupCustomerId(Long groupCustomerId) {
         this.groupCustomerId = groupCustomerId;
     }
-
     public String getNotes() {
         return notes;
     }
-
     public void setNotes(String notes) {
         this.notes = notes;
     }
-
-    public String getStartDate() {
-        return startDate;
+    public String getDate() {
+        return date;
+    }
+    public void setDate(String date) {
+        this.date = date;
     }
 
-    public void setStartDate(String startDate) {
-        this.startDate = startDate;
-    }
-
-    public String getEndDate() {
-        return endDate;
-    }
-
-    public void setEndDate(String endDate) {
-        this.endDate = endDate;
-    }
 }
