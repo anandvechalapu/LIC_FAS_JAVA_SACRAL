965e8d7f9e1701a26f647c46332be2abb75fd8b8
anandvechalapu <anand.vechalapu@gmail.com>
2023-05-02 14:46:54 +0530

    initial commit

---
+++ LIC_FAS_JAVA_SACRAL/src/main/java/com/lic/epgs/customer/customermakercontroller/model/CustomerGroupCompany.java
@@ -0,0 +1,24 @@
+package com.lic.epgs.customer.customermakercontroller.model;
+
+public class CustomerGroupCompany {
+
+  private Integer customerId;
+  private Integer groupId;
+
+  public Integer getCustomerId() {
+    return customerId;
+  }
+
+  public void setCustomerId(Integer customerId) {
+    this.customerId = customerId;
+  }
+
+  public Integer getGroupId() {
+    return groupId;
+  }
+
+  public void setGroupId(Integer groupId) {
+    this.groupId = groupId;
+  }
+
+}
