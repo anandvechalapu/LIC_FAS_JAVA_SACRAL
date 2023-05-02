1679244660424237ab7b0f23683693662bd8e0de
anandvechalapu <anand.vechalapu@gmail.com>
2023-05-02 17:10:04 +0530

    Updated

--- LIC_FAS_JAVA_SACRAL/src/main/java/com/lic/epgs/group/groupcustomerdetailscontroller/controller/GroupCustomerDetailsController.java
+++ LIC_FAS_JAVA_SACRAL/src/main/java/com/lic/epgs/group/groupcustomerdetailscontroller/controller/GroupCustomerDetailsController.java
@@ -1,42 +1,26 @@
 package com.lic.epgs.group.groupcustomerdetailscontroller.controller;
 
 import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-import com.lic.epgs.group.groupcustomerdetailscontroller.model.GroupCustomerDetails;
-import com.lic.epgs.group.groupcustomerdetailscontroller.service.GroupCustomerDetailsService;
+
+import com.lic.epgs.group.groupcustomerdetailscontroller.model.GroupCustomerDetailsController;
+import com.lic.epgs.group.groupcustomerdetailscontroller.service.GetGroupMemberCustomerDetailsService;
 
 @RestController
-@RequestMapping("/groupcustomerdetails")
+@RequestMapping("/group-customer-details")
 public class GroupCustomerDetailsController {
-    
+
     @Autowired
-    private GroupCustomerDetailsService groupCustomerDetailsService;
+    private GetGroupMemberCustomerDetailsService getGroupMemberCustomerDetailsService;
 
-    @PostMapping
-    public GroupCustomerDetails save(@RequestBody GroupCustomerDetails groupCustomerDetails) {
-        return groupCustomerDetailsService.save(groupCustomerDetails);
+    @GetMapping("/{customerCode}/{groupCustomerId}")
+    public List<GroupCustomerDetailsController> getGroupMemberCustomerDetails(@PathVariable String customerCode,
+                                                                            @PathVariable int groupCustomerId) {
+        return getGroupMemberCustomerDetailsService.getGroupMemberCustomerDetails(customerCode, groupCustomerId);
     }
-
-    @GetMapping("/{groupCustomerId}/{unitCode}")
-    public GroupCustomerDetails getGroupDetailsMakerByGroupCustomerIdAndUnitCode(@PathVariable Long groupCustomerId,
-            @PathVariable String unitCode) {
-        return groupCustomerDetailsService.getGroupDetailsMakerByGroupCustomerIdAndUnitCode(groupCustomerId, unitCode);
-    }
-
-    @GetMapping("/{groupCustomerId}")
-    public GroupCustomerDetails getGroupDetailsMakerByGroupCustomerId(@PathVariable Long groupCustomerId) {
-        return groupCustomerDetailsService.getGroupDetailsMakerByGroupCustomerId(groupCustomerId);
-    }
-
-    @GetMapping
-    public List<GroupCustomerDetails> findAll() {
-        return groupCustomerDetailsService.findAll();
-    }
-
 }
