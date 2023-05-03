7ddb6a312750389b4dffd912e5efa707eb6a0cc6
anandvechalapu <anand.vechalapu@gmail.com>
2023-05-03 17:00:52 +0530

    initial commit

---
+++ LIC_FAS_JAVA_SACRAL/src/main/java/com/lic/epgs/proposal/proposalmakercontroller/model/MPHContactPersonDetailsDto.java
@@ -0,0 +1,12 @@
+public class MPHContactPersonDetailsDto { 
+  
+    private Long proposalId; 
+    private Long contactPersonId; 
+    private String contactPersonName; 
+    private String contactPersonDesignation; 
+    private String contactPersonEmail; 
+    private String contactPersonPhone; 
+    private Boolean active; 
+  
+    // getters, setters and other methods 
+}
