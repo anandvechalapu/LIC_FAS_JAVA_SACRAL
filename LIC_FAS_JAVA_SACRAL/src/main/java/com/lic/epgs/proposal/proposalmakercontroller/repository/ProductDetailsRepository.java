234e12a64e02eabf075303c91b4849244ebd53c1
anandvechalapu <anand.vechalapu@gmail.com>
2023-05-03 16:30:50 +0530

    Updated

--- LIC_FAS_JAVA_SACRAL/src/main/java/com/lic/epgs/proposal/proposalmakercontroller/repository/ProductDetailsRepository.java
+++ LIC_FAS_JAVA_SACRAL/src/main/java/com/lic/epgs/proposal/proposalmakercontroller/repository/ProductDetailsRepository.java
@@ -3,16 +3,14 @@ package com.lic.epgs.proposal.proposalmakercontroller.repository;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
+
 import com.lic.epgs.proposal.proposalmakercontroller.dto.ProductDetailsDto;
 import com.lic.epgs.proposal.proposalmakercontroller.entity.ProductDetailsEntity;
-
-import java.util.List;
 
 @Repository
 public interface ProductDetailsRepository extends JpaRepository<ProductDetailsEntity, Long> {
 
-    @Query("SELECT new com.lic.epgs.proposal.proposalmakercontroller.dto.ProductDetailsDto(pd.leadProductId, pd.productName) " +
-            "FROM ProductDetailsEntity pd")
-    public List<ProductDetailsDto> getProductNameList();
+	@Query("SELECT new com.lic.epgs.proposal.proposalmakercontroller.dto.ProductDetailsDto(p.leadProductId, p.productName) FROM ProductDetailsEntity p")
+	ProductDetailsDto[] getProductNameList_PROPOSALMAKERCONTROLLER11();
 
 }
