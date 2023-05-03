ebe70cec6ea3b1c8873eb98680ca4ee367f3b2fb
anandvechalapu <anand.vechalapu@gmail.com>
2023-05-03 18:49:15 +0530

    Updated

--- LIC_FAS_JAVA_SACRAL/src/main/java/com/lic/epgs/proposal/proposalmakercontroller/model/Proposal.java
+++ LIC_FAS_JAVA_SACRAL/src/main/java/com/lic/epgs/proposal/proposalmakercontroller/model/Proposal.java
@@ -1,50 +1,50 @@
 package com.lic.epgs.proposal.proposalmakercontroller.model;
 
-import javax.persistence.Column;
 import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.ManyToOne;
 import javax.persistence.Table;
 
 @Entity
-@Table(name = "proposal")
+@Table(name="proposal")
 public class Proposal {
 
     @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    @Column(name = "proposal_id")
-    private Long proposalId;
+    private Long id;
 
-    @Column(name = "user_modified")
-    private String userModified;
-    
-    @Column(name = "status")
-    private String status;
-	
-    // Getters and Setters
-    
-    public Long getProposalId() {
-        return proposalId;
-    }
+    @ManyToOne
+    private Customer customer;
 
-    public void setProposalId(Long proposalId) {
-        this.proposalId = proposalId;
-    }
+    @ManyToOne
+    private SubCustomer subCustomer;
 
-    public String getUserModified() {
-        return userModified;
-    }
+    @ManyToOne
+    private Trust trust;
 
-    public void setUserModified(String userModified) {
-        this.userModified = userModified;
-    }
-    
-    public String getStatus() {
-        return status;
-    }
+    @ManyToOne
+    private GroupCustomer groupCustomer;
 
-    public void setStatus(String status) {
-        this.status = status;
-    }
+    @ManyToOne
+    private Lead lead;
+
+    @ManyToOne
+    private ProposalBasic proposalBasic;
+
+    @ManyToOne
+    private ProposalChannel proposalChannel;
+
+    @ManyToOne
+    private ProposalProduct proposalProduct;
+
+    @ManyToOne
+    private BankAccount bankAccount;
+
+    @ManyToOne
+    private ContactPerson contactPerson;
+
+    @ManyToOne
+    private Address address;
+
+    // getters and setters
+
 }
