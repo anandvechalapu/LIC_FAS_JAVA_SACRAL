7012d10c83d82c097c7fcfde32a775a4bcc28154
anandvechalapu <anand.vechalapu@gmail.com>
2023-05-03 14:47:23 +0530

    Updated

--- LIC_FAS_JAVA_SACRAL/src/main/java/com/lic/epgs/proposal/proposalcheckercontroller/service/ProposalCheckerControllerService.java
+++ LIC_FAS_JAVA_SACRAL/src/main/java/com/lic/epgs/proposal/proposalcheckercontroller/service/ProposalCheckerControllerService.java
@@ -1,24 +1,44 @@
 package com.lic.epgs.proposal.proposalcheckercontroller.service;
 
-import java.util.Optional;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-import com.lic.epgs.proposal.proposalcheckercontroller.entity.Proposal;
+import com.lic.epgs.proposal.proposalcheckercontroller.dto.ProposalBasicDetailsDto;
+import com.lic.epgs.proposal.proposalcheckercontroller.model.Proposal;
 import com.lic.epgs.proposal.proposalcheckercontroller.repository.ProposalCheckerControllerRepository;
 
 @Service
 public class ProposalCheckerControllerService {
-    
+
     @Autowired
     private ProposalCheckerControllerRepository proposalCheckerControllerRepository;
-    
-    public Optional<Proposal> getProposal(Long proposalId) {
-        return proposalCheckerControllerRepository.findById(proposalId);
+
+    public ProposalBasicDetailsDto sendToMaker(Proposal proposal) {
+        return proposalCheckerControllerRepository.sendToMaker(proposal);
     }
-    
-    public int approveProposal(Long proposalId, String modifiedBy) {
-        return proposalCheckerControllerRepository.approveProposal(proposalId, modifiedBy);
+
+    public void markProposalAsInactive(Proposal proposal) {
+        proposalCheckerControllerRepository.markProposalAsInactive(proposal);
     }
+
+    public void updateProposalStatusAndWorkflowStatus(Proposal proposal) {
+        proposalCheckerControllerRepository.updateProposalStatusAndWorkflowStatus(proposal);
+    }
+
+    public Proposal copyExistingProposal(Proposal proposal) {
+        return proposalCheckerControllerRepository.copyExistingProposal(proposal);
+    }
+
+    public void updateMappedFilesForNewProposal(Proposal proposal) {
+        proposalCheckerControllerRepository.updateMappedFilesForNewProposal(proposal);
+    }
+
+    public ProposalBasicDetailsDto getProposalBasicDetails(Proposal proposal) {
+        return proposalCheckerControllerRepository.getProposalBasicDetails(proposal);
+    }
+
+    public void returnErrorIfProposalIsInvalid(Proposal proposal) {
+        proposalCheckerControllerRepository.returnErrorIfProposalIsInvalid(proposal);
+    }
+
 }
