5c637ea23b9bafcb4209eada0aa1d74e060077db
anandvechalapu <anand.vechalapu@gmail.com>
2023-05-03 18:48:53 +0530

    Updated

--- LIC_FAS_JAVA_SACRAL/src/main/java/com/lic/epgs/proposal/proposalmakercontroller/repository/ProposalRepository.java
+++ LIC_FAS_JAVA_SACRAL/src/main/java/com/lic/epgs/proposal/proposalmakercontroller/repository/ProposalRepository.java
@@ -1,13 +1,13 @@
 package com.lic.epgs.proposal.proposalmakercontroller.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
+import org.springframework.data.jpa.repository.Query;
 
-import com.lic.epgs.proposal.proposalmakercontroller.entity.Proposal;
+import com.lic.epgs.proposal.proposalmakercontroller.model.Proposal;
 
-@Repository
-public interface ProposalRepository extends JpaRepository<Proposal, Integer> {
+public interface ProposalRepository extends JpaRepository<Proposal, Long> {
 
-  Proposal getProposalDetailsByProposalNumber_PROPOSALMAKERCONTROLLER18(int proposalNumber);
+    @Query("SELECT p FROM Proposal p LEFT JOIN FETCH p.customer c LEFT JOIN FETCH p.subCustomer sc LEFT JOIN FETCH p.trust t LEFT JOIN FETCH p.groupCustomer gc LEFT JOIN FETCH p.lead l LEFT JOIN FETCH p.proposalBasic pb LEFT JOIN FETCH p.proposalChannel pc LEFT JOIN FETCH p.proposalProduct pp LEFT JOIN FETCH p.bankAccount ba LEFT JOIN FETCH p.contactPerson cp LEFT JOIN FETCH p.address a WHERE p.id = ?1")
+    Proposal getProposalOverallDetails_PROPOSALMAKERCONTROLLER34(Long proposalId);
 
 }
