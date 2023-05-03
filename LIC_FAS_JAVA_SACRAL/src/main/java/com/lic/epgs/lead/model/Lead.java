95abeaf966108234bea609547ff70ab7c3aca645
anandvechalapu <anand.vechalapu@gmail.com>
2023-05-03 12:07:51 +0530

    Updated

--- LIC_FAS_JAVA_SACRAL/src/main/java/com/lic/epgs/lead/model/Lead.java
+++ LIC_FAS_JAVA_SACRAL/src/main/java/com/lic/epgs/lead/model/Lead.java
@@ -1,5 +1,7 @@
 package com.lic.epgs.lead.model;
 
+import java.util.Date;
+
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
@@ -8,50 +10,151 @@ import javax.persistence.Id;
 @Entity
 public class Lead {
 
-    @Id
-    @GeneratedValue(strategy= GenerationType.IDENTITY)
-    private Long id;
-    private String name;
-    private String createdBy;
-    private String channel;
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	private Long leadId;
+	private String leadName;
+	private String lmsLeadNo;
+	private String leadContactNo;
+	private String PAN;
+	private String emailId;
+	private Long customerId;
+	private String customerCode;
+	private String leadStatus;
+	private String leadSubStatus;
+	private String workflowStatus;
+	private String unitCode;
+	private String createdBy;
+	private Date createdOn;
+	private Date leadExpiryDate;
+	private String leadNumber;
 
-    public Lead() {}
+	public Long getLeadId() {
+		return leadId;
+	}
 
-    public Lead(String name, String createdBy, String channel) {
-        this.name = name;
-        this.createdBy = createdBy;
-        this.channel = channel;
-    }
+	public void setLeadId(Long leadId) {
+		this.leadId = leadId;
+	}
 
-    public Long getId() {
-        return id;
-    }
+	public String getLeadName() {
+		return leadName;
+	}
 
-    public void setId(Long id) {
-        this.id = id;
-    }
+	public void setLeadName(String leadName) {
+		this.leadName = leadName;
+	}
 
-    public String getName() {
-        return name;
-    }
+	public String getLmsLeadNo() {
+		return lmsLeadNo;
+	}
 
-    public void setName(String name) {
-        this.name = name;
-    }
+	public void setLmsLeadNo(String lmsLeadNo) {
+		this.lmsLeadNo = lmsLeadNo;
+	}
 
-    public String getCreatedBy() {
-        return createdBy;
-    }
+	public String getLeadContactNo() {
+		return leadContactNo;
+	}
 
-    public void setCreatedBy(String createdBy) {
-        this.createdBy = createdBy;
-    }
+	public void setLeadContactNo(String leadContactNo) {
+		this.leadContactNo = leadContactNo;
+	}
 
-    public String getChannel() {
-        return channel;
-    }
+	public String getPAN() {
+		return PAN;
+	}
 
-    public void setChannel(String channel) {
-        this.channel = channel;
-    }
+	public void setPAN(String pAN) {
+		PAN = pAN;
+	}
+
+	public String getEmailId() {
+		return emailId;
+	}
+
+	public void setEmailId(String emailId) {
+		this.emailId = emailId;
+	}
+
+	public Long getCustomerId() {
+		return customerId;
+	}
+
+	public void setCustomerId(Long customerId) {
+		this.customerId = customerId;
+	}
+
+	public String getCustomerCode() {
+		return customerCode;
+	}
+
+	public void setCustomerCode(String customerCode) {
+		this.customerCode = customerCode;
+	}
+
+	public String getLeadStatus() {
+		return leadStatus;
+	}
+
+	public void setLeadStatus(String leadStatus) {
+		this.leadStatus = leadStatus;
+	}
+
+	public String getLeadSubStatus() {
+		return leadSubStatus;
+	}
+
+	public void setLeadSubStatus(String leadSubStatus) {
+		this.leadSubStatus = leadSubStatus;
+	}
+
+	public String getWorkflowStatus() {
+		return workflowStatus;
+	}
+
+	public void setWorkflowStatus(String workflowStatus) {
+		this.workflowStatus = workflowStatus;
+	}
+
+	public String getUnitCode() {
+		return unitCode;
+	}
+
+	public void setUnitCode(String unitCode) {
+		this.unitCode = unitCode;
+	}
+
+	public String getCreatedBy() {
+		return createdBy;
+	}
+
+	public void setCreatedBy(String createdBy) {
+		this.createdBy = createdBy;
+	}
+
+	public Date getCreatedOn() {
+		return createdOn;
+	}
+
+	public void setCreatedOn(Date createdOn) {
+		this.createdOn = createdOn;
+	}
+
+	public Date getLeadExpiryDate() {
+		return leadExpiryDate;
+	}
+
+	public void setLeadExpiryDate(Date leadExpiryDate) {
+		this.leadExpiryDate = leadExpiryDate;
+	}
+
+	public String getLeadNumber() {
+		return leadNumber;
+	}
+
+	public void setLeadNumber(String leadNumber) {
+		this.leadNumber = leadNumber;
+	}
+
 }
