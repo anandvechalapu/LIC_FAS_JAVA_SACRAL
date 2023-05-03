35c7ccb7c11ac54805e5840ffc53a8d1f947d392
anandvechalapu <anand.vechalapu@gmail.com>
2023-05-03 18:13:30 +0530

    initial commit

---
+++ LIC_FAS_JAVA_SACRAL/src/main/java/com/lic/epgs/proposal/proposalmakercontroller/model/MPHContactPersonDetailTempModel.java
@@ -0,0 +1,77 @@
+package com.lic.epgs.proposal.proposalmakercontroller.model;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+public class MPHContactPersonDetailTempModel {
+
+    @Id
+    @GeneratedValue(strategy=GenerationType.AUTO)
+    private Long contactPersonId;
+    private Long proposalId;
+    private String contactPersonName;
+    private String contactPersonEmail;
+    private String contactPersonPhone;
+    private String contactPersonRole;
+    private String contactPersonAddress;
+
+    public Long getContactPersonId() {
+        return contactPersonId;
+    }
+
+    public void setContactPersonId(Long contactPersonId) {
+        this.contactPersonId = contactPersonId;
+    }
+
+    public Long getProposalId() {
+        return proposalId;
+    }
+
+    public void setProposalId(Long proposalId) {
+        this.proposalId = proposalId;
+    }
+
+    public String getContactPersonName() {
+        return contactPersonName;
+    }
+
+    public void setContactPersonName(String contactPersonName) {
+        this.contactPersonName = contactPersonName;
+    }
+
+    public String getContactPersonEmail() {
+        return contactPersonEmail;
+    }
+
+    public void setContactPersonEmail(String contactPersonEmail) {
+        this.contactPersonEmail = contactPersonEmail;
+    }
+
+    public String getContactPersonPhone() {
+        return contactPersonPhone;
+    }
+
+    public void setContactPersonPhone(String contactPersonPhone) {
+        this.contactPersonPhone = contactPersonPhone;
+    }
+
+    public String getContactPersonRole() {
+        return contactPersonRole;
+    }
+
+    public void setContactPersonRole(String contactPersonRole) {
+        this.contactPersonRole = contactPersonRole;
+    }
+
+    public String getContactPersonAddress() {
+        return contactPersonAddress;
+    }
+
+    public void setContactPersonAddress(String contactPersonAddress) {
+        this.contactPersonAddress = contactPersonAddress;
+    }
+
+}
