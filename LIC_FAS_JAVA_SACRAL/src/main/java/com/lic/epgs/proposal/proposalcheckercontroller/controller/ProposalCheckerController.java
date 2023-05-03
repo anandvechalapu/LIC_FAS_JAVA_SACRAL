a4d0a598e58fa3dc42a1ae0d9d6896cfe4355387
anandvechalapu <anand.vechalapu@gmail.com>
2023-05-03 14:48:25 +0530

    Updated

--- LIC_FAS_JAVA_SACRAL/src/main/java/com/lic/epgs/proposal/proposalcheckercontroller/controller/ProposalCheckerController.java
+++ LIC_FAS_JAVA_SACRAL/src/main/java/com/lic/epgs/proposal/proposalcheckercontroller/controller/ProposalCheckerController.java
@@ -1,44 +1,53 @@
 package com.lic.epgs.proposal.proposalcheckercontroller.controller;
 
-import java.util.Optional;
-
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.lic.epgs.proposal.proposalcheckercontroller.dto.ProposalBasicDetailsDto;
 import com.lic.epgs.proposal.proposalcheckercontroller.model.Proposal;
 import com.lic.epgs.proposal.proposalcheckercontroller.service.ProposalCheckerControllerService;
 
 @RestController
-@RequestMapping("/proposals")
 public class ProposalCheckerController {
-    
+
     @Autowired
     private ProposalCheckerControllerService proposalCheckerControllerService;
-    
-    @GetMapping("/{proposalId}")
-    public ResponseEntity<Proposal> getProposal(@PathVariable("proposalId") Long proposalId) {
-        Optional<Proposal> optionalProposal = proposalCheckerControllerService.getProposal(proposalId);
-        if (optionalProposal.isPresent()) {
-            return new ResponseEntity<Proposal>(optionalProposal.get(), HttpStatus.OK);
-        } else {
-            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
-        }
+
+    @PostMapping("/sendToMaker")
+    public ProposalBasicDetailsDto sendToMaker(@RequestBody Proposal proposal) {
+        return proposalCheckerControllerService.sendToMaker(proposal);
     }
-    
-    @PostMapping("/approve")
-    public ResponseEntity<Object> approveProposal(@RequestBody Proposal proposal) {
-        int count = proposalCheckerControllerService.approveProposal(proposal.getProposalId(), proposal.getModifiedBy());
-        if (count == 1) {
-            return new ResponseEntity<>(HttpStatus.OK);
-        } else {
-            return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
-        }
+
+    @PostMapping("/markProposalAsInactive")
+    public void markProposalAsInactive(@RequestBody Proposal proposal) {
+        proposalCheckerControllerService.markProposalAsInactive(proposal);
     }
+
+    @PostMapping("/updateProposalStatusAndWorkflowStatus")
+    public void updateProposalStatusAndWorkflowStatus(@RequestBody Proposal proposal) {
+        proposalCheckerControllerService.updateProposalStatusAndWorkflowStatus(proposal);
+    }
+
+    @PostMapping("/copyExistingProposal")
+    public Proposal copyExistingProposal(@RequestBody Proposal proposal) {
+        return proposalCheckerControllerService.copyExistingProposal(proposal);
+    }
+
+    @PostMapping("/updateMappedFilesForNewProposal")
+    public void updateMappedFilesForNewProposal(@RequestBody Proposal proposal) {
+        proposalCheckerControllerService.updateMappedFilesForNewProposal(proposal);
+    }
+
+    @PostMapping("/getProposalBasicDetails")
+    public ProposalBasicDetailsDto getProposalBasicDetails(@RequestBody Proposal proposal) {
+        return proposalCheckerControllerService.getProposalBasicDetails(proposal);
+    }
+
+    @PostMapping("/returnErrorIfProposalIsInvalid")
+    public void returnErrorIfProposalIsInvalid(@RequestBody Proposal proposal) {
+        proposalCheckerControllerService.returnErrorIfProposalIsInvalid(proposal);
+    }
+
 }
