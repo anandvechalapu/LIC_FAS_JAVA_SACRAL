bb501768acc95d783640062909447dbfa19fbb23
anandvechalapu <anand.vechalapu@gmail.com>
2023-05-03 16:19:15 +0530

    Updated

--- LIC_FAS_JAVA_SACRAL/src/main/java/com/lic/epgs/proposal/proposalmakercontroller/controller/GetProductAndChannelDetailsProposalMakerController.java
+++ LIC_FAS_JAVA_SACRAL/src/main/java/com/lic/epgs/proposal/proposalmakercontroller/controller/GetProductAndChannelDetailsProposalMakerController.java
@@ -1,53 +1,32 @@
 package com.lic.epgs.proposal.proposalmakercontroller.controller;
 
-import java.util.List;
-import java.util.Optional;
-
+import com.lic.epgs.model.ProductAndChannelDetails;
+import com.lic.epgs.proposal.proposalmakercontroller.service.GetProductAndChannelDetailsProposalMakerControllerService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.PutMapping;
-import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.lic.epgs.proposal.proposalmakercontroller.model.Lead;
-import com.lic.epgs.proposal.proposalmakercontroller.service.GetProductAndChannelDetailsProposalMakerControllerService;
+import java.util.List;
 
 @RestController
+@RequestMapping("/proposalmakercontroller/")
 public class GetProductAndChannelDetailsProposalMakerController {
  
     @Autowired
     private GetProductAndChannelDetailsProposalMakerControllerService getProductAndChannelDetailsProposalMakerControllerService;
  
-    @GetMapping("/leads")
-    public List<Lead> findAll() {
-        return getProductAndChannelDetailsProposalMakerControllerService.findAll();
-    }
- 
-    @GetMapping("/leads/{leadId}")
-    public Optional<Lead> findById(@PathVariable Long leadId) {
-        return getProductAndChannelDetailsProposalMakerControllerService.findById(leadId);
-    }
- 
-    @PostMapping("/leads")
-    public Lead save(@RequestBody Lead lead) {
-        return getProductAndChannelDetailsProposalMakerControllerService.save(lead);
-    }
- 
-    @PutMapping("/leads/{leadId}")
-    public Lead update(@PathVariable Long leadId, @RequestBody Lead lead) {
-        return getProductAndChannelDetailsProposalMakerControllerService.save(lead);
-    }
- 
-    @DeleteMapping("/leads/{leadId}")
-    public void deleteById(@PathVariable Long leadId) {
-        getProductAndChannelDetailsProposalMakerControllerService.deleteById(leadId);
-    }
- 
-    @GetMapping("/leads/findByLeadId/{leadId}")
-    public List<Lead> findByLeadId(@PathVariable Long leadId) {
-        return getProductAndChannelDetailsProposalMakerControllerService.findByLeadId(leadId);
+    /**
+     * Retrieve the product and channel details associated with a given lead 
+     * from the database and return them in an organized manner.
+     * 
+     * @param leadId the id of the lead to retrieve product and channel details for
+     * @return the product and channel details associated with the lead
+     * @throws Exception if an error or exception occurs during the retrieval process
+     */
+    @GetMapping("/{leadId}")
+    public List<ProductAndChannelDetails> getProductAndChannelDetails(@PathVariable("leadId") String leadId) throws Exception {
+        return getProductAndChannelDetailsProposalMakerControllerService.getProductAndChannelDetails(leadId);
     }
 }
