9a095f2e9a78216040d27ac566ea0da617a122b9
anandvechalapu <anand.vechalapu@gmail.com>
2023-05-03 16:31:30 +0530

    Updated

--- LIC_FAS_JAVA_SACRAL/src/main/java/com/lic/epgs/proposal/proposalmakercontroller/service/ProductDetailsService.java
+++ LIC_FAS_JAVA_SACRAL/src/main/java/com/lic/epgs/proposal/proposalmakercontroller/service/ProductDetailsService.java
@@ -1,21 +1,21 @@
 package com.lic.epgs.proposal.proposalmakercontroller.service;
 
-import com.lic.epgs.proposal.proposalmakercontroller.dto.ProductDetailsDto;
-import com.lic.epgs.proposal.proposalmakercontroller.entity.ProductDetailsEntity;
-import com.lic.epgs.proposal.proposalmakercontroller.repository.ProductDetailsRepository;
+import java.util.Arrays;
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-import java.util.List;
+import com.lic.epgs.proposal.proposalmakercontroller.dto.ProductDetailsDto;
+import com.lic.epgs.proposal.proposalmakercontroller.repository.ProductDetailsRepository;
 
 @Service
 public class ProductDetailsService {
-
-    @Autowired
-    private ProductDetailsRepository productDetailsRepository;
-
-    public List<ProductDetailsDto> getProductNameList() {
-        return productDetailsRepository.getProductNameList();
-    }
-
+	
+	@Autowired
+	private ProductDetailsRepository productDetailsRepository;
+	
+	public List<ProductDetailsDto> getProductNameList_PROPOSALMAKERCONTROLLER11(){
+		return Arrays.asList(productDetailsRepository.getProductNameList_PROPOSALMAKERCONTROLLER11());
+	}
 }
