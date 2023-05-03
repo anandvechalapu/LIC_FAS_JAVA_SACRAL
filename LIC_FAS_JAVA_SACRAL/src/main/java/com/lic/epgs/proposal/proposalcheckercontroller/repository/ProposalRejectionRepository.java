cd78712c0ddbe3620275144ffafbde37c694d1e4
anandvechalapu <anand.vechalapu@gmail.com>
2023-05-03 14:39:59 +0530

    Updated

--- LIC_FAS_JAVA_SACRAL/src/main/java/com/lic/epgs/proposal/proposalcheckercontroller/repository/ProposalRejectionRepository.java
+++ LIC_FAS_JAVA_SACRAL/src/main/java/com/lic/epgs/proposal/proposalcheckercontroller/repository/ProposalRejectionRepository.java
@@ -1,23 +1,29 @@
+@Repository
+package com.lic.epgs.proposal.proposalcheckercontroller.repository;
+
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import com.lic.epgs.proposal.proposalcheckercontroller.model.ProposalRejection;
 
+@Repository
 public interface ProposalRejectionRepository extends JpaRepository<ProposalRejection, Long> {
-	 
-	ProposalRejection findByProposalId(Long proposalId);
-	 
-	List<ProposalRejection> findByStatusAndWorkFlowStatus(String status, String workFlowStatus); 
-	 
-	void updateProposalStatusAndWorkFlowStatus(String status, String workFlowStatus, Long proposalId); 
-	 
-	void updateProposalRemarks(String remarks, Long proposalId); 
-	 
-	void markProposalInactive(Long proposalId); 
-	 
-	void createNewVersionOfProposal(Long proposalId); 
-	 
-	void updateRelatedTables(Long proposalId); 
-	 
-	void copyNotesToNewProposalVersion(Long proposalId); 
-	 
+    
+	// Find proposal by proposal ID
+    ProposalRejection findByProposalId(String proposalId);
+    
+	// Update proposal status to Rejected and Work Flow Status to Rejected
+    void updateProposalStatusToRejectedAndWorkFlowStatusToRejected(String proposalId);
+    
+	// Update rejection remark and/or remarks for the proposal
+    void updateRejectionRemarkForProposal(String rejectionRemark, String proposalId);
+    
+	// Mark proposal as inactive and create new version with updated status
+    void markProposalAsInactiveAndCreateNewVersionWithUpdatedStatus(String proposalId);
+    
+	// Update proposal in all related tables 
+    void updateProposalInAllRelatedTables(String proposalId);
+    
+	// Copy notes associated with the proposal to new proposal version
+    void copyNotesAssociatedWithProposalToNewProposalVersion(String proposalId);
 }
