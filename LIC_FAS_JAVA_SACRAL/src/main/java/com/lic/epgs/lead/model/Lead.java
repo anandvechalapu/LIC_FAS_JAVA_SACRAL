58a97263652c45b3ef2b3cff9a3ed17805afa59f
anandvechalapu <anand.vechalapu@gmail.com>
2023-05-03 10:59:02 +0530

    Updated

--- LIC_FAS_JAVA_SACRAL/src/main/java/com/lic/epgs/lead/model/Lead.java
+++ LIC_FAS_JAVA_SACRAL/src/main/java/com/lic/epgs/lead/model/Lead.java
@@ -4,84 +4,40 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
-import javax.persistence.Table;
 
 @Entity
-@Table(name = "lead")
 public class Lead {
- 
-    @Id
-    @GeneratedValue(strategy = GenerationType.AUTO)
-    private Long leadId;
- 
-    private String leadName;
- 
-    private String leadStatus;
- 
-    private String modifiedBy;
- 
-    private String transactionStatus;
- 
-    private String transactionMessage;
- 
-    public Lead() {
-    }
- 
-    public Lead(Long leadId, String leadName, String leadStatus, String modifiedBy, String transactionStatus,
-                String transactionMessage) {
-        this.leadId = leadId;
-        this.leadName = leadName;
-        this.leadStatus = leadStatus;
-        this.modifiedBy = modifiedBy;
-        this.transactionStatus = transactionStatus;
-        this.transactionMessage = transactionMessage;
-    }
- 
-    public Long getLeadId() {
-        return leadId;
-    }
- 
-    public void setLeadId(Long leadId) {
-        this.leadId = leadId;
-    }
- 
-    public String getLeadName() {
-        return leadName;
-    }
- 
-    public void setLeadName(String leadName) {
-        this.leadName = leadName;
-    }
- 
-    public String getLeadStatus() {
-        return leadStatus;
-    }
- 
-    public void setLeadStatus(String leadStatus) {
-        this.leadStatus = leadStatus;
-    }
- 
-    public String getModifiedBy() {
-        return modifiedBy;
-    }
- 
-    public void setModifiedBy(String modifiedBy) {
-        this.modifiedBy = modifiedBy;
-    }
- 
-    public String getTransactionStatus() {
-        return transactionStatus;
-    }
- 
-    public void setTransactionStatus(String transactionStatus) {
-        this.transactionStatus = transactionStatus;
-    }
- 
-    public String getTransactionMessage() {
-        return transactionMessage;
-    }
- 
-    public void setTransactionMessage(String transactionMessage) {
-        this.transactionMessage = transactionMessage;
-    }
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+
+	private String unitCode;
+
+	private String createdBy;
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getUnitCode() {
+		return unitCode;
+	}
+
+	public void setUnitCode(String unitCode) {
+		this.unitCode = unitCode;
+	}
+
+	public String getCreatedBy() {
+		return createdBy;
+	}
+
+	public void setCreatedBy(String createdBy) {
+		this.createdBy = createdBy;
+	}
+
 }
