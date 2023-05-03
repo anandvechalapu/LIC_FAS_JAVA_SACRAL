df1086943271ca7fda6baf84531b0582ad6c985b
anandvechalapu <anand.vechalapu@gmail.com>
2023-05-03 16:17:41 +0530

    Updated

--- LIC_FAS_JAVA_SACRAL/src/main/java/com/lic/epgs/proposal/proposalmakercontroller/repository/GetProductAndChannelDetailsProposalMakerControllerRepository.java
+++ LIC_FAS_JAVA_SACRAL/src/main/java/com/lic/epgs/proposal/proposalmakercontroller/repository/GetProductAndChannelDetailsProposalMakerControllerRepository.java
@@ -1,14 +1,13 @@
 @Repository
-public interface GetProductAndChannelDetailsProposalMakerControllerRepository extends CrudRepository<Lead, Long> {
+public interface GetProductAndChannelDetailsProposalMakerControllerRepository {
  
-    List<Lead> findByLeadId(Long leadId);
- 
-    Lead save(Lead lead);
- 
-    List<Lead> findAll();
- 
-    void deleteById(Long leadId);
- 
-    Optional<Lead> findById(Long leadId);
- 
+    /**
+     * Retrieve the product and channel details associated with a given lead 
+     * from the database and return them in an organized manner.
+     * 
+     * @param leadId the id of the lead to retrieve product and channel details for
+     * @return the product and channel details associated with the lead
+     * @throws Exception if an error or exception occurs during the retrieval process
+     */
+    public List<ProductAndChannelDetails> getProductAndChannelDetails(String leadId) throws Exception;
 }
