a1c685fc41a5e47ceefd6c24e67633f73f9276b8
anandvechalapu <anand.vechalapu@gmail.com>
2023-05-03 16:31:54 +0530

    Updated

--- LIC_FAS_JAVA_SACRAL/src/main/java/com/lic/epgs/proposal/proposalmakercontroller/controller/ProductDetailsController.java
+++ LIC_FAS_JAVA_SACRAL/src/main/java/com/lic/epgs/proposal/proposalmakercontroller/controller/ProductDetailsController.java
@@ -1,24 +1,25 @@
 package com.lic.epgs.proposal.proposalmakercontroller.controller;
 
-import com.lic.epgs.proposal.proposalmakercontroller.dto.ProductDetailsDto;
-import com.lic.epgs.proposal.proposalmakercontroller.service.ProductDetailsService;
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.util.List;
+import com.lic.epgs.proposal.proposalmakercontroller.dto.ProductDetailsDto;
+import com.lic.epgs.proposal.proposalmakercontroller.service.ProductDetailsService;
+
 
 @RestController
-@RequestMapping("/proposal")
+@RequestMapping("/proposalmakercontroller")
 public class ProductDetailsController {
 
-    @Autowired
-    private ProductDetailsService productDetailsService;
-
-    @GetMapping("/product-name-list")
-    public List<ProductDetailsDto> getProductNameList() {
-        return productDetailsService.getProductNameList();
-    }
-
+	@Autowired
+	private ProductDetailsService productDetailsService;
+	
+	@GetMapping("/productNameList_PROPOSALMAKERCONTROLLER11")
+	public List<ProductDetailsDto> getProductNameList_PROPOSALMAKERCONTROLLER11(){
+		return productDetailsService.getProductNameList_PROPOSALMAKERCONTROLLER11();
+	}
 }
