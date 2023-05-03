ce832244bbf7ee5102e01de7929932d30c59fd64
anandvechalapu <anand.vechalapu@gmail.com>
2023-05-03 12:13:19 +0530

    initial commit

---
+++ LIC_FAS_JAVA_SACRAL/src/main/java/com/lic/epgs/lead/model/RejectLead.java
@@ -0,0 +1,103 @@
+package com.lic.epgs.lead.model;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+public class RejectLead {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long leadId;
+    
+    private String leadName;
+    
+    private String leadEmail;
+    
+    private String leadPhone;
+    
+    private String productName;
+    
+    private String productType;
+    
+    private String channelName;
+    
+    private String channelType;
+    
+    private String rejectionReason;
+
+    public Long getLeadId() {
+        return leadId;
+    }
+
+    public void setLeadId(Long leadId) {
+        this.leadId = leadId;
+    }
+
+    public String getLeadName() {
+        return leadName;
+    }
+
+    public void setLeadName(String leadName) {
+        this.leadName = leadName;
+    }
+
+    public String getLeadEmail() {
+        return leadEmail;
+    }
+
+    public void setLeadEmail(String leadEmail) {
+        this.leadEmail = leadEmail;
+    }
+
+    public String getLeadPhone() {
+        return leadPhone;
+    }
+
+    public void setLeadPhone(String leadPhone) {
+        this.leadPhone = leadPhone;
+    }
+
+    public String getProductName() {
+        return productName;
+    }
+
+    public void setProductName(String productName) {
+        this.productName = productName;
+    }
+
+    public String getProductType() {
+        return productType;
+    }
+
+    public void setProductType(String productType) {
+        this.productType = productType;
+    }
+
+    public String getChannelName() {
+        return channelName;
+    }
+
+    public void setChannelName(String channelName) {
+        this.channelName = channelName;
+    }
+
+    public String getChannelType() {
+        return channelType;
+    }
+
+    public void setChannelType(String channelType) {
+        this.channelType = channelType;
+    }
+
+    public String getRejectionReason() {
+        return rejectionReason;
+    }
+
+    public void setRejectionReason(String rejectionReason) {
+        this.rejectionReason = rejectionReason;
+    }
+
+}
