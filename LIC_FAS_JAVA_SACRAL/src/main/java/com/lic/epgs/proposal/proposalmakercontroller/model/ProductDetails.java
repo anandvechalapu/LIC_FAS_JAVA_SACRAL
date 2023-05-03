6ebe921581b6148c0493e8d952d28c6151d375a1
anandvechalapu <anand.vechalapu@gmail.com>
2023-05-03 16:31:08 +0530

    Updated

--- LIC_FAS_JAVA_SACRAL/src/main/java/com/lic/epgs/proposal/proposalmakercontroller/model/ProductDetails.java
+++ LIC_FAS_JAVA_SACRAL/src/main/java/com/lic/epgs/proposal/proposalmakercontroller/model/ProductDetails.java
@@ -1,32 +1,33 @@
 package com.lic.epgs.proposal.proposalmakercontroller.model;
 
-import javax.persistence.*;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
 
 @Entity
-@Table(name = "product_details")
 public class ProductDetails {
-
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    @Column(name = "lead_product_id")
-    private Long leadProductId;
-
-    @Column(name = "product_name")
-    private String productName;
-
-    public Long getLeadProductId() {
-        return leadProductId;
-    }
-
-    public void setLeadProductId(Long leadProductId) {
-        this.leadProductId = leadProductId;
-    }
-
-    public String getProductName() {
-        return productName;
-    }
-
-    public void setProductName(String productName) {
-        this.productName = productName;
-    }
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	private Long leadProductId;
+	
+	private String productName;
+	
+	public Long getLeadProductId() {
+		return leadProductId;
+	}
+	
+	public void setLeadProductId(Long leadProductId) {
+		this.leadProductId = leadProductId;
+	}
+	
+	public String getProductName() {
+		return productName;
+	}
+	
+	public void setProductName(String productName) {
+		this.productName = productName;
+	}
+	
 }
