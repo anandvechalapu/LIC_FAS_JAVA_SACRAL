d0e5aeec92b8e3c894d36de665ef005bb7fc76c1
anandvechalapu <anand.vechalapu@gmail.com>
2023-05-04 17:12:55 +0530

    initial commit

---
+++ LIC_FAS_JAVA_SACRAL/src/main/java/com/lic/epgs/trust/model/TrustNotes.java
@@ -0,0 +1,22 @@
+package com.lic.epgs.trust.model;
+public class TrustNotes {
+    private Long trustId;
+    private String trustNote;
+
+    public Long getTrustId() {
+        return trustId;
+    }
+
+    public void setTrustId(Long trustId) {
+        this.trustId = trustId;
+    }
+
+    public String getTrustNote() {
+        return trustNote;
+    }
+
+    public void setTrustNote(String trustNote) {
+        this.trustNote = trustNote;
+    }
+
+}
