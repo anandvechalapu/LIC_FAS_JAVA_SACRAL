fe2db1d16f52a1fdb5e22491622f63d6586aaa32
anandvechalapu <anand.vechalapu@gmail.com>
2023-05-04 12:17:29 +0530

    Updated

--- LIC_FAS_JAVA_SACRAL/src/main/java/com/lic/epgs/subcustomer/subcustomermakercontroller/service/SubCustomerService.java
+++ LIC_FAS_JAVA_SACRAL/src/main/java/com/lic/epgs/subcustomer/subcustomermakercontroller/service/SubCustomerService.java
@@ -1,9 +1,9 @@
 package com.lic.epgs.subcustomer.subcustomermakercontroller.service;
 
-import com.lic.epgs.subcustomer.subcustomermakercontroller.model.SubCustomer;
-import com.lic.epgs.subcustomer.subcustomermakercontroller.repository.SubCustomerRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import com.lic.epgs.subcustomer.subcustomermakercontroller.model.SubCustomer;
+import com.lic.epgs.subcustomer.subcustomermakercontroller.repository.SubCustomerRepository;
 
 @Service
 public class SubCustomerService {
@@ -11,83 +11,36 @@ public class SubCustomerService {
     @Autowired
     private SubCustomerRepository subCustomerRepository;
 
-    public SubCustomer findByCustomerId(Long customerId) {
-        return subCustomerRepository.findByCustomerId(customerId);
+    public SubCustomer findByName(String name) {
+        return subCustomerRepository.findByName(name);
     }
 
-    public void saveSubCustomerContactDetails(SubCustomer subCustomer) {
-        subCustomerRepository.saveSubCustomerContactDetails(subCustomer);
+    public SubCustomer findByCode(String code) {
+        return subCustomerRepository.findByCode(code);
     }
 
-    public void updateSubCustomerContactDetails(SubCustomer subCustomer) {
-        subCustomerRepository.updateSubCustomerContactDetails(subCustomer);
+    public SubCustomer save(SubCustomer subCustomer) {
+        return subCustomerRepository.save(subCustomer);
     }
 
-    public void deleteSubCustomerContactDetails(SubCustomer subCustomer) {
-        subCustomerRepository.deleteSubCustomerContactDetails(subCustomer);
+    public void deleteByName(String name) {
+        subCustomerRepository.deleteByName(name);
     }
 
-    public SubCustomer findByContactType(String contactType) {
-        return subCustomerRepository.findByContactType(contactType);
+    public void deleteByCode(String code) {
+        subCustomerRepository.deleteByCode(code);
     }
 
-    public SubCustomer findByCity(String city) {
-        return subCustomerRepository.findByCity(city);
+    public void updateSubCustomerMappedDetails(String name, String code) {
+        subCustomerRepository.updateSubCustomerMappedDetails(name, code);
     }
 
-    public SubCustomer findByGCTFlag(String GCTFlag) {
-        return subCustomerRepository.findByGCTFlag(GCTFlag);
+    public void generateSubCustomerCode(String name) {
+        subCustomerRepository.generateSubCustomerCode(name);
     }
 
-    public SubCustomer findByDepartmentName(String departmentName) {
-        return subCustomerRepository.findByDepartmentName(departmentName);
+    public void validateSubCustomerName(String name) {
+        subCustomerRepository.validateSubCustomerName(name);
     }
 
-    public SubCustomer findBySalutation(String salutation) {
-        return subCustomerRepository.findBySalutation(salutation);
-    }
-
-    public SubCustomer findByFirstName(String firstName) {
-        return subCustomerRepository.findByFirstName(firstName);
-    }
-
-    public SubCustomer findByMiddleName(String middleName) {
-        return subCustomerRepository.findByMiddleName(middleName);
-    }
-
-    public SubCustomer findByLastName(String lastName) {
-        return subCustomerRepository.findByLastName(lastName);
-    }
-
-    public SubCustomer findByDesignation(String designation) {
-        return subCustomerRepository.findByDesignation(designation);
-    }
-
-    public SubCustomer findByCountryCode(String countryCode) {
-        return subCustomerRepository.findByCountryCode(countryCode);
-    }
-
-    public SubCustomer findByMobileNumber(String mobileNumber) {
-        return subCustomerRepository.findByMobileNumber(mobileNumber);
-    }
-
-    public SubCustomer findByStdCode(String stdCode) {
-        return subCustomerRepository.findByStdCode(stdCode);
-    }
-
-    public SubCustomer findByLandlineNumber(String landlineNumber) {
-        return subCustomerRepository.findByLandlineNumber(landlineNumber);
-    }
-
-    public SubCustomer findByEmailId(String emailId) {
-        return subCustomerRepository.findByEmailId(emailId);
-    }
-
-    public SubCustomer findByChannelColorCode(String channelColorCode) {
-        return subCustomerRepository.findByChannelColorCode(channelColorCode);
-    }
-
-    public SubCustomer findByChannelUserId(String channelUserId) {
-        return subCustomerRepository.findByChannelUserId(channelUserId);
-    }
 }
