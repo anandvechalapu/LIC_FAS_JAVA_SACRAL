3c33ce8e182b7b371be8e84176f73465aa01b420
anandvechalapu <anand.vechalapu@gmail.com>
2023-05-04 10:27:40 +0530

    Updated

--- LIC_FAS_JAVA_SACRAL/src/main/java/com/lic/epgs/subcustomer/subcustomercheckercontroller/model/SubCustomer.java
+++ LIC_FAS_JAVA_SACRAL/src/main/java/com/lic/epgs/subcustomer/subcustomercheckercontroller/model/SubCustomer.java
@@ -1,84 +1,120 @@
-package com.lic.epgs.subcustomer.subcustomercheckercontroller.model; 
- 
-import javax.persistence.Entity; 
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id; 
+package com.lic.epgs.subcustomer.subcustomercheckercontroller.model;
 
-@Entity 
-public class SubCustomer { 
- 
-    @Id 
-    @GeneratedValue(strategy = GenerationType.AUTO) 
-    private Long subCustomerId; 
-    private String firstName; 
-    private String lastName; 
-    private String address; 
-    private String status; 
-    private String workflowStatus; 
- 
-    public SubCustomer() { 
-    } 
- 
-    public SubCustomer(Long subCustomerId, 
-                       String firstName, 
-                       String lastName, 
-                       String address, 
-                       String status, 
-                       String workflowStatus) { 
-        this.subCustomerId = subCustomerId; 
-        this.firstName = firstName; 
-        this.lastName = lastName; 
-        this.address = address; 
-        this.status = status; 
-        this.workflowStatus = workflowStatus; 
-    } 
- 
-    public Long getSubCustomerId() { 
-        return subCustomerId; 
-    } 
- 
-    public void setSubCustomerId(Long subCustomerId) { 
-        this.subCustomerId = subCustomerId; 
-    } 
- 
-    public String getFirstName() { 
-        return firstName; 
-    } 
- 
-    public void setFirstName(String firstName) { 
-        this.firstName = firstName; 
-    } 
- 
-    public String getLastName() { 
-        return lastName; 
-    } 
- 
-    public void setLastName(String lastName) { 
-        this.lastName = lastName; 
-    } 
- 
-    public String getAddress() { 
-        return address; 
-    } 
- 
-    public void setAddress(String address) { 
-        this.address = address; 
-    } 
- 
-    public String getStatus() { 
-        return status; 
-    } 
- 
-    public void setStatus(String status) { 
-        this.status = status; 
-    } 
- 
-    public String getWorkflowStatus() { 
-        return workflowStatus; 
-    } 
- 
-    public void setWorkflowStatus(String workflowStatus) { 
-        this.workflowStatus = workflowStatus; 
-    } 
+import javax.persistence.*;
+import java.util.Date;
+
+@Entity
+@Table(name = "sub_customer")
+public class SubCustomer {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "customer_code")
+    private String customerCode;
+
+    @Column(name = "customer_name")
+    private String customerName;
+
+    @Column(name = "customer_type")
+    private String customerType;
+
+    @Column(name = "pan")
+    private String pan;
+
+    @Column(name = "start_date")
+    private Date startDate;
+
+    @Column(name = "end_date")
+    private Date endDate;
+
+    @Column(name = "role_type")
+    private String roleType;
+
+    @Column(name = "unit_code")
+    private String unitCode;
+
+    @Column(name = "sub_customer_status")
+    private String subCustomerStatus;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getCustomerCode() {
+        return customerCode;
+    }
+
+    public void setCustomerCode(String customerCode) {
+        this.customerCode = customerCode;
+    }
+
+    public String getCustomerName() {
+        return customerName;
+    }
+
+    public void setCustomerName(String customerName) {
+        this.customerName = customerName;
+    }
+
+    public String getCustomerType() {
+        return customerType;
+    }
+
+    public void setCustomerType(String customerType) {
+        this.customerType = customerType;
+    }
+
+    public String getPan() {
+        return pan;
+    }
+
+    public void setPan(String pan) {
+        this.pan = pan;
+    }
+
+    public Date getStartDate() {
+        return startDate;
+    }
+
+    public void setStartDate(Date startDate) {
+        this.startDate = startDate;
+    }
+
+    public Date getEndDate() {
+        return endDate;
+    }
+
+    public void setEndDate(Date endDate) {
+        this.endDate = endDate;
+    }
+
+    public String getRoleType() {
+        return roleType;
+    }
+
+    public void setRoleType(String roleType) {
+        this.roleType = roleType;
+    }
+
+    public String getUnitCode() {
+        return unitCode;
+    }
+
+    public void setUnitCode(String unitCode) {
+        this.unitCode = unitCode;
+    }
+
+    public String getSubCustomerStatus() {
+        return subCustomerStatus;
+    }
+
+    public void setSubCustomerStatus(String subCustomerStatus) {
+        this.subCustomerStatus = subCustomerStatus;
+    }
 }
