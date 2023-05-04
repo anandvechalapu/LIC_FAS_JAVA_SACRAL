8a4c2d2eef8f74c340bcad6034ed49bbf92e5acc
anandvechalapu <anand.vechalapu@gmail.com>
2023-05-04 12:37:54 +0530

    Updated

--- LIC_FAS_JAVA_SACRAL/src/main/java/com/lic/epgs/subcustomer/subcustomermakercontroller/model/Customer.java
+++ LIC_FAS_JAVA_SACRAL/src/main/java/com/lic/epgs/subcustomer/subcustomermakercontroller/model/Customer.java
@@ -1,69 +1,101 @@
 package com.lic.epgs.subcustomer.subcustomermakercontroller.model;
 
+import java.io.Serializable;
+import java.util.Date;
+
+import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.Table;
 
 @Entity
-public class Customer {
+@Table(name = "customer")
+public class Customer implements Serializable {
 
-  @Id
-  @GeneratedValue(strategy = GenerationType.AUTO)
-  private Long id;
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	@Column(name = "id")
+	private Long id;
+	
+	@Column(name = "customer_code")
+	private String customerCode;
+	
+	@Column(name = "customer_name")
+	private String customerName;
+	
+	@Column(name = "customer_type")
+	private String customerType;
+	
+	@Column(name = "pan")
+	private String pan;
+	
+	@Column(name = "created_on")
+	private Date createdOn;
+	
+	@Column(name = "status")
+	private String status;
+	
+	public Long getId() {
+		return id;
+	}
+	
+	public void setId(Long id) {
+		this.id = id;
+	}
+	
+	public String getCustomerCode() {
+		return customerCode;
+	}
+	
+	public void setCustomerCode(String customerCode) {
+		this.customerCode = customerCode;
+	}
+	
+	public String getCustomerName() {
+		return customerName;
+	}
+	
+	public void setCustomerName(String customerName) {
+		this.customerName = customerName;
+	}
+	
+	public String getCustomerType() {
+		return customerType;
+	}
+	
+	public void setCustomerType(String customerType) {
+		this.customerType = customerType;
+	}
+	
+	public String getPan() {
+		return pan;
+	}
+	
+	public void setPan(String pan) {
+		this.pan = pan;
+	}
+	
+	public Date getCreatedOn() {
+		return createdOn;
+	}
+	
+	public void setCreatedOn(Date createdOn) {
+		this.createdOn = createdOn;
+	}
+	
+	public String getStatus() {
+		return status;
+	}
+	
+	public void setStatus(String status) {
+		this.status = status;
+	}
 
-  private String name;
-  private String info;
-  private String latestInfo;
-  private String nonUniqueResult;
-  private String exception;
-  
-  public Long getId() {
-    return id;
-  }
-  
-  public void setId(Long id) {
-    this.id = id;
-  }
-  
-  public String getName() {
-    return name;
-  }
-  
-  public void setName(String name) {
-    this.name = name;
-  }
-  
-  public String getInfo() {
-    return info;
-  }
-  
-  public void setInfo(String info) {
-    this.info = info;
-  }
-  
-  public String getLatestInfo() {
-    return latestInfo;
-  }
-  
-  public void setLatestInfo(String latestInfo) {
-    this.latestInfo = latestInfo;
-  }
-  
-  public String getNonUniqueResult() {
-    return nonUniqueResult;
-  }
-  
-  public void setNonUniqueResult(String nonUniqueResult) {
-    this.nonUniqueResult = nonUniqueResult;
-  }
-  
-  public String getException() {
-    return exception;
-  }
-  
-  public void setException(String exception) {
-    this.exception = exception;
-  }
-  
 }
