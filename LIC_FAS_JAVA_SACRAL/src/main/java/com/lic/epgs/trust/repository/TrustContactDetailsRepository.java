7e8f915edd2c8c3f870ed1e751026f7cac1ed794
anandvechalapu <anand.vechalapu@gmail.com>
2023-05-04 15:37:36 +0530

    Updated

--- LIC_FAS_JAVA_SACRAL/src/main/java/com/lic/epgs/trust/repository/TrustContactDetailsRepository.java
+++ LIC_FAS_JAVA_SACRAL/src/main/java/com/lic/epgs/trust/repository/TrustContactDetailsRepository.java
@@ -1,50 +1,11 @@
-package com.lic.epgs.trust.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-import com.lic.epgs.trust.entity.TrustContactDetails;
-
 @Repository
-public interface TrustContactDetailsRepository extends JpaRepository<TrustContactDetails, Long> {
-
-    /**
-     * Finds trust contact details by trust ID.
-     * 
-     * @param trustId The trust ID.
-     * @return The trust contact details.
-     */
-    TrustContactDetails findByTrustId(Long trustId);
-
-    /**
-     * Finds trust contact details by contact person ID.
-     * 
-     * @param contactPersonId The contact person ID.
-     * @return The trust contact details.
-     */
-    TrustContactDetails findByContactPersonId(Long contactPersonId);
-
-    /**
-     * Saves the trust contact details.
-     * 
-     * @param trustContactDetails The trust contact details.
-     * @return The saved trust contact details.
-     */
-    TrustContactDetails save(TrustContactDetails trustContactDetails);
-
-    /**
-     * Updates the trust contact details.
-     * 
-     * @param trustContactDetails The trust contact details.
-     * @return The updated trust contact details.
-     */
-    TrustContactDetails update(TrustContactDetails trustContactDetails);
-
-    /**
-     * Deletes the trust contact details.
-     * 
-     * @param trustContactDetails The trust contact details.
-     */
-    void delete(TrustContactDetails trustContactDetails);
-
+public interface TrustContactDetailsRepository extends JpaRepository<ContactEntityList, Long> {
+ 
+    CommonTrustDto listTrustContacts(Long trustId);
+ 
+    ContactDetailsDto saveContactDetails(ContactEntityList contactEntityList);
+ 
+    void deleteContactDetails(ContactEntityList contactEntityList);
+ 
+    List<ContactEntityList> findAllByTrustId(Long trustId);
 }
