7fdac0aba1b497258856b2b6d6134f0aa5d60d8a
anandvechalapu <anand.vechalapu@gmail.com>
2023-05-04 15:17:10 +0530

    Updated

--- LIC_FAS_JAVA_SACRAL/src/main/java/com/lic/epgs/trust/controller/TrustManagementController.java
+++ LIC_FAS_JAVA_SACRAL/src/main/java/com/lic/epgs/trust/controller/TrustManagementController.java
@@ -1,34 +1,26 @@
 package com.lic.epgs.trust.controller;
 
-import com.lic.epgs.trust.dto.CommonTrustDto;
-import com.lic.epgs.trust.service.TrustManagementService;
+import java.util.List;
+ 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.lic.epgs.trust.dto.TrustDTO;
+import com.lic.epgs.trust.service.InProgressTrustCodeSearchTRUSTMANAGEMENTCONTROLLER15Service;
 
 @RestController
 @RequestMapping("/trust")
 public class TrustManagementController {
 
-    @Autowired
-    private TrustManagementService trustManagementService;
+@Autowired
+private InProgressTrustCodeSearchTRUSTMANAGEMENTCONTROLLER15Service inProgressTrustCodeSearchTRUSTMANAGEMENTCONTROLLER15Service;
 
-    @GetMapping("/{trustId}")
-    public ResponseEntity<CommonTrustDto> getTrustById(@PathVariable Long trustId) {
-        CommonTrustDto trust = trustManagementService.getTrustById(trustId);
-        return new ResponseEntity<>(trust, HttpStatus.OK);
-    }
-
-    @PostMapping
-    public ResponseEntity<CommonTrustDto> saveTrust(@RequestBody CommonTrustDto trustDto) {
-        trustManagementService.saveTrust(trustDto);
-        return new ResponseEntity<>(trustDto, HttpStatus.CREATED);
-    }
+@GetMapping("/search")
+public List<TrustDTO> searchByTrustCodeAndUnitCodeAndRole(@RequestParam String trustCode, @RequestParam String unitCode, @RequestParam String role) {
+	return inProgressTrustCodeSearchTRUSTMANAGEMENTCONTROLLER15Service.findByTrustCodeAndUnitCodeAndRole(trustCode, unitCode, role);
+}
 
 }
