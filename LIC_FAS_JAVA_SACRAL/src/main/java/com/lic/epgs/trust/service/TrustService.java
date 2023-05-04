81b102097ac42fa7d77a92926016005d7893dee5
anandvechalapu <anand.vechalapu@gmail.com>
2023-05-04 14:39:29 +0530

    Updated

--- LIC_FAS_JAVA_SACRAL/src/main/java/com/lic/epgs/trust/service/TrustService.java
+++ LIC_FAS_JAVA_SACRAL/src/main/java/com/lic/epgs/trust/service/TrustService.java
@@ -2,14 +2,11 @@ package com.lic.epgs.trust.service;
 
 import com.lic.epgs.trust.dto.TrustDetailsDto;
 import com.lic.epgs.trust.entity.BankAccountDetailsEntity;
-import com.lic.epgs.trust.entity.BankAccountDetailsTempEntity;
-import com.lic.epgs.trust.entity.ContactDetailsTempEntity;
+import com.lic.epgs.trust.entity.ContactDetailEntity;
 import com.lic.epgs.trust.entity.TrustDetailsTempEntity;
 import com.lic.epgs.trust.repository.TrustRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-
-import java.util.List;
 
 @Service
 public class TrustService {
@@ -17,28 +14,64 @@ public class TrustService {
     @Autowired
     private TrustRepository trustRepository;
 
-    public TrustDetailsTempEntity findByTrustId(Long trustId) {
+    /**
+     * Retrieves a TrustDetailsTempEntity associated with the trustId from the trustDetailsTempRepository
+     *
+     * @param trustId trust id
+     * @return trust details temp entity
+     */
+    public TrustDetailsTempEntity getTrustDetailsTemp(String trustId) {
         return trustRepository.findByTrustId(trustId);
     }
 
-    public List<BankAccountDetailsTempEntity> findByTrustIdAndIsActive(Long trustId, Boolean isActive) {
-        return trustRepository.findByTrustIdAndIsActive(trustId, isActive);
-    }
-
-    public BankAccountDetailsEntity save(BankAccountDetailsEntity bankAccountDetailsEntity) {
-        return trustRepository.save(bankAccountDetailsEntity);
-    }
-
-    public List<ContactDetailsTempEntity> findContactDetailsTempEntityByTrustIdAndIsActive(Long trustId, Boolean isActive) {
-        return trustRepository.findContactDetailsTempEntityByTrustIdAndIsActive(trustId, isActive);
-    }
-
-    public TrustDetailsTempEntity save(TrustDetailsTempEntity trustDetailsTempEntity) {
+    /**
+     * Saves the modified TrustDetailsTempEntity in the trustDetailsTempRepository
+     *
+     * @param trustDetailsTempEntity trust details temp entity
+     * @return saved trust details temp entity
+     */
+    public TrustDetailsTempEntity saveTrustDetailsTemp(TrustDetailsTempEntity trustDetailsTempEntity) {
         return trustRepository.save(trustDetailsTempEntity);
     }
 
-    public TrustDetailsTempEntity saveTrustDetails(TrustDetailsDto trustDetailsDto, String role) {
-        return trustRepository.saveTrustDetails(trustDetailsDto, role);
+    /**
+     * Retrieves all BankAccountDetailsTempEntity objects associated with the trustId from the bankAccountDetailsTempRepository
+     *
+     * @param trustId trust id
+     * @return list of bank account details temp entity
+     */
+    public Iterable<BankAccountDetailsEntity> getBankAccountDetailsTemp(String trustId) {
+        return trustRepository.findAllByTrustId(trustId);
+    }
+
+    /**
+     * Saves all modified BankAccountDetailsTempEntity objects in the bankAccountDetailsTempRepository
+     *
+     * @param bankAccountDetailsTempEntities list of bank account details temp entity
+     * @return saved list of bank account details temp entity
+     */
+    public Iterable<BankAccountDetailsEntity> saveBankAccountDetailsTemp(Iterable<BankAccountDetailsEntity> bankAccountDetailsTempEntities) {
+        return trustRepository.saveAll(bankAccountDetailsTempEntities);
+    }
+
+    /**
+     * Retrieves all ContactDetailsTempEntity objects associated with the trustId from the contactDetailsTempRepository
+     *
+     * @param trustId trust id
+     * @return list of contact details temp entity
+     */
+    public Iterable<ContactDetailEntity> getContactDetailsTemp(Long trustId) {
+        return trustRepository.findAllByTrustId(trustId);
+    }
+
+    /**
+     * Saves all modified ContactDetailsTempEntity objects in the contactDetailsTempRepository
+     *
+     * @param contactDetailsTempEntities list of contact details temp entity
+     * @return saved list of contact details temp entity
+     */
+    public Iterable<ContactDetailEntity> saveContactDetailsTemp(Iterable<ContactDetailEntity> contactDetailsTempEntities) {
+        return trustRepository.saveAll(contactDetailsTempEntities);
     }
 
 }
