48b5b6185f3a836a7188c88730356d3461812b45
anandvechalapu <anand.vechalapu@gmail.com>
2023-05-04 13:44:46 +0530

    Updated

--- LIC_FAS_JAVA_SACRAL/src/main/java/com/lic/epgs/trust/controller/TrustController.java
+++ LIC_FAS_JAVA_SACRAL/src/main/java/com/lic/epgs/trust/controller/TrustController.java
@@ -1,57 +1,51 @@
-// TrustController.java
-
 package com.lic.epgs.trust.controller;
 
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PutMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
-import com.lic.epgs.trust.model.TrustDetails;
+import com.lic.epgs.trust.dto.TrustDetailsDto;
+import com.lic.epgs.trust.entity.BankAccountDetailsEntity;
+import com.lic.epgs.trust.entity.BankAccountDetailsTempEntity;
+import com.lic.epgs.trust.entity.ContactDetailsTempEntity;
+import com.lic.epgs.trust.entity.TrustDetailsTempEntity;
 import com.lic.epgs.trust.service.TrustService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
-@RequestMapping("/trust")
+@RequestMapping(path = "/trust")
 public class TrustController {
-    
+
     @Autowired
     private TrustService trustService;
-    
-    // Endpoint to retrieve trust details from the temporary table
-    @GetMapping("/{trustId}")
-    public TrustDetails findByTrustId(@PathVariable Long trustId) {
+
+    @GetMapping(path = "/findByTrustId/{trustId}")
+    public TrustDetailsTempEntity findByTrustId(@PathVariable Long trustId) {
         return trustService.findByTrustId(trustId);
     }
-    
-    // Endpoint to retrieve bank account details from the temporary table associated with the trustId
-    @GetMapping("/{trustId}/account/{bankAccountId}")
-    public TrustDetails findByTrustIdAndBankAccountId(@PathVariable Long trustId, @PathVariable Long bankAccountId) {
-        return trustService.findByTrustIdAndBankAccountId(trustId, bankAccountId);
+
+    @GetMapping(path = "/findByTrustIdAndIsActive/{trustId}/{isActive}")
+    public List<BankAccountDetailsTempEntity> findByTrustIdAndIsActive(@PathVariable Long trustId, @PathVariable Boolean isActive) {
+        return trustService.findByTrustIdAndIsActive(trustId, isActive);
     }
-    
-    // Endpoint to retrieve contact details from the temporary table associated with the trustId
-    @GetMapping("/{trustId}/contact/{contactPersonId}")
-    public TrustDetails findByTrustIdAndContactPersonId(@PathVariable Long trustId, @PathVariable Long contactPersonId) {
-        return trustService.findByTrustIdAndContactPersonId(trustId, contactPersonId);
+
+    @PostMapping(path = "/saveBankAccountDetails")
+    public BankAccountDetailsEntity save(@RequestBody BankAccountDetailsEntity bankAccountDetailsEntity) {
+        return trustService.save(bankAccountDetailsEntity);
     }
-    
-    // Endpoint to retrieve address details from the temporary table associated with the trustId
-    @GetMapping("/{trustId}/address/{addressId}")
-    public TrustDetails findByTrustIdAndAddressId(@PathVariable Long trustId, @PathVariable Long addressId) {
-        return trustService.findByTrustIdAndAddressId(trustId, addressId);
+
+    @GetMapping(path = "/findContactDetailsTempEntityByTrustIdAndIsActive/{trustId}/{isActive}")
+    public List<ContactDetailsTempEntity> findContactDetailsTempEntityByTrustIdAndIsActive(@PathVariable Long trustId, @PathVariable Boolean isActive) {
+        return trustService.findContactDetailsTempEntityByTrustIdAndIsActive(trustId, isActive);
     }
-    
-    // Endpoint to save the newly created trust details entity in the temporary table
-    @PutMapping("/save")
-    public TrustDetails save(@RequestBody TrustDetails trustDetails) {
-        return trustService.save(trustDetails);
+
+    @PostMapping(path = "/save")
+    public TrustDetailsTempEntity save(@RequestBody TrustDetailsTempEntity trustDetailsTempEntity) {
+        return trustService.save(trustDetailsTempEntity);
     }
-    
-    // Endpoint to update the trust details entity in the temporary table
-    @PutMapping("/update/{trustId}/{role}")
-    public TrustDetails updateTrustDetails(@PathVariable Long trustId, @PathVariable String role) {
-        return trustService.updateTrustDetails(trustId, role);
+
+    @PostMapping(path = "/saveTrustDetails/{role}")
+    public TrustDetailsTempEntity saveTrustDetails(@RequestBody TrustDetailsDto trustDetailsDto, @PathVariable String role) {
+        return trustService.saveTrustDetails(trustDetailsDto, role);
     }
+
 }
