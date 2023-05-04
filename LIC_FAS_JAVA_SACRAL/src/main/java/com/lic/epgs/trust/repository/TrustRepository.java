86c8bec6e152862abf0e4d33f29345041b43a45f
anandvechalapu <anand.vechalapu@gmail.com>
2023-05-04 17:16:51 +0530

    Updated

--- LIC_FAS_JAVA_SACRAL/src/main/java/com/lic/epgs/trust/repository/TrustRepository.java
+++ LIC_FAS_JAVA_SACRAL/src/main/java/com/lic/epgs/trust/repository/TrustRepository.java
@@ -1,17 +1,25 @@
 package com.lic.epgs.trust.repository;
 
-import java.util.List;
-
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
-import org.springframework.stereotype.Repository;
 
 import com.lic.epgs.trust.model.Trust;
 
-@Repository
 public interface TrustRepository extends JpaRepository<Trust, Long> {
 
-	@Query("SELECT t FROM Trust t WHERE t.trustId = ?1 AND t.unitCode = ?2")
-	List<Trust> getExistingTrustDetails(String trustId, String unitCode);
+	/**
+	 * Saves the trust details in the database and marks it as pending for the approver.
+	 * 
+	 * @param trust The trust object to save
+	 * @return The trust object that was saved
+	 */
+	Trust saveAndSendForApproval(Trust trust);
+
+	/**
+	 * Updates the trust details in the database and marks it as pending for the approver.
+	 * 
+	 * @param trust The trust object to update
+	 * @return The trust object that was updated
+	 */
+	Trust updateAndSendForApproval(Trust trust);
 
 }
