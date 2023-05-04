219569e4e881e5a8a3562a8fa9f6d98527d0d8b4
anandvechalapu <anand.vechalapu@gmail.com>
2023-05-04 15:55:35 +0530

    Updated

--- LIC_FAS_JAVA_SACRAL/src/main/java/com/lic/epgs/trust/model/TrustAddressDetails.java
+++ LIC_FAS_JAVA_SACRAL/src/main/java/com/lic/epgs/trust/model/TrustAddressDetails.java
@@ -1,88 +1,123 @@
 package com.lic.epgs.trust.model;
 
+import java.io.Serializable;
+
+import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.Table;
 
 @Entity
-public class TrustAddressDetails {
+@Table(name="trust_address_details")
+public class TrustAddressDetails implements Serializable {
+
+	private static final long serialVersionUID = 1L;
 	
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
-	private Long addressId;
-	private String modifiedBy;
-	private String createdBy;
-	private Long trustId;
-	private String addressLine1;
-	private String addressLine2;
-	private String city;
-	private String state;
-	private String zipCode;
-	private Boolean isActive;
+	@Column(name="id")
+	private Long id;
 	
-	public Long getAddressId() {
-		return addressId;
+	@Column(name="trust_id")
+	private Long trustId;
+	
+	@Column(name="address_id")
+	private Long addressId;
+	
+	@Column(name="role")
+	private String role;
+	
+	@Column(name="active")
+	private boolean active;
+	
+	@Column(name="created_by")
+	private String createdBy;
+	
+	@Column(name="created_on")
+	private String createdOn;
+	
+	@Column(name="modified_by")
+	private String modifiedBy;
+	
+	@Column(name="modified_on")
+	private String modifiedOn;
+	
+	public TrustAddressDetails() {
+		// Empty constructor
 	}
-	public void setAddressId(Long addressId) {
-		this.addressId = addressId;
+
+	public Long getId() {
+		return id;
 	}
-	public String getModifiedBy() {
-		return modifiedBy;
+
+	public void setId(Long id) {
+		this.id = id;
 	}
-	public void setModifiedBy(String modifiedBy) {
-		this.modifiedBy = modifiedBy;
-	}
-	public String getCreatedBy() {
-		return createdBy;
-	}
-	public void setCreatedBy(String createdBy) {
-		this.createdBy = createdBy;
-	}
+
 	public Long getTrustId() {
 		return trustId;
 	}
+
 	public void setTrustId(Long trustId) {
 		this.trustId = trustId;
 	}
-	public String getAddressLine1() {
-		return addressLine1;
+
+	public Long getAddressId() {
+		return addressId;
 	}
-	public void setAddressLine1(String addressLine1) {
-		this.addressLine1 = addressLine1;
+
+	public void setAddressId(Long addressId) {
+		this.addressId = addressId;
 	}
-	public String getAddressLine2() {
-		return addressLine2;
+
+	public String getRole() {
+		return role;
 	}
-	public void setAddressLine2(String addressLine2) {
-		this.addressLine2 = addressLine2;
+
+	public void setRole(String role) {
+		this.role = role;
 	}
-	public String getCity() {
-		return city;
+
+	public boolean isActive() {
+		return active;
 	}
-	public void setCity(String city) {
-		this.city = city;
+
+	public void setActive(boolean active) {
+		this.active = active;
 	}
-	public String getState() {
-		return state;
+
+	public String getCreatedBy() {
+		return createdBy;
 	}
-	public void setState(String state) {
-		this.state = state;
+
+	public void setCreatedBy(String createdBy) {
+		this.createdBy = createdBy;
 	}
-	public String getZipCode() {
-		return zipCode;
+
+	public String getCreatedOn() {
+		return createdOn;
 	}
-	public void setZipCode(String zipCode) {
-		this.zipCode = zipCode;
+
+	public void setCreatedOn(String createdOn) {
+		this.createdOn = createdOn;
 	}
-	public Boolean getIsActive() {
-		return isActive;
+
+	public String getModifiedBy() {
+		return modifiedBy;
 	}
-	public void setIsActive(Boolean isActive) {
-		this.isActive = isActive;
+
+	public void setModifiedBy(String modifiedBy) {
+		this.modifiedBy = modifiedBy;
 	}
-	
-	
-	
+
+	public String getModifiedOn() {
+		return modifiedOn;
+	}
+
+	public void setModifiedOn(String modifiedOn) {
+		this.modifiedOn = modifiedOn;
+	}
 
 }
