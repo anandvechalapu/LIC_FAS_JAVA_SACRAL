6476a093650cd4907427af59c9e6b1d35d3befbf
anandvechalapu <anand.vechalapu@gmail.com>
2023-05-04 10:20:31 +0530

    Updated

--- LIC_FAS_JAVA_SACRAL/src/main/java/com/lic/epgs/subcustomer/subcustomercheckercontroller/controller/SubCustomerCheckerController.java
+++ LIC_FAS_JAVA_SACRAL/src/main/java/com/lic/epgs/subcustomer/subcustomercheckercontroller/controller/SubCustomerCheckerController.java
@@ -1,40 +1,50 @@
-package com.lic.epgs.subcustomer.subcustomercheckercontroller.controller;
+package com.lic.epgs.subcustomer.subcustomercheckercontroller.controller; 
 
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PutMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.beans.factory.annotation.Autowired; 
+import org.springframework.web.bind.annotation.GetMapping; 
+import org.springframework.web.bind.annotation.PostMapping; 
+import org.springframework.web.bind.annotation.RequestBody; 
+import org.springframework.web.bind.annotation.RequestMapping; 
+import org.springframework.web.bind.annotation.RequestParam; 
+import org.springframework.web.bind.annotation.RestController; 
 
-import com.lic.epgs.subcustomer.subcustomercheckercontroller.model.SubCustomer;
-import com.lic.epgs.subcustomer.subcustomercheckercontroller.service.SubCustomerCheckerControllerService;
+import com.lic.epgs.subcustomer.subcustomercheckercontroller.model.SubCustomer; 
+import com.lic.epgs.subcustomer.subcustomercheckercontroller.service.SubCustomerCheckerControllerService; 
 
-@RestController
-@RequestMapping("/subcustomer")
-public class SubCustomerCheckerController {
-
-    @Autowired
-    private SubCustomerCheckerControllerService service;
-
-    @GetMapping("/")
-    public ResponseEntity<SubCustomer> getSubCustomerByCustomerId(@RequestParam("customerId") Long customerId) {
-        SubCustomer subCustomer = service.findSubCustomerByCustomerId(customerId);
-        if(subCustomer == null) {
-            return ResponseEntity.notFound().build();
-        }
-        return ResponseEntity.ok(subCustomer);
-    }
-
-    @PutMapping("/")
-    public ResponseEntity<SubCustomer> updateSubCustomer(@RequestBody SubCustomer subCustomer) {
-        SubCustomer updatedSubCustomer = service.updateSubCustomer(subCustomer.getAddress(), subCustomer.getContact(), subCustomer.getNotes(), subCustomer.getBankAccountDetails(), subCustomer.getCustomerId());
-        if(updatedSubCustomer == null) {
-            return ResponseEntity.notFound().build();
-        }
-        return ResponseEntity.ok(updatedSubCustomer);
-    }
-
+@RestController 
+@RequestMapping("/api/subcustomerchecker") 
+public class SubCustomerCheckerController { 
+ 
+    @Autowired 
+    private SubCustomerCheckerControllerService subCustomerCheckerControllerService; 
+ 
+    @GetMapping("/validate") 
+    public boolean validateSubCustomerId(@RequestParam Long subCustomerId) { 
+        return subCustomerCheckerControllerService.validateSubCustomerId(subCustomerId); 
+    } 
+ 
+    @PostMapping("/setstatus") 
+    public boolean setSubCustomerStatusAndWorkflowStatus(@RequestParam Long subCustomerId, 
+                                                         @RequestParam String newStatus, 
+                                                         @RequestParam String newWorkflowStatus) { 
+        return subCustomerCheckerControllerService.setSubCustomerStatusAndWorkflowStatus(subCustomerId, newStatus, newWorkflowStatus); 
+    } 
+ 
+    @PostMapping("/updatedetails") 
+    public boolean updateSubCustomerDetails(@RequestParam Long subCustomerId, 
+                                            @RequestParam String newFirstName, 
+                                            @RequestParam String newLastName, 
+                                            @RequestParam String newAddress) { 
+        return subCustomerCheckerControllerService.updateSubCustomerDetails(subCustomerId, newFirstName, newLastName, newAddress); 
+    } 
+ 
+    @PostMapping("/updateresponse") 
+    public boolean updateSubCustomerResponse(@RequestParam Long subCustomerId) { 
+        return subCustomerCheckerControllerService.updateSubCustomerResponse(subCustomerId); 
+    } 
+ 
+    @PostMapping("/create") 
+    public SubCustomer createSubCustomer(@RequestBody SubCustomer subCustomer) { 
+        return subCustomerCheckerControllerService.createSubCustomer(subCustomer); 
+    } 
 }
