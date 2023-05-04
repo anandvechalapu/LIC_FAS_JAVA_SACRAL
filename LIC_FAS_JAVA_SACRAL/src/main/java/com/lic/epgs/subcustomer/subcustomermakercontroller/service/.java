59845eebddbab5d29bcacc2c8c2292cf5fc1bfa3
anandvechalapu <anand.vechalapu@gmail.com>
2023-05-04 12:25:08 +0530

    Updated

--- LIC_FAS_JAVA_SACRAL/src/main/java/com/lic/epgs/subcustomer/subcustomermakercontroller/service/.java
+++ LIC_FAS_JAVA_SACRAL/src/main/java/com/lic/epgs/subcustomer/subcustomermakercontroller/service/.java
@@ -1,25 +1,13 @@
 package com.lic.epgs.subcustomer.subcustomermakercontroller.service;
 
+import com.lic.epgs.subcustomer.subcustomermakercontroller.model.CustomerUnitOfficeTempEntity;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.time.LocalDateTime;
 import java.util.List;
 
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
-import org.springframework.data.repository.query.Param;
-import org.springframework.stereotype.Repository;
-
-import com.lic.epgs.subcustomer.subcustomermakercontroller.entity.LoadInformation;
-
-@Repository
-public interface SubcustomerMakercontrollerService {
-	
-	List<LoadInformation> findByRoleTypeAndUnitCode(String roleType, String unitCode);
-	
-	List<LoadInformation> getExistingLoads(String roleType, String unitCode);
-	
-	List<LoadInformation> getExistingLoadsNative(String roleType, String unitCode);
-	
-	List<LoadInformation> getExistingLoadsWithCustomerInfo(String roleType, String unitCode);
-	
-	int getExistingLoadCount(String roleType, String unitCode);
-	
+public interface SaveUnitDetails_SUBCUSTOMERMAKERCONTROLLER18Service {
+    CustomerUnitOfficeTempEntity findByCustomerIdAndSubCustomerIdAndSourceUnitIdAndChannelColorCodeAndChannelUserIdAndFromDateAndToDateAndCreatedByAndModifiedByAndActiveState(Long customerId, Long subCustomerId, String sourceUnitId, String channelColorCode, String channelUserId, LocalDateTime fromDate, LocalDateTime toDate, String createdBy, String modifiedBy, Boolean activeState);
+    List<CustomerUnitOfficeTempEntity> findByCustomerIdAndSubCustomerIdAndSourceUnitIdAndChannelColorCodeAndChannelUserIdAndFromDateAndToDateAndCreatedByAndModifiedByAndActiveStateIn(Long customerId, Long subCustomerId, String sourceUnitId, String channelColorCode, String channelUserId, LocalDateTime fromDate, LocalDateTime toDate, String createdBy, String modifiedBy, Boolean[] activeState);
+    List<CustomerUnitOfficeTempEntity> saveAll(List<CustomerUnitOfficeTempEntity> customerUnitOfficeTempEntities);
 }
