39bed95f965bdc1a2f512e5d13b67f1ac95d199e
anandvechalapu <anand.vechalapu@gmail.com>
2023-05-04 11:54:58 +0530

    Updated

--- LIC_FAS_JAVA_SACRAL/src/main/java/com/lic/epgs/subcustomer/subcustomermakercontroller/repository/SubcustomerMakercontrollerRepository.java
+++ LIC_FAS_JAVA_SACRAL/src/main/java/com/lic/epgs/subcustomer/subcustomermakercontroller/repository/SubcustomerMakercontrollerRepository.java
@@ -1,18 +1,21 @@
+package com.lic.epgs.subcustomer.subcustomermakercontroller.repository;
+
+import com.lic.epgs.subcustomer.subcustomermakercontroller.dto.AddressDetailsDto;
+import com.lic.epgs.subcustomer.subcustomermakercontroller.entity.AddressDetailsTempEntity;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
 @Repository
-public interface SubcustomerMakercontrollerRepository extends JpaRepository<LoadInformation, Long> {
-	
-	List<LoadInformation> findByRoleTypeAndUnitCode(String roleType, String unitCode);
-	
-	@Query("FROM LoadInformation l WHERE l.roleType = :roleType and l.unitCode = :unitCode")
-	List<LoadInformation> getExistingLoads(@Param("roleType") String roleType, @Param("unitCode") String unitCode);
-	
-	@Query(value = "SELECT * FROM LOAD_INFORMATION WHERE ROLE_TYPE = :roleType AND UNIT_CODE = :unitCode", nativeQuery = true)
-	List<LoadInformation> getExistingLoadsNative(@Param("roleType") String roleType, @Param("unitCode") String unitCode);
-	
-	@Query("FROM LoadInformation l WHERE l.roleType = :roleType and l.unitCode = :unitCode and l.customerInformation is not null")
-	List<LoadInformation> getExistingLoadsWithCustomerInfo(@Param("roleType") String roleType, @Param("unitCode") String unitCode);
-	
-	@Query("SELECT COUNT(*) FROM LoadInformation l WHERE l.roleType = :roleType and l.unitCode = :unitCode")
-	int getExistingLoadCount(@Param("roleType") String roleType, @Param("unitCode") String unitCode);
-	
+public interface SubcustomerMakercontrollerRepository extends JpaRepository<AddressDetailsTempEntity, Long> {
+
+    @Modifying
+    @Query("UPDATE AddressDetailsTempEntity a SET a.subcustomerId = ?1, a.addressType = ?2, a.addressTypeName = ?3, a.country = ?4, a.state = ?5, a.district = ?6, a.addressLine1 = ?7, a.addressLine2 = ?8, a.addressLine3 = ?9, a.town = ?10, a.locality = ?11, a.pin = ?12, a.gcFlag = ?13, a.printFlag = ?14, a.createdBy = ?15, a.modifiedBy = ?16 WHERE a.id = ?17")
+    int updateSubCustomerAddressDetails(Long subcustomerId, String addressType, String addressTypeName, String country, String state, String district, String addressLine1, String addressLine2, String addressLine3, String town, String locality, String pin, String gcFlag, String printFlag, String createdBy, String modifiedBy, Long id);
+
+    Optional<AddressDetailsDto> findBySubcustomerId(Long subcustomerId);
+
 }
