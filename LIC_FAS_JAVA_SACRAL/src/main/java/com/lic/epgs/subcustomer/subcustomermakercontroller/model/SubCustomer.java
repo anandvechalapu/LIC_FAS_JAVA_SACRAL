bdf8389288477a1945ed71159ca0ad4a8314673a
anandvechalapu <anand.vechalapu@gmail.com>
2023-05-04 12:16:49 +0530

    Updated

--- LIC_FAS_JAVA_SACRAL/src/main/java/com/lic/epgs/subcustomer/subcustomermakercontroller/model/SubCustomer.java
+++ LIC_FAS_JAVA_SACRAL/src/main/java/com/lic/epgs/subcustomer/subcustomermakercontroller/model/SubCustomer.java
@@ -1,167 +1,56 @@
 package com.lic.epgs.subcustomer.subcustomermakercontroller.model;
 
-import javax.persistence.Column;
 import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.Table;
 
 @Entity
-@Table(name="subcustomer")
+@Table(name = "subcustomer")
 public class SubCustomer {
-  
     @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    @Column(name = "customer_id")
-    private Long customerId;
-
-    @Column(name = "contact_type")
-    private String contactType;
-
-    @Column(name = "city")
-    private String city;
-
-    @Column(name = "GCT_flag")
-    private String GCTFlag;
-
-    @Column(name = "department_name")
-    private String departmentName;
-
-    @Column(name = "salutation")
-    private String salutation;
-
-    @Column(name = "first_name")
-    private String firstName;
-
-    @Column(name = "middle_name")
-    private String middleName;
-
-    @Column(name = "last_name")
-    private String lastName;
-
-    @Column(name = "designation")
-    private String designation;
-
-    @Column(name = "country_code")
-    private String countryCode;
-
-    @Column(name = "mobile_number")
-    private String mobileNumber;
-
-    @Column(name = "std_code")
-    private String stdCode;
-
-    @Column(name = "landline_number")
-    private String landlineNumber;
-
-    @Column(name = "email_id")
-    private String emailId;
-
-    @Column(name = "channel_color_code")
-    private String channelColorCode;
-
-    @Column(name = "channel_user_id")
-    private String channelUserId;
-
-    public Long getCustomerId() {
-        return customerId;
+    private Long id;
+    private String name;
+    private String code;
+    private String mappedDetails;
+    
+    public SubCustomer() {}
+    
+    public SubCustomer(Long id, String name, String code, String mappedDetails) {
+        this.id = id;
+        this.name = name;
+        this.code = code;
+        this.mappedDetails = mappedDetails;
     }
-
-    public void setCustomerId(Long customerId) {
-        this.customerId = customerId;
+    
+    public Long getId() {
+        return id;
     }
-
-    public String getContactType() {
-        return contactType;
+    
+    public void setId(Long id) {
+        this.id = id;
     }
-
-    public void setContactType(String contactType) {
-        this.contactType = contactType;
+    
+    public String getName() {
+        return name;
     }
-
-    public String getCity() {
-        return city;
+    
+    public void setName(String name) {
+        this.name = name;
     }
-
-    public void setCity(String city) {
-        this.city = city;
+    
+    public String getCode() {
+        return code;
     }
-
-    public String getGCTFlag() {
-        return GCTFlag;
+    
+    public void setCode(String code) {
+        this.code = code;
     }
-
-    public void setGCTFlag(String GCTFlag) {
-        this.GCTFlag = GCTFlag;
+    
+    public String getMappedDetails() {
+        return mappedDetails;
     }
-
-    public String getDepartmentName() {
-        return departmentName;
+    
+    public void setMappedDetails(String mappedDetails) {
+        this.mappedDetails = mappedDetails;
     }
-
-    public void setDepartmentName(String departmentName) {
-        this.departmentName = departmentName;
-    }
-
-    public String getSalutation() {
-        return salutation;
-    }
-
-    public void setSalutation(String salutation) {
-        this.salutation = salutation;
-    }
-
-    public String getFirstName() {
-        return firstName;
-    }
-
-    public void setFirstName(String firstName) {
-        this.firstName = firstName;
-    }
-
-    public String getMiddleName() {
-        return middleName;
-    }
-
-    public void setMiddleName(String middleName) {
-        this.middleName = middleName;
-    }
-
-    public String getLastName() {
-        return lastName;
-    }
-
-    public void setLastName(String lastName) {
-        this.lastName = lastName;
-    }
-
-    public String getDesignation() {
-        return designation;
-    }
-
-    public void setDesignation(String designation) {
-        this.designation = designation;
-    }
-
-    public String getCountryCode() {
-        return countryCode;
-    }
-
-    public void setCountryCode(String countryCode) {
-        this.countryCode = countryCode;
-    }
-
-    public String getMobileNumber() {
-        return mobileNumber;
-    }
-
-    public void setMobileNumber(String mobileNumber) {
-        this.mobileNumber = mobileNumber;
-    }
-
-    public String getStdCode() {
-        return stdCode;
-    }
-
-    public void setStdCode(String stdCode
+}
