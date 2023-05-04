66331a41fe980e4804dd92b20a24b46d5f075575
anandvechalapu <anand.vechalapu@gmail.com>
2023-05-04 12:43:08 +0530

    Updated

--- LIC_FAS_JAVA_SACRAL/src/main/java/com/lic/epgs/subcustomer/subcustomermakercontroller/service/SubCustomerMakerControllerService.java
+++ LIC_FAS_JAVA_SACRAL/src/main/java/com/lic/epgs/subcustomer/subcustomermakercontroller/service/SubCustomerMakerControllerService.java
@@ -1,21 +1,29 @@
 package com.lic.epgs.subcustomer.subcustomermakercontroller.service;
 
-import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-import com.lic.epgs.subcustomer.subcustomermakercontroller.repository.SubCustomerMakerControllerRepository;
 import com.lic.epgs.subcustomer.subcustomermakercontroller.model.SubCustomerMakerController;
+import com.lic.epgs.subcustomer.subcustomermakercontroller.repository.SubCustomerMakerControllerRepository;
 
 @Service
 public class SubCustomerMakerControllerService {
-
+    
 	@Autowired
 	private SubCustomerMakerControllerRepository subCustomerMakerControllerRepository;
-
-	public List<SubCustomerMakerController> getMakerNotesBySubCustomerId(long subCustomerId) {
-		return subCustomerMakerControllerRepository.getMakerNotesBySubCustomerId(subCustomerId);
+	
+	public Optional<SubCustomerMakerController> findByStatusAndWorkflowStatus(String status, String workflowStatus) {
+		return subCustomerMakerControllerRepository.findByStatusAndWorkflowStatus(status, workflowStatus);
 	}
-
+	
+	public void updateStatusById(String status, Long id) {
+		subCustomerMakerControllerRepository.updateStatusById(status, id);
+	}
+	
+	public void updateMappedDetailsById(String mappedDetails, Long id) {
+		subCustomerMakerControllerRepository.updateMappedDetailsById(mappedDetails, id);
+	}
+	
 }
