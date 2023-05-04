fd191a59415a64a81f26856061e6b1e1b3f41500
anandvechalapu <anand.vechalapu@gmail.com>
2023-05-04 15:46:49 +0530

    Updated

--- LIC_FAS_JAVA_SACRAL/src/main/java/com/lic/epgs/trust/repository/TrustDocumentDetailsRepository.java
+++ LIC_FAS_JAVA_SACRAL/src/main/java/com/lic/epgs/trust/repository/TrustDocumentDetailsRepository.java
@@ -1,3 +1,4 @@
+@Repository
 package com.lic.epgs.trust.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
@@ -7,17 +8,5 @@ import com.lic.epgs.trust.model.TrustDocumentDetails;
 
 @Repository
 public interface TrustDocumentDetailsRepository extends JpaRepository<TrustDocumentDetails, Long> {
-
-    //method to add document details to a trust
-    TrustDocumentDetails addDocumentDetailsToTrust(TrustDocumentDetails trustDocumentDetails);
-    
-    //method to update or remove document details associated with a trust
-    TrustDocumentDetails updateOrRemoveDocumentDetails(TrustDocumentDetails trustDocumentDetails);
-    
-    //method to provide a response to the user indicating the success or failure of the request to add, update or remove document details
-    TrustDocumentDetails getResponseForDocumentDetailsRequest(TrustDocumentDetails trustDocumentDetails);
-    
-    //method to log all requests to add, update or remove document details
-    TrustDocumentDetails logDocumentDetailsRequest(TrustDocumentDetails trustDocumentDetails);
-    
+  List<TrustDocumentDetails> findByTrustId(Long trustId);
 }
