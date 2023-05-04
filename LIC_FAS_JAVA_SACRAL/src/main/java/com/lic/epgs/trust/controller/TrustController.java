43c54488d77c67e875e7f46bc407829fa040af02
anandvechalapu <anand.vechalapu@gmail.com>
2023-05-04 17:19:05 +0530

    Updated

--- LIC_FAS_JAVA_SACRAL/src/main/java/com/lic/epgs/trust/controller/TrustController.java
+++ LIC_FAS_JAVA_SACRAL/src/main/java/com/lic/epgs/trust/controller/TrustController.java
@@ -1,33 +1,28 @@
 package com.lic.epgs.trust.controller;
 
-import java.util.List;
-
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.lic.epgs.trust.model.Trust;
 import com.lic.epgs.trust.service.TrustService;
 
 @RestController
-@RequestMapping(value = "/trust")
 public class TrustController {
 
 	@Autowired
 	private TrustService trustService;
 
-	@GetMapping
-	public ResponseEntity<List<Trust>> getExistingTrustDetails(@RequestParam String trustId,
-			@RequestParam String unitCode) {
-		List<Trust> existingTrustDetails = trustService.getExistingTrustDetails(trustId, unitCode);
-		if (existingTrustDetails != null) {
-			return new ResponseEntity<>(existingTrustDetails, HttpStatus.OK);
-		}
-		return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+	@PostMapping("/trust/save")
+	public Trust saveAndSendForApproval(@RequestBody Trust trust) {
+		return trustService.saveAndSendForApproval(trust);
+	}
+
+	@PutMapping("/trust/update")
+	public Trust updateAndSendForApproval(@RequestBody Trust trust) {
+		return trustService.updateAndSendForApproval(trust);
 	}
 
 }
