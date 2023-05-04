a595ada9641d92ceb15a3d50bcb0509ddea692d9
anandvechalapu <anand.vechalapu@gmail.com>
2023-05-04 10:11:37 +0530

    Updated

--- LIC_FAS_JAVA_SACRAL/src/main/java/com/lic/epgs/subcustomer/subcustomercheckercontroller/service/SubCustomerCheckerControllerService.java
+++ LIC_FAS_JAVA_SACRAL/src/main/java/com/lic/epgs/subcustomer/subcustomercheckercontroller/service/SubCustomerCheckerControllerService.java
@@ -2,23 +2,18 @@ package com.lic.epgs.subcustomer.subcustomercheckercontroller.service;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-import org.springframework.transaction.annotation.Transactional;
 
-import com.lic.epgs.subcustomer.subcustomercheckercontroller.model.SubCustomer;
+import com.lic.epgs.subcustomer.subcustomercheckercontroller.entity.SubCustomerCheckerController;
 import com.lic.epgs.subcustomer.subcustomercheckercontroller.repository.SubCustomerCheckerControllerRepository;
 
 @Service
-@Transactional
 public class SubCustomerCheckerControllerService {
 
     @Autowired
-    private SubCustomerCheckerControllerRepository repo;
+    private SubCustomerCheckerControllerRepository subCustomerCheckerControllerRepository;
 
-    public SubCustomer findSubCustomerByCustomerId(Long customerId) {
-        return repo.findSubCustomerByCustomerId(customerId);
+    public SubCustomerCheckerController getExitingSubCustomerDetails(String subCustomerId) {
+        return subCustomerCheckerControllerRepository.getExitingSubCustomerDetails(subCustomerId);
     }
 
-    public void updateSubCustomer(String address, String contact, String notes, String bankAccountDetails, Long customerId) {
-        repo.updateSubCustomer(address, contact, notes, bankAccountDetails, customerId);
-    }
 }
