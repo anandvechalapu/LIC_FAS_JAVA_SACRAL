dcff3a1f9de8243119ce93116e5b59eebe1c0d61
anandvechalapu <anand.vechalapu@gmail.com>
2023-05-04 17:26:46 +0530

    Updated

--- LIC_FAS_JAVA_SACRAL/src/main/java/com/lic/epgs/trust/service/TrustManagementService.java
+++ LIC_FAS_JAVA_SACRAL/src/main/java/com/lic/epgs/trust/service/TrustManagementService.java
@@ -1,21 +1,45 @@
 package com.lic.epgs.trust.service;
 
-import com.lic.epgs.trust.dto.CommonTrustDto;
-import com.lic.epgs.trust.repository.TrustManagementRepository;
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import com.lic.epgs.trust.model.TrustData;
+import com.lic.epgs.trust.repository.TrustManagementRepository;
+
 @Service
 public class TrustManagementService {
-
-    @Autowired
-    private TrustManagementRepository trustManagementRepository;
-
-    public CommonTrustDto getTrustById(Long trustId) {
-        return trustManagementRepository.findByTrustId(trustId);
-    }
-
-    public void saveTrust(CommonTrustDto trustDto) {
-        trustManagementRepository.save(trustDto);
-    }
+	
+	@Autowired
+	TrustManagementRepository trustManagementRepository;
+	
+	public TrustData findByInactiveStatus(Boolean inactiveStatus) {
+		return trustManagementRepository.findByInactiveStatus(inactiveStatus);
+	}
+	
+	public TrustData findByPendingForModification(Boolean pendingForModification) {
+		return trustManagementRepository.findByPendingForModification(pendingForModification);
+	}
+	
+	public TrustData findByBankAccountDetailsAndContactDetailsAndAddressDetailsAndDocumentDetails(String bankAccountDetails, String contactDetails, String addressDetails, String documentDetails) {
+		return trustManagementRepository.findByBankAccountDetailsAndContactDetailsAndAddressDetailsAndDocumentDetails(bankAccountDetails, contactDetails, addressDetails, documentDetails);
+	}
+	
+	public TrustData findByModifiedByUserAndModifiedOnDate(String modifiedByUser, String modifiedOnDate) {
+		return trustManagementRepository.findByModifiedByUserAndModifiedOnDate(modifiedByUser, modifiedOnDate);
+	}
+	
+	public void sendTrustDataToMaker(TrustData trustData) {
+		trustManagementRepository.save(trustData);
+	}
+	
+	public List<TrustData> getAllTrustData() {
+		return trustManagementRepository.findAll();
+	}
+	
+	public String getSuccessMessage() {
+		return trustManagementRepository.getSuccessMessage();
+	}
+	
 }
