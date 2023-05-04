cf82bfb2078baf4f19f7865ca5db6c288e644115
anandvechalapu <anand.vechalapu@gmail.com>
2023-05-04 10:19:50 +0530

    Updated

--- LIC_FAS_JAVA_SACRAL/src/main/java/com/lic/epgs/subcustomer/subcustomercheckercontroller/service/SubCustomerCheckerControllerService.java
+++ LIC_FAS_JAVA_SACRAL/src/main/java/com/lic/epgs/subcustomer/subcustomercheckercontroller/service/SubCustomerCheckerControllerService.java
@@ -1,19 +1,39 @@
-package com.lic.epgs.subcustomer.subcustomercheckercontroller.service;
+package com.lic.epgs.subcustomer.subcustomercheckercontroller.service; 
 
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Service;
+import org.springframework.beans.factory.annotation.Autowired; 
+import org.springframework.stereotype.Service; 
 
-import com.lic.epgs.subcustomer.subcustomercheckercontroller.entity.SubCustomerCheckerController;
-import com.lic.epgs.subcustomer.subcustomercheckercontroller.repository.SubCustomerCheckerControllerRepository;
+import com.lic.epgs.subcustomer.subcustomercheckercontroller.repository.SubCustomerCheckerControllerRepository; 
+import com.lic.epgs.subcustomer.SubCustomer; 
 
-@Service
-public class SubCustomerCheckerControllerService {
-
-    @Autowired
-    private SubCustomerCheckerControllerRepository subCustomerCheckerControllerRepository;
-
-    public SubCustomerCheckerController getExitingSubCustomerDetails(String subCustomerId) {
-        return subCustomerCheckerControllerRepository.getExitingSubCustomerDetails(subCustomerId);
-    }
-
+@Service 
+public class SubCustomerCheckerControllerService { 
+ 
+    @Autowired 
+    private SubCustomerCheckerControllerRepository subCustomerCheckerControllerRepository; 
+ 
+    public boolean validateSubCustomerId(Long subCustomerId) { 
+        return subCustomerCheckerControllerRepository.validateSubCustomerId(subCustomerId); 
+    } 
+ 
+    public boolean setSubCustomerStatusAndWorkflowStatus(Long subCustomerId, 
+                                                         String newStatus, 
+                                                         String newWorkflowStatus) { 
+        return subCustomerCheckerControllerRepository.setSubCustomerStatusAndWorkflowStatus(subCustomerId, newStatus, newWorkflowStatus); 
+    } 
+ 
+    public boolean updateSubCustomerDetails(Long subCustomerId, 
+                                            String newFirstName, 
+                                            String newLastName, 
+                                            String newAddress) { 
+        return subCustomerCheckerControllerRepository.updateSubCustomerDetails(subCustomerId, newFirstName, newLastName, newAddress); 
+    } 
+ 
+    public boolean updateSubCustomerResponse(Long subCustomerId) { 
+        return subCustomerCheckerControllerRepository.updateSubCustomerResponse(subCustomerId); 
+    } 
+ 
+    public SubCustomer createSubCustomer(SubCustomer subCustomer) { 
+        return subCustomerCheckerControllerRepository.save(subCustomer); 
+    } 
 }
