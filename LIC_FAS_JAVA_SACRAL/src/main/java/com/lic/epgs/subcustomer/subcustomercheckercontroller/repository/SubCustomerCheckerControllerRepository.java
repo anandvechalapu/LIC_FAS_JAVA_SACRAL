806866b604600245c2cd3c7d973b46b09b287b29
anandvechalapu <anand.vechalapu@gmail.com>
2023-05-04 10:18:55 +0530

    Updated

--- LIC_FAS_JAVA_SACRAL/src/main/java/com/lic/epgs/subcustomer/subcustomercheckercontroller/repository/SubCustomerCheckerControllerRepository.java
+++ LIC_FAS_JAVA_SACRAL/src/main/java/com/lic/epgs/subcustomer/subcustomercheckercontroller/repository/SubCustomerCheckerControllerRepository.java
@@ -1,7 +1,25 @@
-import org.springframework.data.jpa.repository.JpaRepository;
+package com.lic.epgs.subcustomer.subcustomercheckercontroller.repository; 
 
-public interface SubCustomerCheckerControllerRepository extends JpaRepository<SubCustomerCheckerController, Long> {
-
-    SubCustomerCheckerController getExitingSubCustomerDetails(String subCustomerId);
-
+import org.springframework.data.jpa.repository.JpaRepository; 
+import org.springframework.stereotype.Repository; 
+ 
+@Repository 
+public interface SubCustomerCheckerControllerRepository extends JpaRepository<SubCustomer, Long> { 
+ 
+    // Method to validate the sub customer ID 
+    boolean validateSubCustomerId(Long subCustomerId); 
+ 
+    // Method to set the sub customer status and workflow status to the new values 
+    boolean setSubCustomerStatusAndWorkflowStatus(Long subCustomerId, 
+                                                  String newStatus, 
+                                                  String newWorkflowStatus); 
+ 
+    // Method to update the customer details associated with the sub customer 
+    boolean updateSubCustomerDetails(Long subCustomerId, 
+                                     String newFirstName, 
+                                     String newLastName, 
+                                     String newAddress); 
+ 
+    // Method to return a response indicating the success or failure of the update 
+    boolean updateSubCustomerResponse(Long subCustomerId); 
 }
