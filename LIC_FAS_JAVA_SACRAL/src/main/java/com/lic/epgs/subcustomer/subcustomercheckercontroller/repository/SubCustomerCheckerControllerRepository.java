0f036df5326b5ce4968e56e25e2cf5e308d4ae5d
anandvechalapu <anand.vechalapu@gmail.com>
2023-05-04 10:10:48 +0530

    Updated

--- LIC_FAS_JAVA_SACRAL/src/main/java/com/lic/epgs/subcustomer/subcustomercheckercontroller/repository/SubCustomerCheckerControllerRepository.java
+++ LIC_FAS_JAVA_SACRAL/src/main/java/com/lic/epgs/subcustomer/subcustomercheckercontroller/repository/SubCustomerCheckerControllerRepository.java
@@ -1,20 +1,7 @@
-package com.lic.epgs.subcustomer.subcustomercheckercontroller.repository;
-
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Modifying;
-import org.springframework.data.jpa.repository.Query;
-import org.springframework.stereotype.Repository;
 
-import com.lic.epgs.subcustomer.subcustomercheckercontroller.model.SubCustomer;
+public interface SubCustomerCheckerControllerRepository extends JpaRepository<SubCustomerCheckerController, Long> {
 
-@Repository
-public interface SubCustomerCheckerControllerRepository extends JpaRepository<SubCustomer, Long> {
-
-    @Modifying
-    @Query("UPDATE SubCustomer SET address = ?1, contact = ?2, notes = ?3, bankAccountDetails = ?4 WHERE customerId = ?5")
-    void updateSubCustomer(String address, String contact, String notes, String bankAccountDetails, Long customerId);
-
-    @Query("SELECT s FROM SubCustomer s WHERE s.customerId = ?1")
-    SubCustomer findSubCustomerByCustomerId(Long customerId);
+    SubCustomerCheckerController getExitingSubCustomerDetails(String subCustomerId);
 
 }
