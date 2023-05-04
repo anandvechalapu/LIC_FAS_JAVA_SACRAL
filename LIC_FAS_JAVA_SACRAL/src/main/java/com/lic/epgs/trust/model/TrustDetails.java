8d4cc4b1775b2d752695565f4060e5d6cc2a98fc
anandvechalapu <anand.vechalapu@gmail.com>
2023-05-04 16:13:23 +0530

    Updated

--- LIC_FAS_JAVA_SACRAL/src/main/java/com/lic/epgs/trust/model/TrustDetails.java
+++ LIC_FAS_JAVA_SACRAL/src/main/java/com/lic/epgs/trust/model/TrustDetails.java
@@ -5,44 +5,44 @@ import javax.persistence.Id;
 
 @Entity
 public class TrustDetails {
-
-	@Id
-	private Long id;
-	private String trustId;
-	private String unitCode;
-	private String status;
-	private String description;
-	
-	// Getters and Setters
-	
-	public Long getId() {
-		return id;
-	}
-	public void setId(Long id) {
-		this.id = id;
-	}
-	public String getTrustId() {
-		return trustId;
-	}
-	public void setTrustId(String trustId) {
-		this.trustId = trustId;
-	}
-	public String getUnitCode() {
-		return unitCode;
-	}
-	public void setUnitCode(String unitCode) {
-		this.unitCode = unitCode;
-	}
-	public String getStatus() {
-		return status;
-	}
-	public void setStatus(String status) {
-		this.status = status;
-	}
-	public String getDescription() {
-		return description;
-	}
-	public void setDescription(String description) {
-		this.description = description;
-	}	
+    
+    @Id
+    private Long id;
+    
+    private String trustId;
+    private String contactPersonId;
+    private String role;
+    
+    public Long getId() {
+        return id;
+    }
+    
+    public void setId(Long id) {
+        this.id = id;
+    }
+    
+    public String getTrustId() {
+        return trustId;
+    }
+    
+    public void setTrustId(String trustId) {
+        this.trustId = trustId;
+    }
+    
+    public String getContactPersonId() {
+        return contactPersonId;
+    }
+    
+    public void setContactPersonId(String contactPersonId) {
+        this.contactPersonId = contactPersonId;
+    }
+    
+    public String getRole() {
+        return role;
+    }
+    
+    public void setRole(String role) {
+        this.role = role;
+    }
+    
 }
