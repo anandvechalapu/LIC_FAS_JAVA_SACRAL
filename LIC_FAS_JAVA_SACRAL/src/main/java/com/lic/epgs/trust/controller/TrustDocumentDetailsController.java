a94d3c0ab23cbeee6130beda1d94c7c1da3b5c60
anandvechalapu <anand.vechalapu@gmail.com>
2023-05-04 15:50:45 +0530

    Updated

--- LIC_FAS_JAVA_SACRAL/src/main/java/com/lic/epgs/trust/controller/TrustDocumentDetailsController.java
+++ LIC_FAS_JAVA_SACRAL/src/main/java/com/lic/epgs/trust/controller/TrustDocumentDetailsController.java
@@ -1,8 +1,13 @@
 package com.lic.epgs.trust.controller;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.lic.epgs.trust.model.TrustDocumentDetails;
@@ -10,27 +15,23 @@ import com.lic.epgs.trust.service.TrustDocumentDetailsService;
 
 @RestController
 public class TrustDocumentDetailsController {
-	
+
 	@Autowired
-	private TrustDocumentDetailsService trustDocumentDetailsService;
+	TrustDocumentDetailsService trustDocumentDetailsService;
 	
-	@PostMapping("/trust/document/details")
-	public TrustDocumentDetails addDocumentDetailsToTrust(@RequestBody TrustDocumentDetails trustDocumentDetails) {
-		return trustDocumentDetailsService.addDocumentDetailsToTrust(trustDocumentDetails);
+	@GetMapping("/trustDocuments")
+	public List<TrustDocumentDetails> getTrustDocuments(@RequestParam Long trustId) {
+		return trustDocumentDetailsService.findByTrustId(trustId);
 	}
 	
-	@PostMapping("/trust/document/update/remove")
-	public TrustDocumentDetails updateOrRemoveDocumentDetails(@RequestBody TrustDocumentDetails trustDocumentDetails) {
-		return trustDocumentDetailsService.updateOrRemoveDocumentDetails(trustDocumentDetails);
+	@PostMapping("/trustDocuments/save")
+	public TrustDocumentDetails saveTrustDocument(@RequestBody TrustDocumentDetails trustDocumentDetails) {
+		return trustDocumentDetailsService.saveTrustDocumentDetails(trustDocumentDetails);
 	}
 	
-	@PostMapping("/trust/document/response")
-	public TrustDocumentDetails getResponseForDocumentDetailsRequest(@RequestBody TrustDocumentDetails trustDocumentDetails) {
-		return trustDocumentDetailsService.getResponseForDocumentDetailsRequest(trustDocumentDetails);
+	@DeleteMapping("/trustDocuments/delete")
+	public void deleteTrustDocument(@RequestParam Long documentId) {
+		trustDocumentDetailsService.deleteTrustDocumentDetails(documentId);
 	}
 	
-	@PostMapping("/trust/document/log")
-	public TrustDocumentDetails logDocumentDetailsRequest(@RequestBody TrustDocumentDetails trustDocumentDetails) {
-		return trustDocumentDetailsService.logDocumentDetailsRequest(trustDocumentDetails);
-	}
 }
