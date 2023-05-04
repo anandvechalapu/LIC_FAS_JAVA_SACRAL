22b9920c4dab62a252b3bbeac301d4a881c555da
anandvechalapu <anand.vechalapu@gmail.com>
2023-05-04 12:18:44 +0530

    Updated

--- LIC_FAS_JAVA_SACRAL/src/main/java/com/lic/epgs/subcustomer/subcustomermakercontroller/controller/SubCustomerController.java
+++ LIC_FAS_JAVA_SACRAL/src/main/java/com/lic/epgs/subcustomer/subcustomermakercontroller/controller/SubCustomerController.java
@@ -1,97 +1,59 @@
 package com.lic.epgs.subcustomer.subcustomermakercontroller.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
 import com.lic.epgs.subcustomer.subcustomermakercontroller.model.SubCustomer;
 import com.lic.epgs.subcustomer.subcustomermakercontroller.service.SubCustomerService;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.*;
 
 @RestController
-@RequestMapping("/subcustomer")
 public class SubCustomerController {
 
     @Autowired
     private SubCustomerService subCustomerService;
 
-    @GetMapping("/{customerId}")
-    public SubCustomer findByCustomerId(@PathVariable Long customerId) {
-        return subCustomerService.findByCustomerId(customerId);
+    @GetMapping("/subcustomer/findByName")
+    public SubCustomer findByName(@RequestParam("name") String name) {
+        return subCustomerService.findByName(name);
     }
 
-    @PostMapping("/")
-    public void saveSubCustomerContactDetails(@RequestBody SubCustomer subCustomer) {
-        subCustomerService.saveSubCustomerContactDetails(subCustomer);
+    @GetMapping("/subcustomer/findByCode")
+    public SubCustomer findByCode(@RequestParam("code") String code) {
+        return subCustomerService.findByCode(code);
     }
 
-    @PutMapping("/")
-    public void updateSubCustomerContactDetails(@RequestBody SubCustomer subCustomer) {
-        subCustomerService.updateSubCustomerContactDetails(subCustomer);
+    @PostMapping("/subcustomer/save")
+    public SubCustomer save(@RequestBody SubCustomer subCustomer) {
+        return subCustomerService.save(subCustomer);
     }
 
-    @DeleteMapping("/")
-    public void deleteSubCustomerContactDetails(@RequestBody SubCustomer subCustomer) {
-        subCustomerService.deleteSubCustomerContactDetails(subCustomer);
+    @GetMapping("/subcustomer/deleteByName")
+    public void deleteByName(@RequestParam("name") String name) {
+        subCustomerService.deleteByName(name);
     }
 
-    @GetMapping("/contactType/{contactType}")
-    public SubCustomer findByContactType(@PathVariable String contactType) {
-        return subCustomerService.findByContactType(contactType);
+    @GetMapping("/subcustomer/deleteByCode")
+    public void deleteByCode(@RequestParam("code") String code) {
+        subCustomerService.deleteByCode(code);
     }
 
-    @GetMapping("/city/{city}")
-    public SubCustomer findByCity(@PathVariable String city) {
-        return subCustomerService.findByCity(city);
+    @PostMapping("/subcustomer/updateSubCustomerMappedDetails")
+    public void updateSubCustomerMappedDetails(@RequestBody String name, String code) {
+        subCustomerService.updateSubCustomerMappedDetails(name, code);
     }
 
-    @GetMapping("/GCTFlag/{GCTFlag}")
-    public SubCustomer findByGCTFlag(@PathVariable String GCTFlag) {
-        return subCustomerService.findByGCTFlag(GCTFlag);
+    @PostMapping("/subcustomer/generateSubCustomerCode")
+    public void generateSubCustomerCode(@RequestBody String name) {
+        subCustomerService.generateSubCustomerCode(name);
     }
 
-    @GetMapping("/departmentName/{departmentName}")
-    public SubCustomer findByDepartmentName(@PathVariable String departmentName) {
-        return subCustomerService.findByDepartmentName(departmentName);
+    @PostMapping("/subcustomer/validateSubCustomerName")
+    public void validateSubCustomerName(@RequestBody String name) {
+        subCustomerService.validateSubCustomerName(name);
     }
 
-    @GetMapping("/salutation/{salutation}")
-    public SubCustomer findBySalutation(@PathVariable String salutation) {
-        return subCustomerService.findBySalutation(salutation);
-    }
-
-    @GetMapping("/firstName/{firstName}")
-    public SubCustomer findByFirstName(@PathVariable String firstName) {
-        return subCustomerService.findByFirstName(firstName);
-    }
-
-    @GetMapping("/middleName/{middleName}")
-    public SubCustomer findByMiddleName(@PathVariable String middleName) {
-        return subCustomerService.findByMiddleName(middleName);
-    }
-
-    @GetMapping("/lastName/{lastName}")
-    public SubCustomer findByLastName(@PathVariable String lastName) {
-        return subCustomerService.findByLastName(lastName);
-    }
-
-    @GetMapping("/designation/{designation}")
-    public SubCustomer findByDesignation(@PathVariable String designation) {
-        return subCustomerService.findByDesignation(designation);
-    }
-
-    @GetMapping("/countryCode/{countryCode}")
-    public SubCustomer findByCountryCode(@PathVariable String countryCode) {
-        return subCustomerService.findByCountryCode(countryCode);
-    }
-
-    @GetMapping("/mobileNumber/{mobileNumber}")
-    public SubCustomer findByMobileNumber(@PathVariable String mobileNumber) {
-        return subCustomerService.findByMobileNumber(mobileNumber);
-    }
-
-    @GetMapping("/stdCode/{stdCode}")
-    public SubCustomer findByStdCode(@PathVariable String stdCode) {
-        return subCustomerService.findByStdCode(stdCode);
-    }
-
-    @GetMapping("/landlineNumber/{landlineNumber}")
-    public SubCustomer findByLandlineNumber(@PathVariable String landlineNumber) {
-        return subCustomerService.findByLandlineNumber(landlineNumber);
+}
