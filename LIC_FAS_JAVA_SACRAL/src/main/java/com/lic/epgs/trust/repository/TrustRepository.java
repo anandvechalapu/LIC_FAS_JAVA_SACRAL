e2732995d42c2eb37eac89a8a7c589e35feef7a2
anandvechalapu <anand.vechalapu@gmail.com>
2023-05-04 14:59:44 +0530

    Updated

--- LIC_FAS_JAVA_SACRAL/src/main/java/com/lic/epgs/trust/repository/TrustRepository.java
+++ LIC_FAS_JAVA_SACRAL/src/main/java/com/lic/epgs/trust/repository/TrustRepository.java
@@ -1,59 +1,17 @@
 package com.lic.epgs.trust.repository;
 
-import com.lic.epgs.trust.dto.TrustDetailsDto;
-import com.lic.epgs.trust.entity.BankAccountDetailsEntity;
-import com.lic.epgs.trust.entity.ContactDetailEntity;
-import com.lic.epgs.trust.entity.TrustDetailsTempEntity;
-import org.springframework.data.repository.CrudRepository;
+import java.util.List;
 
-public interface TrustRepository extends CrudRepository<TrustDetailsTempEntity, Long> {
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.stereotype.Repository;
 
-    /**
-     * Retrieves a TrustDetailsTempEntity associated with the trustId from the trustDetailsTempRepository
-     *
-     * @param trustId trust id
-     * @return trust details temp entity
-     */
-    TrustDetailsTempEntity findByTrustId(String trustId);
+import com.lic.epgs.trust.model.Trust;
 
-    /**
-     * Saves the modified TrustDetailsTempEntity in the trustDetailsTempRepository
-     *
-     * @param trustDetailsTempEntity trust details temp entity
-     * @return saved trust details temp entity
-     */
-    TrustDetailsTempEntity save(TrustDetailsTempEntity trustDetailsTempEntity);
+@Repository
+public interface TrustRepository extends JpaRepository<Trust, Long> {
 
-    /**
-     * Retrieves all BankAccountDetailsTempEntity objects associated with the trustId from the bankAccountDetailsTempRepository
-     *
-     * @param trustId trust id
-     * @return list of bank account details temp entity
-     */
-    Iterable<BankAccountDetailsEntity> findAllByTrustId(String trustId);
-
-    /**
-     * Saves all modified BankAccountDetailsTempEntity objects in the bankAccountDetailsTempRepository
-     *
-     * @param bankAccountDetailsTempEntities list of bank account details temp entity
-     * @return saved list of bank account details temp entity
-     */
-    Iterable<BankAccountDetailsEntity> saveAll(Iterable<BankAccountDetailsEntity> bankAccountDetailsTempEntities);
-
-    /**
-     * Retrieves all ContactDetailsTempEntity objects associated with the trustId from the contactDetailsTempRepository
-     *
-     * @param trustId trust id
-     * @return list of contact details temp entity
-     */
-    Iterable<ContactDetailEntity> findAllByTrustId(Long trustId);
-
-    /**
-     * Saves all modified ContactDetailsTempEntity objects in the contactDetailsTempRepository
-     *
-     * @param contactDetailsTempEntities list of contact details temp entity
-     * @return saved list of contact details temp entity
-     */
-    Iterable<ContactDetailEntity> saveAll(Iterable<ContactDetailEntity> contactDetailsTempEntities);
+	@Query("SELECT t FROM Trust t WHERE t.trustId = ?1 AND t.unitCode = ?2")
+	List<Trust> getExistingTrustDetails(String trustId, String unitCode);
 
 }
