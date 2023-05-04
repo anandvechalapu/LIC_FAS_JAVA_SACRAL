878ff9ed70f643e0ffde7752b610a64738b01a81
anandvechalapu <anand.vechalapu@gmail.com>
2023-05-04 15:03:41 +0530

    Updated

--- LIC_FAS_JAVA_SACRAL/src/main/java/com/lic/epgs/trust/model/TrustDetails.java
+++ LIC_FAS_JAVA_SACRAL/src/main/java/com/lic/epgs/trust/model/TrustDetails.java
@@ -1,13 +1,48 @@
 package com.lic.epgs.trust.model;
 
-import lombok.Data;
+import javax.persistence.Entity;
+import javax.persistence.Id;
 
-@Data
+@Entity
 public class TrustDetails {
 
-    private String trustId;
-    private TrustDetailsDto trustDetails;
-    private Iterable<BankAccountDetailsEntity> bankAccountDetails;
-    private Iterable<ContactDetailEntity> contactDetails;
-
+	@Id
+	private Long id;
+	private String trustId;
+	private String unitCode;
+	private String status;
+	private String description;
+	
+	// Getters and Setters
+	
+	public Long getId() {
+		return id;
+	}
+	public void setId(Long id) {
+		this.id = id;
+	}
+	public String getTrustId() {
+		return trustId;
+	}
+	public void setTrustId(String trustId) {
+		this.trustId = trustId;
+	}
+	public String getUnitCode() {
+		return unitCode;
+	}
+	public void setUnitCode(String unitCode) {
+		this.unitCode = unitCode;
+	}
+	public String getStatus() {
+		return status;
+	}
+	public void setStatus(String status) {
+		this.status = status;
+	}
+	public String getDescription() {
+		return description;
+	}
+	public void setDescription(String description) {
+		this.description = description;
+	}	
 }
