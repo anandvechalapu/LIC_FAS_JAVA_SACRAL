e30d2a8b83cce26a747a758cd379f6f2e4e95e70
anandvechalapu <anand.vechalapu@gmail.com>
2023-05-04 12:48:25 +0530

    Updated

--- LIC_FAS_JAVA_SACRAL/src/main/java/com/lic/epgs/subcustomer/subcustomermakercontroller/service/SubCustomerMakerControllerService.java
+++ LIC_FAS_JAVA_SACRAL/src/main/java/com/lic/epgs/subcustomer/subcustomermakercontroller/service/SubCustomerMakerControllerService.java
@@ -10,20 +10,38 @@ import com.lic.epgs.subcustomer.subcustomermakercontroller.repository.SubCustome
 
 @Service
 public class SubCustomerMakerControllerService {
-    
+
 	@Autowired
 	private SubCustomerMakerControllerRepository subCustomerMakerControllerRepository;
-	
-	public Optional<SubCustomerMakerController> findByStatusAndWorkflowStatus(String status, String workflowStatus) {
-		return subCustomerMakerControllerRepository.findByStatusAndWorkflowStatus(status, workflowStatus);
+
+	// method to send sub-customer to checker for approval
+	public void sendForApproval_SUB_CUSTOMER_MAKER_CONTROLLER(SubCustomerMakerController subCustomerMakerController) {
+		subCustomerMakerControllerRepository.save(subCustomerMakerController);
 	}
-	
-	public void updateStatusById(String status, Long id) {
-		subCustomerMakerControllerRepository.updateStatusById(status, id);
+
+	// method to provide sub-customer details
+	public void provideSubCustomerDetails(Long id, String status, String workflowStatus) {
+		Optional<SubCustomerMakerController> subCustomerMakerController = subCustomerMakerControllerRepository
+				.findById(id);
+		if (subCustomerMakerController.isPresent()) {
+			SubCustomerMakerController subCustomerMakerController1 = subCustomerMakerController.get();
+			subCustomerMakerController1.setStatus(status);
+			subCustomerMakerController1.setWorkflowStatus(workflowStatus);
+			subCustomerMakerControllerRepository.save(subCustomerMakerController1);
+		}
 	}
-	
-	public void updateMappedDetailsById(String mappedDetails, Long id) {
-		subCustomerMakerControllerRepository.updateMappedDetailsById(mappedDetails, id);
+
+	// method to update sub-customer mapped details
+	public void updateMappedDetails(SubCustomerMakerController subCustomerMakerController) {
+		subCustomerMakerControllerRepository.save(subCustomerMakerController);
 	}
-	
+
+	// method to provide feedback on success or failure of operation
+	public void provideFeedback(Boolean success) {
+		if (success) {
+			System.out.println("Operation successful");
+		} else {
+			System.out.println("Operation failed");
+		}
+	}
 }
