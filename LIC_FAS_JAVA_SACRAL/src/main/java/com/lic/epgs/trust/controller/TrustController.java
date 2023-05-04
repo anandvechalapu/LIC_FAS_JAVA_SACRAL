42f17ad8c52a516d3fb029148374c01176bc7c11
anandvechalapu <anand.vechalapu@gmail.com>
2023-05-04 14:40:07 +0530

    Updated

--- LIC_FAS_JAVA_SACRAL/src/main/java/com/lic/epgs/trust/controller/TrustController.java
+++ LIC_FAS_JAVA_SACRAL/src/main/java/com/lic/epgs/trust/controller/TrustController.java
@@ -2,50 +2,32 @@ package com.lic.epgs.trust.controller;
 
 import com.lic.epgs.trust.dto.TrustDetailsDto;
 import com.lic.epgs.trust.entity.BankAccountDetailsEntity;
-import com.lic.epgs.trust.entity.BankAccountDetailsTempEntity;
-import com.lic.epgs.trust.entity.ContactDetailsTempEntity;
-import com.lic.epgs.trust.entity.TrustDetailsTempEntity;
+import com.lic.epgs.trust.entity.ContactDetailEntity;
+import com.lic.epgs.trust.model.TrustDetails;
 import com.lic.epgs.trust.service.TrustService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
-import java.util.List;
-
 @RestController
-@RequestMapping(path = "/trust")
+@RequestMapping("/trust")
 public class TrustController {
 
     @Autowired
     private TrustService trustService;
 
-    @GetMapping(path = "/findByTrustId/{trustId}")
-    public TrustDetailsTempEntity findByTrustId(@PathVariable Long trustId) {
-        return trustService.findByTrustId(trustId);
+    @PostMapping("/details")
+    public TrustDetails getTrustDetails(@RequestBody TrustDetails trustDetails) {
+        trustDetails.setTrustDetails(trustService.getTrustDetailsTemp(trustDetails.getTrustId()));
+        trustDetails.setBankAccountDetails(trustService.getBankAccountDetailsTemp(trustDetails.getTrustId()));
+        trustDetails.setContactDetails(trustService.getContactDetailsTemp(trustDetails.getTrustId()));
+        return trustDetails;
     }
 
-    @GetMapping(path = "/findByTrustIdAndIsActive/{trustId}/{isActive}")
-    public List<BankAccountDetailsTempEntity> findByTrustIdAndIsActive(@PathVariable Long trustId, @PathVariable Boolean isActive) {
-        return trustService.findByTrustIdAndIsActive(trustId, isActive);
+    @PutMapping("/details")
+    public TrustDetails updateTrustDetails(@RequestBody TrustDetails trustDetails) {
+        trustDetails.setTrustDetails(trustService.saveTrustDetailsTemp((TrustDetailsDto) trustDetails.getTrustDetails()));
+        trustDetails.setBankAccountDetails(trustService.saveBankAccountDetailsTemp(trustDetails.getBankAccountDetails()));
+        trustDetails.setContactDetails(trustService.saveContactDetailsTemp(trustDetails.getContactDetails()));
+        return trustDetails;
     }
-
-    @PostMapping(path = "/saveBankAccountDetails")
-    public BankAccountDetailsEntity save(@RequestBody BankAccountDetailsEntity bankAccountDetailsEntity) {
-        return trustService.save(bankAccountDetailsEntity);
-    }
-
-    @GetMapping(path = "/findContactDetailsTempEntityByTrustIdAndIsActive/{trustId}/{isActive}")
-    public List<ContactDetailsTempEntity> findContactDetailsTempEntityByTrustIdAndIsActive(@PathVariable Long trustId, @PathVariable Boolean isActive) {
-        return trustService.findContactDetailsTempEntityByTrustIdAndIsActive(trustId, isActive);
-    }
-
-    @PostMapping(path = "/save")
-    public TrustDetailsTempEntity save(@RequestBody TrustDetailsTempEntity trustDetailsTempEntity) {
-        return trustService.save(trustDetailsTempEntity);
-    }
-
-    @PostMapping(path = "/saveTrustDetails/{role}")
-    public TrustDetailsTempEntity saveTrustDetails(@RequestBody TrustDetailsDto trustDetailsDto, @PathVariable String role) {
-        return trustService.saveTrustDetails(trustDetailsDto, role);
-    }
-
 }
