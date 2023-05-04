f12a4897cae8f2e3c21624d3753f8e41250c003f
anandvechalapu <anand.vechalapu@gmail.com>
2023-05-04 12:41:35 +0530

    Updated

--- LIC_FAS_JAVA_SACRAL/src/main/java/com/lic/epgs/subcustomer/subcustomermakercontroller/repository/SubCustomerMakerControllerRepository.java
+++ LIC_FAS_JAVA_SACRAL/src/main/java/com/lic/epgs/subcustomer/subcustomermakercontroller/repository/SubCustomerMakerControllerRepository.java
@@ -1,9 +1,19 @@
 package com.lic.epgs.subcustomer.subcustomermakercontroller.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import com.lic.epgs.subcustomer.subcustomermakercontroller.model.SubCustomerMakerController;
 
 public interface SubCustomerMakerControllerRepository extends JpaRepository<SubCustomerMakerController, Long> {
-
-	public List<SubCustomerMakerController> getMakerNotesBySubCustomerId(long subCustomerId);
-
+ 
+    @Query("SELECT s FROM SubCustomerMakerController s WHERE s.status = ?1 AND s.workflowStatus = ?2")
+    SubCustomerMakerController findByStatusAndWorkflowStatus(String status, String workflowStatus);
+    
+    @Query("UPDATE SubCustomerMakerController s SET s.status = ?1 WHERE s.id = ?2")
+    void updateStatusById(String status, Long id);
+    
+    @Query("UPDATE SubCustomerMakerController s SET s.mappedDetails = ?1 WHERE s.id = ?2")
+    void updateMappedDetailsById(String mappedDetails, Long id);
+    
 }
