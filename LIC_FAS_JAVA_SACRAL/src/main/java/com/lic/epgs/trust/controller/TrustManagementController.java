3a83e04520a5f4c811d6f5fbf3c768015f6a1a88
anandvechalapu <anand.vechalapu@gmail.com>
2023-05-04 16:19:22 +0530

    Updated

--- LIC_FAS_JAVA_SACRAL/src/main/java/com/lic/epgs/trust/controller/TrustManagementController.java
+++ LIC_FAS_JAVA_SACRAL/src/main/java/com/lic/epgs/trust/controller/TrustManagementController.java
@@ -1,26 +1,44 @@
 package com.lic.epgs.trust.controller;
 
-import java.util.List;
- 
-import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.lic.epgs.trust.dto.TrustDTO;
-import com.lic.epgs.trust.service.InProgressTrustCodeSearchTRUSTMANAGEMENTCONTROLLER15Service;
+import com.lic.epgs.trust.model.Trust;
+import com.lic.epgs.trust.service.RemoveTrustDocumentTRUSTMANAGEMENTCONTROLLER24Service;
 
 @RestController
 @RequestMapping("/trust")
 public class TrustManagementController {
 
-@Autowired
-private InProgressTrustCodeSearchTRUSTMANAGEMENTCONTROLLER15Service inProgressTrustCodeSearchTRUSTMANAGEMENTCONTROLLER15Service;
+    private RemoveTrustDocumentTRUSTMANAGEMENTCONTROLLER24Service removeTrustDocumentTRUSTMANAGEMENTCONTROLLER24Service;
 
-@GetMapping("/search")
-public List<TrustDTO> searchByTrustCodeAndUnitCodeAndRole(@RequestParam String trustCode, @RequestParam String unitCode, @RequestParam String role) {
-	return inProgressTrustCodeSearchTRUSTMANAGEMENTCONTROLLER15Service.findByTrustCodeAndUnitCodeAndRole(trustCode, unitCode, role);
-}
+    public TrustManagementController(RemoveTrustDocumentTRUSTMANAGEMENTCONTROLLER24Service removeTrustDocumentTRUSTMANAGEMENTCONTROLLER24Service) {
+        this.removeTrustDocumentTRUSTMANAGEMENTCONTROLLER24Service = removeTrustDocumentTRUSTMANAGEMENTCONTROLLER24Service;
+    }
 
+    @PostMapping("/trust/add")
+    public Trust createTrust(@RequestBody Trust trust) {
+        return trust;
+    }
+
+    @PutMapping("/trust/edit")
+    public Trust updateTrust(@RequestBody Trust trust) {
+        return trust;
+    }
+
+    @DeleteMapping("/trust/remove/{trustId}/{documentId}/{role}")
+    public void removeTrustDocument(@PathVariable Long trustId, @PathVariable Long documentId, @PathVariable String role) {
+        removeTrustDocumentTRUSTMANAGEMENTCONTROLLER24Service.removeTrustDocument(trustId, documentId, role);
+    }
+
+    @GetMapping("/trust/check/{trustId}/{documentId}/{role}")
+    public int checkIfTrustDocumentExists(@PathVariable Long trustId, @PathVariable Long documentId, @PathVariable String role) {
+        return removeTrustDocumentTRUSTMANAGEMENTCONTROLLER24Service.checkIfTrustDocumentExists(trustId, documentId, role);
+    }
 }
